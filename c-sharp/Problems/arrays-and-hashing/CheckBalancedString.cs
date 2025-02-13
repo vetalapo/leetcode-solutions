e87fa5c4353ef3695d918295389a2ee94be07246
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-13 23:52:09 +0100

    #3340: Check Balanced String: solution & tests

---
+++ c-sharp/Problems/arrays-and-hashing/CheckBalancedString.cs
@@ -0,0 +1,59 @@
+﻿/**
+ * 3340
+ * Check Balanced String
+ **
+ * You are given a string num consisting of only digits.
+ * A string of digits is called balanced
+ * if the sum of the digits at even indices
+ * is equal to the sum of digits at odd indices.
+ *
+ * Return true if num is balanced, otherwise return false.
+ *
+ * Example 1:
+ *   Input: num = "1234"
+ *   Output: false
+ *   Explanation:
+ *     The sum of digits at even indices is 1 + 3 == 4,
+ *     and the sum of digits at odd indices is 2 + 4 == 6.
+ *     Since 4 is not equal to 6,
+ *     num is not balanced.
+ *
+ * Example 2:
+ *   Input: num = "24123"
+ *   Output: true
+ *   Explanation:
+ *     The sum of digits at even indices is 2 + 1 + 3 == 6,
+ *     and the sum of digits at odd indices is 4 + 2 == 6.
+ *     Since both are equal
+ *     the num is balanced.
+ *
+ * Constraints:
+ *   • 2 <= num.length <= 100
+ *   • num consists of digits only
+ **
+ * https://leetcode.com/problems/check-balanced-string/
+***/
+
+namespace Problems;
+
+public class CheckBalancedString
+{
+    public bool IsBalanced( string num )
+    {
+        int diff = 0;
+
+        for ( int i = 0; i < num.Length; i++ )
+        {
+            if ( ( i & 1 ) == 0 )
+            {
+                diff += num[i] - '0';
+            }
+            else
+            {
+                diff -= num[i] - '0';
+            }
+        }
+
+        return diff == 0;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/CheckBalancedStringTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CheckBalancedStringTests
+{
+    [TestCase( "1234", ExpectedResult = false )]
+    [TestCase( "24123", ExpectedResult = true )]
+    public bool IsBalancedTest( string num ) =>
+        new CheckBalancedString().IsBalanced( num );
+}
