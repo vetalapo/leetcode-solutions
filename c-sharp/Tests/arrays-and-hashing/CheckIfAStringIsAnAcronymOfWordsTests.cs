7e193346b9b47fcbb6e680277c80523670a41a4b
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-14 23:30:17 +0100

    #2828: Check if a String Is an Acronym of Words; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/CheckIfAStringIsAnAcronymOfWordsTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class CheckIfAStringIsAnAcronymOfWordsTests
+{
+    [TestCase( new string[] { "alice", "bob", "charlie" }, "abc", ExpectedResult = true )]
+    [TestCase( new string[] { "an", "apple" }, "a", ExpectedResult = false )]
+    [TestCase( new string[] { "never", "gonna", "give", "up", "on", "you" }, "ngguoy", ExpectedResult = true )]
+    public bool IsAcronymTest( IList<string> words, string s ) =>
+        new CheckIfAStringIsAnAcronymOfWords().IsAcronym( words, s );
+}
