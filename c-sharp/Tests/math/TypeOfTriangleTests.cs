652ee8ca8bf7e86a89fe62c86c431b9b85eb1440
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-15 18:49:04 +0100

    #3024: Type of Triangle; solution & tests

---
+++ c-sharp/Tests/math/TypeOfTriangleTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class TypeOfTriangleTests
+{
+    [TestCase( new int[] { 3, 3, 3 }, ExpectedResult = "equilateral" )]
+    [TestCase( new int[] { 3, 4, 5 }, ExpectedResult = "scalene" )]
+    public string TriangleTypeTest( int[] nums ) =>
+        new TypeOfTriangle().TriangleType( nums );
+}
