002dafca014b13bc7a931a5fefc13858ebc645e4
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-16 23:49:24 +0100

    #2549: Count Distinct Numbers on Board; solution & tests

---
+++ c-sharp/Tests/math/CountDistinctNumbersOnBoardTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountDistinctNumbersOnBoardTests
+{
+    [TestCase( 5, ExpectedResult = 4 )]
+    [TestCase( 3, ExpectedResult = 2 )]
+    [TestCase( 1, ExpectedResult = 1 )]
+    public int DistinctIntegersTest( int n ) =>
+        new CountDistinctNumbersOnBoard().DistinctIntegers( n );
+}
