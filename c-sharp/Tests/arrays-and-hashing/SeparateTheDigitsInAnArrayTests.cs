9734bf49043b992cd92fac4551c81e4f4452dd17
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-17 17:48:17 +0100

    #2553: Separate the Digits in an Array; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/SeparateTheDigitsInAnArrayTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SeparateTheDigitsInAnArrayTests
+{
+    [TestCase( new int[] { 13, 25, 83, 77 }, ExpectedResult = new int[] { 1, 3, 2, 5, 8, 3, 7, 7 } )]
+    [TestCase( new int[] { 7, 1, 3, 9 }, ExpectedResult = new int[] { 7, 1, 3, 9 } )]
+    public int[] SeparateDigitsTest( int[] nums ) =>
+        new SeparateTheDigitsInAnArray().SeparateDigits( nums );
+}
