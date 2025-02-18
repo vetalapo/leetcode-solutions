261e4149ee3902fee342aff8b4b394395b1a1c7b
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-18 17:46:15 +0100

    #485: Max Consecutive Ones; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/MaxConsecutiveOnesTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MaxConsecutiveOnesTests
+{
+    [TestCase( new int[] { 1, 1, 0, 1, 1, 1 }, ExpectedResult = 3 )]
+    [TestCase( new int[] { 1, 0, 1, 1, 0, 1 }, ExpectedResult = 2 )]
+    public int FindMaxConsecutiveOnesTest( int[] nums ) =>
+        new MaxConsecutiveOnes().FindMaxConsecutiveOnes( nums );
+}
