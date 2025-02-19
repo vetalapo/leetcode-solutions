4a6b3b7934c46cab85af2f553378cf37733c4873
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-19 17:32:08 +0100

    #1207: Unique Number of Occurrences; solution & tests

---
+++ c-sharp/Problems/arrays-and-hashing/UniqueNumberOfOccurrences.cs
@@ -0,0 +1,67 @@
+﻿/**
+ * 1207
+ * Unique Number of Occurrences
+ **
+ * Given an array of integers arr,
+ * return true if the number of occurrences of each value in the array is unique or
+ * false otherwise.
+ *
+ * Example 1:
+ *   Input: arr = [1,2,2,1,1,3]
+ *   Output: true
+ *   Explanation:
+ *     The value 1 has 3 occurrences, 2 has 2 and 3 has 1.
+ *     No two values have the same number of occurrences.
+ *
+ * Example 2:
+ *   Input: arr = [1,2]
+ *   Output: false
+ *
+ * Example 3:
+ *   Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+ *   Output: true
+ *
+ * Constraints:
+ *   • 1 <= arr.length <= 1000
+ *   • -1000 <= arr[i] <= 1000
+ *
+ * Hint 1:
+ *   Find the number of occurrences of each element in the array using a hash map.
+ *
+ * Hint 2:
+ *   Iterate through the hash map and check if there is a repeated value.
+ **
+ * https://leetcode.com/problems/unique-number-of-occurrences/
+***/
+
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Problems;
+
+public class UniqueNumberOfOccurrences
+{
+    public bool UniqueOccurrences( int[] arr )
+    {
+        Dictionary<int, int> countMap = [];
+
+        foreach ( int num in arr )
+        {
+            ref int count = ref CollectionsMarshal.GetValueRefOrAddDefault( countMap, num, out bool _ );
+
+            count++;
+        }
+
+        HashSet<int> freqSet = new( countMap.Count );
+
+        foreach ( int count in countMap.Values )
+        {
+            if ( !freqSet.Add( count ) )
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/UniqueNumberOfOccurrencesTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class UniqueNumberOfOccurrencesTests
+{
+    [TestCase( new int[] { 1, 2, 2, 1, 1, 3 }, ExpectedResult = true )]
+    [TestCase( new int[] { 1, 2 }, ExpectedResult = false )]
+    [TestCase( new int[] { -3, 0, 1, -3, 1, 1, 1, -3, 10, 0 }, ExpectedResult = true )]
+    public bool UniqueOccurrencesTest( int[] arr ) =>
+        new UniqueNumberOfOccurrences().UniqueOccurrences( arr );
+}
