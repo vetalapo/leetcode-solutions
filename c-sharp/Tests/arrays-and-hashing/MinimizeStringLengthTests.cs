24fed074911841e05df97b1590d6a53225a3277a
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-20 18:37:40 +0100

    #2716: Minimize String Length: solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/MinimizeStringLengthTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MinimizeStringLengthTests
+{
+    [TestCase( "aaabc", ExpectedResult = 3 )]
+    [TestCase( "cbbd", ExpectedResult = 3 )]
+    [TestCase( "baadccab", ExpectedResult = 4 )]
+    public int MinimizedStringLengthTest( string s ) =>
+        new MinimizeStringLength().MinimizedStringLength( s );
+}
