7d63a4da68ba7a4d08bcf06d26bd90b74576c736
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-21 19:40:38 +0100

    #2733: Neither Minimum nor Maximum; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/NeitherMinimumNorMaximumTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class NeitherMinimumNorMaximumTests
+{
+    [TestCase( new int[] { 3, 2, 1, 4 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { 1, 2 }, ExpectedResult = -1 )]
+    [TestCase( new int[] { 2, 1, 3 }, ExpectedResult = 2 )]
+    public int FindNonMinOrMaxTest( int[] nums ) =>
+        new NeitherMinimumNorMaximum().FindNonMinOrMax( nums );
+}
