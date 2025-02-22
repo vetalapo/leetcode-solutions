5b969ef99c5351bb4ecd28fd902d34f5e8f81d67
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-22 18:58:46 +0100

    #1704: Determine if String Halves Are Alike; solution & tests

---
+++ c-sharp/Tests/two-pointers/DetermineIfStringHalvesAreAlikeTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class DetermineIfStringHalvesAreAlikeTests
+{
+    [TestCase( "book", ExpectedResult = true )]
+    [TestCase( "textbook", ExpectedResult = false )]
+    public bool HalvesAreAlikeTest( string s ) =>
+        new DetermineIfStringHalvesAreAlike().HalvesAreAlike( s );
+}
