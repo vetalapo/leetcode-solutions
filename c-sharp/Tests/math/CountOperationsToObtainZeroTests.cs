b6b126cd001efedd10c12c07ded3677e6e2cfd2c
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-24 20:36:18 +0100

    #2169: Count Operations to Obtain Zero; solution & tests

---
+++ c-sharp/Tests/math/CountOperationsToObtainZeroTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountOperationsToObtainZeroTests
+{
+    [TestCase( 2, 3, ExpectedResult = 3 )]
+    [TestCase( 10, 10, ExpectedResult = 1 )]
+    public int CountOperationsTest( int num1, int num2 ) =>
+        new CountOperationsToObtainZero().CountOperations( num1, num2 );
+}
