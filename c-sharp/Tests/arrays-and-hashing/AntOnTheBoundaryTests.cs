e4973612b1015c97c56506af18a692fb60184eae
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-25 16:27:43 +0100

    #3028: Ant on the Boundary; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/AntOnTheBoundaryTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class AntOnTheBoundaryTests
+{
+    [TestCase( new int[] { 2, 3, -5 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 3, 2, -3, -4 }, ExpectedResult = 0 )]
+    public int ReturnToBoundaryCountTest( int[] nums ) =>
+        new AntOnTheBoundary().ReturnToBoundaryCount( nums );
+}
