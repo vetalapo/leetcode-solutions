43a24764f94120d34757a62ffdf4b4092f482910
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-26 18:47:32 +0100

    #2441: Largest Positive Integer That Exists With Its Negative; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/LargestPositiveIntegerThatExistsWithItsNegativeTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class LargestPositiveIntegerThatExistsWithItsNegativeTests
+{
+    [TestCase( new int[] { -1, 2, -3, 3 }, ExpectedResult = 3 )]
+    [TestCase( new int[] { -1, 10, 6, 7, -7, 1 }, ExpectedResult = 7 )]
+    [TestCase( new int[] { -10, 8, 6, 7, -2, -3 }, ExpectedResult = -1 )]
+    public int FindMaxKTest( int[] nums ) =>
+        new LargestPositiveIntegerThatExistsWithItsNegative().FindMaxK( nums );
+}
