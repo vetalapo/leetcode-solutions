99f375e66b44ed02eb92dc66b746fe975c2a55ab
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-27 13:50:58 +0100

    #191: Number of 1 Bits; solution & tests

---
+++ c-sharp/Tests/bit-manipulation/NumberOf1BitsTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class NumberOf1BitsTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int HammingWeightTest( int n ) =>
+        new NumberOf1Bits().HammingWeight( n );
+
+    [TestCaseSource( nameof( TestCases ) )]
+    public int HammingWeightOptimizedTest( int n ) =>
+        new NumberOf1Bits().HammingWeightOptimized( n );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( 11 ).Returns( 3 );
+
+            yield return new TestCaseData( 128 ).Returns( 1 );
+
+            yield return new TestCaseData( 2147483645 ).Returns( 30 );
+        }
+    }
+}
