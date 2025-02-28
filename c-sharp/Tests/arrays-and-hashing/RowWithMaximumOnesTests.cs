705ad3717425a9836d29005ac8e5fd6466e4af6a
Vitaly Dushkin <vetalapo@gmail.com>
2025-02-28 17:38:48 +0100

    #2643: Row With Maximum Ones; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/RowWithMaximumOnesTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class RowWithMaximumOnesTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int[] RowAndMaximumOnesTest( int[][] mat ) =>
+        new RowWithMaximumOnes().RowAndMaximumOnes( mat );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( [new int[][] { [0, 1], [1, 0] }] ).Returns( new int[] { 0, 1 } );
+
+            yield return new TestCaseData( [new int[][] { [0, 0, 0], [0, 1, 1] }] ).Returns( new int[] { 1, 2 } );
+
+            yield return new TestCaseData( [new int[][] { [0, 0], [1, 1], [0, 0] }] ).Returns( new int[] { 1, 2 } );
+        }
+    }
+}
