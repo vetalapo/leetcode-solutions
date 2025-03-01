1ddbe575c8a46246a87a799f13aa2da09861696c
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-01 20:18:41 +0100

    #1304: Find N Unique Integers Sum up to Zero; solution & tests

---
+++ c-sharp/Problems/math/FindNUniqueIntegersSumUpToZero.cs
@@ -0,0 +1,59 @@
+﻿/**
+ * 1304
+ * Find N Unique Integers Sum up to Zero
+ **
+ * Given an integer n,
+ * return any array containing n unique integers such
+ * that they add up to 0.
+ * 
+ * Example 1:
+ *   Input: n = 5
+ *   Output: [-7,-1,1,3,4]
+ *   Explanation:
+ *     These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+ * 
+ * Example 2:
+ *   Input: n = 3
+ *   Output: [-1,0,1]
+ * 
+ * Example 3:
+ *   Input: n = 1
+ *   Output: [0]
+ *
+ * Constraints:
+ *   • 1 <= n <= 1000
+ *
+ * Hint 1:
+ *   Return an array where the values are symmetric. (+x , -x).
+ *
+ * Hint 2:
+ *   If n is odd, append value 0 in your returned array.
+ **
+ * https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+***/
+
+namespace Problems;
+
+public class FindNUniqueIntegersSumUpToZero
+{
+    public int[] SumZero( int n )
+    {
+        int secLead = -n / 2;
+        bool isEven = ( n & 1 ) == 0;
+
+        int[] result = new int[n];
+
+        for ( int i = 0; i < n; i++ )
+        {
+            if ( secLead == 0 && isEven )
+            {
+                secLead++;
+            }
+
+            result[i] = secLead;
+            secLead++;
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/math/FindNUniqueIntegersSumUpToZeroTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class FindNUniqueIntegersSumUpToZeroTests
+{
+    private static readonly int[] cases = [5, 3, 1, 500, 1000];
+
+    [TestCaseSource( nameof( cases ) )]
+    public void SumZeroTest( int n )
+    {
+        int[] result = new FindNUniqueIntegersSumUpToZero().SumZero( n );
+
+        Assert.AreEqual( result.Sum(), 0 );
+    }
+}
