5d8fbfbbf943a93d2e4ff6f1689765c1e27f6f55
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-03 15:25:31 +0100

    #2057: Smallest Index With Equal Value; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/SmallestIndexWithEqualValueTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SmallestIndexWithEqualValueTests
+{
+    [TestCase( new int[] { 0, 1, 2 }, ExpectedResult = 0 )]
+    [TestCase( new int[] { 4, 3, 2, 1 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, ExpectedResult = -1 )]
+    public int SmallestEqualTest( int[] nums ) =>
+        new SmallestIndexWithEqualValue().SmallestEqual( nums );
+}
