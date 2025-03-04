f35942a53e9c63559eaaf10317bf72d181059181
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-04 13:59:53 +0100

    #2980: Check if Bitwise OR Has Trailing Zeros; solution & tests

---
+++ c-sharp/Tests/bit-manipulation/CheckIfBitwiseORHasTrailingZerosTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CheckIfBitwiseORHasTrailingZerosTests
+{
+    [TestCase( new int[] { 1, 2, 3, 4, 5 }, ExpectedResult = true )]
+    [TestCase( new int[] { 2, 4, 8, 16 }, ExpectedResult = true )]
+    [TestCase( new int[] { 1, 3, 5, 7, 9 }, ExpectedResult = false )]
+    [TestCase( new int[] { 1, 2, 3, 5, 7, 9 }, ExpectedResult = false )]
+    public bool HasTrailingZerosTest( int[] nums ) =>
+        new CheckIfBitwiseORHasTrailingZeros().HasTrailingZeros( nums );
+}
