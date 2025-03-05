31e7c49d5ba0458997d805ff95b125a95c52e739
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-05 17:29:30 +0100

    #3467: Transform Array by Parity; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/TransformArrayByParityTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class TransformArrayByParityTests
+{
+    [TestCase( new int[] { 4, 3, 2, 1 }, ExpectedResult = new int[] { 0, 0, 1, 1 } )]
+    [TestCase( new int[] { 1, 5, 1, 4, 2 }, ExpectedResult = new int[] { 0, 0, 1, 1, 1 } )]
+    public int[] TransformArrayTest( int[] nums ) =>
+        new TransformArrayByParity().TransformArray( nums );
+}
