1b975e7584aad82ea23a66026b62cd39b7dc9e80
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-06 09:35:09 +0100

    #1491: Average Salary Excluding the Minimum and Maximum Salary; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/AverageSalaryExcludingTheMinimumAndMaximumSalaryTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class AverageSalaryExcludingTheMinimumAndMaximumSalaryTests
+{
+    [TestCase( new int[] { 4000, 3000, 1000, 2000 }, ExpectedResult = 2500.0 )]
+    [TestCase( new int[] { 1000, 2000, 3000 }, ExpectedResult = 2000.0 )]
+    public double AverageTest( int[] salary ) =>
+        new AverageSalaryExcludingTheMinimumAndMaximumSalary().Average( salary );
+}
