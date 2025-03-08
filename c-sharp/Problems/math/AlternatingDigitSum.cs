c1c05f2370227279679dcacde4ce3c9d85341213
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-08 21:30:34 +0100

    #2544: Alternating Digit Sum; solution & tests

---
+++ c-sharp/Problems/math/AlternatingDigitSum.cs
@@ -0,0 +1,65 @@
+﻿/**
+ * 2544
+ * Alternating Digit Sum
+ **
+ * You are given a positive integer n.
+ * Each digit of n has a sign according to the following rules:
+ *   • The most significant digit is assigned a positive sign.
+ *   • Each other digit has an opposite sign to its adjacent digits.
+ *
+ * Return the sum of all digits with their corresponding sign.
+ *
+ * Example 1:
+ *   Input: n = 521
+ *   Output: 4
+ *   Explanation: (+5) + (-2) + (+1) = 4.
+ *
+ * Example 2:
+ *   Input: n = 111
+ *   Output: 1
+ *   Explanation: (+1) + (-1) + (+1) = 1.
+ *
+ * Example 3:
+ *   Input: n = 886996
+ *   Output: 0
+ *   Explanation: (+8) + (-8) + (+6) + (-9) + (+9) + (-6) = 0.
+ *
+ * Constraints:
+ *   • 1 <= n <= 10^9
+ *
+ * Hint 1:
+ *   The first step is to loop over the digits.
+ *   We can convert the integer into a string,
+ *   an array of digits, or just loop over its digits.
+ *
+ * Hint 2:
+ *   Keep a variable sign that initially equals 1
+ *   and a variable answer that initially equals 0.
+ *
+ * Hint 3:
+ *   Each time you loop over a digit i,
+ *   add sign * i to answer,
+ *   then multiply sign by -1.
+ **
+ * https://leetcode.com/problems/alternating-digit-sum/
+***/
+
+namespace Problems;
+
+public class AlternatingDigitSum
+{
+    public int AlternateDigitSum( int n )
+    {
+        int sum = 0;
+        int sign = 1;
+
+        while ( n > 0 )
+        {
+            sign *= -1;
+            sum += sign * ( n % 10 );
+            n /= 10;
+        }
+
+        return sum * sign;
+    }
+}
---
+++ c-sharp/Tests/math/AlternatingDigitSumTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class AlternatingDigitSumTests
+{
+    [TestCase( 521, ExpectedResult = 4 )]
+    [TestCase( 111, ExpectedResult = 1 )]
+    [TestCase( 886996, ExpectedResult = 0 )]
+    public int AlternateDigitSumTest( int n ) =>
+        new AlternatingDigitSum().AlternateDigitSum( n );
+}
