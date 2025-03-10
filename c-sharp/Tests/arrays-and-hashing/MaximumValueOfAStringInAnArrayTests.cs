da4a72f6fabb77628998fa42384dc74fc865e670
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-10 15:41:57 +0100

    #2496: Maximum Value of a String in an Array; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/MaximumValueOfAStringInAnArrayTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MaximumValueOfAStringInAnArrayTests
+{
+    [TestCase( [new string[] { "alic3", "bob", "3", "4", "00000" }], ExpectedResult = 5 )]
+    [TestCase( [new string[] { "1", "01", "001", "0001" }], ExpectedResult = 1 )]
+    public int MaximumValueTest( string[] strs ) =>
+        new MaximumValueOfAStringInAnArray().MaximumValue( strs );
+}
