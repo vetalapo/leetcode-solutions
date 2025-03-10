2dccbae898472176bb8d19e0f2e03ebac0a4d349
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-10 02:53:19 +0100

    #1903: Largest Odd Number in String; solution & tests

---
+++ c-sharp/Problems/greedy/LargestOddNumberInString.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * 1903
+ * Largest Odd Number in String
+ **
+ * You are given a string num, representing a large integer.
+ * Return the largest-valued odd integer (as a string)
+ * that is a non-empty substring of num,
+ * or an empty string "" if no odd integer exists.
+ *
+ * A substring is a contiguous sequence of characters within a string.
+ *
+ * Example 1:
+ *   Input: num = "52"
+ *   Output: "5"
+ *   Explanation:
+ *     The only non-empty substrings are "5", "2", and "52".
+ *     "5" is the only odd number.
+ *
+ * Example 2:
+ *   Input: num = "4206"
+ *   Output: ""
+ *   Explanation: There are no odd numbers in "4206".
+ *
+ * Example 3:
+ *   Input: num = "35427"
+ *   Output: "35427"
+ *   Explanation: "35427" is already an odd number.
+ *
+ * Constraints:
+ *   • 1 <= num.length <= 10^5
+ *   • num only consists of digits and does not contain any leading zeros.
+ *
+ * Hint 1:
+ *   In what order should you iterate through the digits?
+ *
+ * Hint 2:
+ *   If an odd number exists, where must the number start from?
+ **
+ * https://leetcode.com/problems/largest-odd-number-in-string/
+***/
+
+namespace Problems;
+
+public class LargestOddNumberInString
+{
+    public string LargestOddNumber( string num )
+    {
+        int i = num.LastIndexOfAny( ['1', '3', '5', '7', '9'] );
+
+        return num[0..( i + 1 )];
+    }
+}
---
+++ c-sharp/Tests/greedy/LargestOddNumberInStringTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class LargestOddNumberInStringTests
+{
+    [TestCase( "52", ExpectedResult = "5" )]
+    [TestCase( "4206", ExpectedResult = "" )]
+    [TestCase( "35427", ExpectedResult = "35427" )]
+    [TestCase( "10133890", ExpectedResult = "1013389" )]
+    [TestCase( "486379402", ExpectedResult = "486379" )]
+    public string LargestOddNumberTest( string num ) =>
+        new LargestOddNumberInString().LargestOddNumber( num );
+}
