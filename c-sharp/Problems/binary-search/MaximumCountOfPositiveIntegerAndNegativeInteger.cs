5c0d847fe786865681f264ec0c09f93753f478e8
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-12 23:05:11 +0100

    #2529: Maximum Count of Positive Integer and Negative Integer; solution & tests

---
+++ c-sharp/Problems/binary-search/MaximumCountOfPositiveIntegerAndNegativeInteger.cs
@@ -0,0 +1,115 @@
+﻿/**
+ * 2529
+ * Maximum Count of Positive Integer and Negative Integer
+ **
+ * Given an array nums sorted in non-decreasing order,
+ * return the maximum between the number of positive integers
+ * and the number of negative integers.
+ *
+ * In other words,
+ * if the number of positive integers in nums is pos
+ * and the number of negative integers is neg,
+ * then return the maximum of pos and neg.
+ *
+ * Note that 0 is neither positive nor negative.
+ *
+ * Example 1:
+ *   Input: nums = [-2,-1,-1,1,2,3]
+ *   Output: 3
+ *   Explanation:
+ *     There are 3 positive integers and 3 negative integers.
+ *     The maximum count among them is 3.
+ *
+ * Example 2:
+ *   Input: nums = [-3,-2,-1,0,0,1,2]
+ *   Output: 3
+ *   Explanation:
+ *     There are 2 positive integers and 3 negative integers.
+ *     The maximum count among them is 3.
+ *
+ * Example 3:
+ *   Input: nums = [5,20,66,1314]
+ *   Output: 4
+ *   Explanation:
+ *     There are 4 positive integers and 0 negative integers.
+ *     The maximum count among them is 4.
+ *
+ * Constraints:
+ *   • 1 <= nums.length <= 2000
+ *   • -2000 <= nums[i] <= 2000
+ *   • nums is sorted in a non-decreasing order.
+ *
+ * Follow up:
+ *   Can you solve the problem in O(log(n)) time complexity?
+ *
+ * Hint 1:
+ *   Count how many positive integers and negative integers are in the array.
+ *
+ * Hint 2:
+ *   Since the array is sorted, can we use the binary search?
+ **
+ * https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/
+***/
+
+using System;
+
+namespace Problems;
+
+public class MaximumCountOfPositiveIntegerAndNegativeInteger
+{
+    public int MaximumCount( int[] nums )
+    {
+        int negative = LowerBound( nums );
+        int positive = nums.Length - UpperBound( nums );
+
+        return Math.Max( negative, positive );
+    }
+
+    private int LowerBound( int[] nums )
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+        int index = nums.Length;
+
+        while ( left <= right )
+        {
+            int middle = ( right - left ) / 2 + left;
+
+            if ( nums[middle] < 0 )
+            {
+                left = middle + 1;
+            }
+            else if ( nums[middle] >= 0 )
+            {
+                right = middle - 1;
+                index = middle;
+            }
+        }
+
+        return index;
+    }
+
+    private int UpperBound( int[] nums )
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+        int index = nums.Length;
+
+        while ( left <= right )
+        {
+            int middle = ( right - left ) / 2 + left;
+
+            if ( nums[middle] > 0 )
+            {
+                right = middle - 1;
+                index = middle;
+            }
+            else if ( nums[middle] <= 0 )
+            {
+                left = middle + 1;
+            }
+        }
+
+        return index;
+    }
+}
---
+++ c-sharp/Tests/binary-search/MaximumCountOfPositiveIntegerAndNegativeIntegerTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MaximumCountOfPositiveIntegerAndNegativeIntegerTests
+{
+    [TestCase( new int[] { -2, -1, -1, 1, 2, 3 }, ExpectedResult = 3 )]
+    [TestCase( new int[] { -3, -2, -1, 0, 0, 1, 2 }, ExpectedResult = 3 )]
+    [TestCase( new int[] { 5, 20, 66, 1314 }, ExpectedResult = 4 )]
+    [TestCase( new int[] { -1563, -236, -114, -55, 427, 447, 687, 752, 1021, 1636 }, ExpectedResult = 6 )]
+    [TestCase( new int[] { -1 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 0 }, ExpectedResult = 0 )]
+    public int MaximumCountTest( int[] nums ) =>
+        new MaximumCountOfPositiveIntegerAndNegativeInteger().MaximumCount( nums );
+}
