55cf0f8d4c5740238ae786e63deb5c22f125ee0f
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-14 18:19:44 +0100

    #1678: Goal Parser Interpretation; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/GoalParserInterpretationTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class GoalParserInterpretationTests
+{
+    [TestCase( "G()(al)", ExpectedResult = "Goal" )]
+    [TestCase( "G()()()()(al)", ExpectedResult = "Gooooal" )]
+    [TestCase( "(al)G(al)()()G", ExpectedResult = "alGalooG" )]
+    public string InterpretTest( string command ) =>
+        new GoalParserInterpretation().Interpret( command );
+}
