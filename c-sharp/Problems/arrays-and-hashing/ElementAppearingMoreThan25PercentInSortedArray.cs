af9b4eb1986d67d9626873e38bb3d402fc259b09
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-15 18:46:38 +0100

    #1287: Element Appearing More Than 25% In Sorted Array; solution & tests

---
+++ c-sharp/Problems/arrays-and-hashing/ElementAppearingMoreThan25PercentInSortedArray.cs
@@ -0,0 +1,64 @@
+﻿/**
+ * 1287
+ * Element Appearing More Than 25% In Sorted Array
+ **
+ * Given an integer array sorted in non-decreasing order,
+ * there is exactly one integer in the array
+ * that occurs more than 25% of the time,
+ * return that integer.
+ *
+ * Example 1:
+ *   Input: arr = [1,2,2,6,6,6,6,7,10]
+ *   Output: 6
+ *
+ * Example 2:
+ *   Input: arr = [1,1]
+ *   Output: 1
+ *
+ * Constraints:
+ *   • 1 <= arr.length <= 10^4
+ *   • 0 <= arr[i] <= 10^5
+ *
+ * Hint 1:
+ *   Divide the array in four parts
+ *   [1 - 25%] [25 - 50 %] [50 - 75 %] [75% - 100%]
+ * 
+ * Hint 2:
+ *   The answer should be in one of the ends of the intervals.
+ * 
+ * Hint 3:
+ *   In order to check which is element is the answer
+ *   we can count the frequency with binarySearch.
+ **
+ * https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/
+***/
+
+namespace Problems;
+
+public class ElementAppearingMoreThan25PercentInSortedArray
+{
+    public int FindSpecialInteger( int[] arr )
+    {
+        // 25% boundary
+        int threshold = arr.Length / 4;
+
+        for ( int i = 0; i < arr.Length; i++ )
+        {
+            int right = i + 1;
+
+            while ( right < arr.Length && arr[right] == arr[i] )
+            {
+                right++;
+            }
+
+            if ( ( right - i ) > threshold )
+            {
+                return arr[i];
+            }
+
+            i = right - 1;
+        }
+
+        return 0;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/ElementAppearingMoreThan25PercentInSortedArrayTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ElementAppearingMoreThan25PercentInSortedArrayTests
+{
+    [TestCase( new int[] { 1, 2, 2, 6, 6, 6, 6, 7, 10 }, ExpectedResult = 6 )]
+    [TestCase( new int[] { 1, 1 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 1 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 1, 1, 2, 2, 3, 3, 3, 3 }, ExpectedResult = 3 )]
+    public int FindSpecialIntegerTest( int[] arr ) =>
+        new ElementAppearingMoreThan25PercentInSortedArray().FindSpecialInteger( arr );
+}
