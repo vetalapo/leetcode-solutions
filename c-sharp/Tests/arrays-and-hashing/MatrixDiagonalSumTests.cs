2def4c1babc854e61be1a1ee332f7e2587b3c55d
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-16 23:34:42 +0100

    #1572: Matrxi Diagonal Sum; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/MatrixDiagonalSumTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class MatrixDiagonalSumTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int DiagonalSumTest( int[][] mat ) =>
+        new MatrixDiagonalSum().DiagonalSum( mat );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( [new int[][] { [1, 2, 3], [4, 5, 6], [7, 8, 9] }] ).Returns( 25 );
+
+            yield return new TestCaseData( [new int[][] { [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1] }] ).Returns( 8 );
+
+            yield return new TestCaseData( [new int[][] { [5] }] ).Returns( 5 );
+        }
+    }
+}
