02ba4bf32d925f4e6592431eca0998d9fc84cdec
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-17 17:02:09 +0100

    #2206: Divide Array Into Equal Pairs; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/DivideArrayIntoEqualPairsTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class DivideArrayIntoEqualPairsTests
+{
+    [TestCase( new int[] { 3, 2, 3, 2, 2, 2 }, ExpectedResult = true )]
+    [TestCase( new int[] { 1, 2, 3, 4 }, ExpectedResult = false )]
+    [TestCase( new int[] { 6, 12, 11, 20, 17, 4 }, ExpectedResult = false )]
+    public bool DivideArrayTest( int[] nums ) =>
+        new DivideArrayIntoEqualPairs().DivideArray( nums );
+}
