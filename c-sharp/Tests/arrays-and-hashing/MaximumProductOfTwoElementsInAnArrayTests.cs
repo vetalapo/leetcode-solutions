ed40a7dcea511132cda5bb77a6c75577df851799
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-18 16:23:54 +0100

    #1464: Maximum Product of Two Elements in an Array; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/MaximumProductOfTwoElementsInAnArrayTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MaximumProductOfTwoElementsInAnArrayTests
+{
+    [TestCase( new int[] { 3, 4, 5, 2 }, ExpectedResult = 12 )]
+    [TestCase( new int[] { 1, 5, 4, 5 }, ExpectedResult = 16 )]
+    [TestCase( new int[] { 3, 7 }, ExpectedResult = 12 )]
+    public int MaxProductTest( int[] nums ) =>
+        new MaximumProductOfTwoElementsInAnArray().MaxProduct( nums );
+}
