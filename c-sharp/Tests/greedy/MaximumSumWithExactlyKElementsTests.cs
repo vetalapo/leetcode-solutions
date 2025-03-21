e80e86bae6f17c323b4e101ee78012fe9a8b7b7e
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-21 18:17:33 +0100

    #2656: Maximum Sum With Exactly K Elements; solution & tests

---
+++ c-sharp/Tests/greedy/MaximumSumWithExactlyKElementsTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MaximumSumWithExactlyKElementsTests
+{
+    [TestCase( new int[] { 1, 2, 3, 4, 5 }, 3, ExpectedResult = 18 )]
+    [TestCase( new int[] { 5, 5, 5 }, 2, ExpectedResult = 11 )]
+    [TestCase( new int[] { 4, 4, 9, 10, 10, 9, 3, 8, 4 }, 6, ExpectedResult = 75 )]
+    public int MaximizeSumTest( int[] nums, int k ) =>
+        new MaximumSumWithExactlyKElements().MaximizeSum( nums, k );
+}
