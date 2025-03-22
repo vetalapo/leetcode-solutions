f8585cb51d61c37cc5fadcbbec58728417e99205
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-22 22:24:28 +0100

    #728: Self Dividing Numbers; solution & tests

---
+++ c-sharp/Tests/intervals/SelfDividingNumbersSolutionTests.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class SelfDividingNumbersSolutionTests
+{
+    [TestCase( 1, 22, ExpectedResult = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22 } )]
+    [TestCase( 47, 85, ExpectedResult = new int[] { 48, 55, 66, 77 } )]
+    public IList<int> SelfDividingNumbersTest( int left, int right ) =>
+        new SelfDividingNumbersSolution().SelfDividingNumbers( left, right );
+}
