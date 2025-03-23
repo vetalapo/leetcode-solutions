b41f8e23eda455b994355f85f3474bd78e6c2538
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-23 21:30:48 +0100

    #3019: Number of Changing Keys; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/NumberOfChangingKeysTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class NumberOfChangingKeysTests
+{
+    [TestCase( "aAbBcC", ExpectedResult = 2 )]
+    [TestCase( "AaAaAaaA", ExpectedResult = 0 )]
+    [TestCase( "mDVD", ExpectedResult = 3 )]
+    public int CountKeyChangesTest( string s ) =>
+        new NumberOfChangingKeys().CountKeyChanges( s );
+}
