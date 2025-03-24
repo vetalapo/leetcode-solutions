11c0cd80c03b61af6dc1d75741759c08c5731b2f
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-24 22:49:14 +0100

    #338: Counting Bits: solution & tests

---
+++ c-sharp/Tests/bit-manipulation/CountingBitsTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountingBitsTests
+{
+    [TestCase( 2, ExpectedResult = new int[] { 0, 1, 1 } )]
+    [TestCase( 5, ExpectedResult = new int[] { 0, 1, 1, 2, 1, 2 } )]
+    public int[] CountBitsTest( int n ) =>
+        new CountingBits().CountBits( n );
+}
