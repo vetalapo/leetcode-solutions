94d54a06ebe7d97ccd3e7cc47b7f517043da7868
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-25 23:57:18 +0100

    #2241: Maximum Number of Pairs in Array; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/MaximumNumberOfPairsInArrayTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MaximumNumberOfPairsInArrayTests
+{
+    [TestCase( new int[] { 1, 3, 2, 1, 3, 2, 2 }, ExpectedResult = new int[] { 3, 1 } )]
+    [TestCase( new int[] { 1, 1 }, ExpectedResult = new int[] { 1, 0 } )]
+    [TestCase( new int[] { 0 }, ExpectedResult = new int[] { 0, 1 } )]
+    public int[] NumberOfPairsTest( int[] nums ) =>
+        new MaximumNumberOfPairsInArray().NumberOfPairs( nums );
+}
