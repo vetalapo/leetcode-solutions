af5da13147332f6368b7aee933dba7285dfd40d6
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-26 23:56:13 +0100

    #2778: Sum of Squares of Special Elements; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/SumOfSquaresOfSpecialElementsTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SumOfSquaresOfSpecialElementsTests
+{
+    [TestCase( new int[] { 1, 2, 3, 4 }, ExpectedResult = 21 )]
+    [TestCase( new int[] { 2, 7, 1, 19, 18, 3 }, ExpectedResult = 63 )]
+    public int SumOfSquaresTest( int[] nums ) =>
+        new SumOfSquaresOfSpecialElements().SumOfSquares( nums );
+}
