d487f780779316afe0c4830075fd73e382c1bbf5
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-28 17:24:53 +0100

    #2595: Number of Even and Odd Bits; solution & tests

---
+++ c-sharp/Problems/bit-manipulation/NumberOfEvenAndOddBits.cs
@@ -0,0 +1,73 @@
+﻿/**
+ * 2595
+ * Number of Even and Odd Bits
+ **
+ * You are given a positive integer n.
+ * Let even denote the number of even indices in the binary representation of n with value 1.
+ * Let odd denote the number of odd indices in the binary representation of n with value 1.
+ * Note that bits are indexed from right to left in the binary representation of a number.
+ *
+ * Return the array [even, odd].
+ *
+ * Example 1:
+ *   Input: n = 50
+ *   Output: [1,2]
+ *   Explanation:
+ *     The binary representation of 50 is 110010.
+ *     It contains 1 on indices 1, 4, and 5.
+ *
+ * Example 2:
+ *   Input: n = 2
+ *   Output: [0,1]
+ *   Explanation:
+ *     The binary representation of 2 is 10.
+ *     It contains 1 only on index 1.
+ *
+ * Constraints:
+ *   • 1 <= n <= 1000
+ *
+ * Hint 1:
+ *   Maintain two integer variables, even and odd,
+ *   to count the number of even and odd indices
+ *   in the binary representation of integer n.
+ *
+ * Hint 2:
+ *   Divide n by 2 while n is positive,
+ *   and if n modulo 2 is 1,
+ *   add 1 to its corresponding variable.
+ **
+ * https://leetcode.com/problems/number-of-even-and-odd-bits/
+***/
+
+namespace Problems;
+
+public class NumberOfEvenAndOddBits
+{
+    public int[] EvenOddBit( int n )
+    {
+        int evenCount = 0;
+        int oddCount = 0;
+
+        int i = 0;
+
+        while ( n > 0 )
+        {
+            if ( ( n & 1 ) == 1 )
+            {
+                if ( ( i & 1 ) == 0 )
+                {
+                    evenCount++;
+                }
+                else
+                {
+                    oddCount++;
+                }
+            }
+
+            n >>= 1;
+            i++;
+        }
+
+        return [evenCount, oddCount];
+    }
+}
---
+++ c-sharp/Tests/bit-manipulation/NumberOfEvenAndOddBitsTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+using Problems;
+public class NumberOfEvenAndOddBitsTests
+{
+    [TestCase( 50, ExpectedResult = new int[] { 1, 2 } )]
+    [TestCase( 2, ExpectedResult = new int[] { 0, 1 } )]
+    public int[] EvenOddBitTest( int n ) =>
+        new NumberOfEvenAndOddBits().EvenOddBit( n );
+}
