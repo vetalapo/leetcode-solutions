13676090704b5f986431e49d522e46ba16ecdd64
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-29 20:23:04 +0100

    #693: Binary Number with Alternationg Bits; solution & tests

---
+++ c-sharp/Problems/bit-manipulation/BinaryNumberWithAlternatingBits.cs
@@ -0,0 +1,54 @@
+﻿/**
+ * 693
+ * Binary Number with Alternating Bits
+ **
+ * Given a positive integer,
+ * check whether it has alternating bits:
+ * namely, if two adjacent bits will always have different values.
+ *
+ * Example 1:
+ *   Input: n = 5
+ *   Output: true
+ *   Explanation: The binary representation of 5 is: 101
+ *
+ * Example 2:
+ *   Input: n = 7
+ *   Output: false
+ *   Explanation: The binary representation of 7 is: 111.
+ *
+ * Example 3:
+ *   Input: n = 11
+ *   Output: false
+ *   Explanation: The binary representation of 11 is: 1011.
+ *
+ * Constraints:
+ *   • 1 <= n <= 2^31 - 1
+ **
+ * https://leetcode.com/problems/binary-number-with-alternating-bits/
+***/
+
+namespace Problems;
+
+public class BinaryNumberWithAlternatingBits
+{
+    public bool HasAlternatingBits( int n )
+    {
+        int prev = n & 1;
+        n >>= 1;
+
+        while ( n > 0 )
+        {
+            int curr = ( n & 1 );
+
+            if ( curr == prev )
+            {
+                return false;
+            }
+
+            n >>= 1;
+            prev = curr;
+        }
+
+        return true;
+    }
+}
---
+++ c-sharp/Tests/bit-manipulation/BinaryNumberWithAlternatingBitsTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class BinaryNumberWithAlternatingBitsTests
+{
+    [TestCase( 5, ExpectedResult = true )]
+    [TestCase( 7, ExpectedResult = false )]
+    [TestCase( 11, ExpectedResult = false )]
+    public bool HasAlternatingBitsTest( int n ) =>
+        new BinaryNumberWithAlternatingBits().HasAlternatingBits( n );
+}
