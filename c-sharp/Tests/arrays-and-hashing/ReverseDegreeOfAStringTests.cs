8a51a7196388a54cd48dda85c2830f613223cd54
Vitaly Dushkin <vetalapo@gmail.com>
2025-03-31 20:33:08 +0200

    #3498: Reverse Degree of a String; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/ReverseDegreeOfAStringTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ReverseDegreeOfAStringTests
+{
+    [TestCase( "abc", ExpectedResult = 148 )]
+    [TestCase( "zaza", ExpectedResult = 160 )]
+    public int ReverseDegreeTest( string s ) =>
+        new ReverseDegreeOfAString().ReverseDegree( s );
+}
