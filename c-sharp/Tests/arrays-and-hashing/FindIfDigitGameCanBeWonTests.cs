e27ceda87af930f5af05ec5ea86bdd0efa18c0a7
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-01 16:41:24 +0200

    #3232: Find if Digit Game Can Be Won; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/FindIfDigitGameCanBeWonTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FindIfDigitGameCanBeWonTests
+{
+    [TestCase( new int[] { 1, 2, 3, 4, 10 }, ExpectedResult = false )]
+    [TestCase( new int[] { 1, 2, 3, 4, 5, 14 }, ExpectedResult = true )]
+    [TestCase( new int[] { 5, 5, 5, 25 }, ExpectedResult = true )]
+    public bool CanAliceWinTest( int[] nums ) =>
+        new FindIfDigitGameCanBeWon().CanAliceWin( nums );
+}
