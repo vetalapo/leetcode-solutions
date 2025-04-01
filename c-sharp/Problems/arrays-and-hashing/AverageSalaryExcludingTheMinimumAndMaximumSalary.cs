a1f745b3d41b5b167d690d5a42ca63592574ad4a
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-01 16:51:33 +0200

    white space changes

--- c-sharp/Problems/arrays-and-hashing/AverageSalaryExcludingTheMinimumAndMaximumSalary.cs
+++ c-sharp/Problems/arrays-and-hashing/AverageSalaryExcludingTheMinimumAndMaximumSalary.cs
@@ -8,7 +8,7 @@
  * Return the average salary of employees
  * excluding the minimum and maximum salary.
  * Answers within 10-5 of the actual answer will be accepted.
- * 
+ *
  * Example 1:
  *   Input: salary = [4000,3000,1000,2000]
  *   Output: 2500.00000
