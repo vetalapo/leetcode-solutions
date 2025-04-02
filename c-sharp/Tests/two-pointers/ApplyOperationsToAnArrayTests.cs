c52c9928fd9d9e4863ccac37e7c6972886a0a441
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-02 17:33:06 +0200

    #2460: Apply Operations to an Array; solution & tests

---
+++ c-sharp/Tests/two-pointers/ApplyOperationsToAnArrayTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ApplyOperationsToAnArrayTests
+{
+    [TestCase( new int[] { 1, 2, 2, 1, 1, 0 }, ExpectedResult = new int[] { 1, 4, 2, 0, 0, 0 } )]
+    [TestCase( new int[] { 0, 1 }, ExpectedResult = new int[] { 1, 0 } )]
+    public int[] ApplyOperationsTest( int[] nums ) =>
+        new ApplyOperationsToAnArray().ApplyOperations( nums );
+}
