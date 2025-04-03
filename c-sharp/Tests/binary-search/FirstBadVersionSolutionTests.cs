95b02f2def9da8763b66b178ddb1b5748e75616c
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-03 17:49:34 +0200

    #278: First Bad Version; solution & tests

---
+++ c-sharp/Tests/binary-search/FirstBadVersionSolutionTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FirstBadVersionSolutionTests
+{
+    [TestCase( 5, 4, ExpectedResult = 4 )]
+    [TestCase( 1, 1, ExpectedResult = 1 )]
+    [TestCase( 2147483647, 2147483644, ExpectedResult = 2147483644 )]
+    public int FirstBadVersionTest( int n, int bad ) =>
+        new FirstBadVersionSolution( bad ).FirstBadVersion( n );
+}
