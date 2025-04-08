7204f270529aa7c60a92fe47ebaee48fe9d1c3ed
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-08 20:44:32 +0200

    #1550: Three Consecutive Odds; solution & tests

---
+++ c-sharp/Problems/arrays-and-hashing/ThreeConsecutiveOdds.cs
@@ -0,0 +1,56 @@
+﻿/**
+ * 1550
+ * Three Consecutive Odds
+ **
+ * Given an integer array arr,
+ * return true if there are three consecutive odd numbers in the array.
+ * Otherwise, return false.
+ *
+ * Example 1:
+ *   Input: arr = [2,6,4,1]
+ *   Output: false
+ *   Explanation: There are no three consecutive odds.
+ *
+ * Example 2:
+ *   Input: arr = [1,2,34,3,4,5,7,23,12]
+ *   Output: true
+ *   Explanation: [5,7,23] are three consecutive odds.
+ *
+ * Constraints:
+ *   • 1 <= arr.length <= 1000
+ *   • 1 <= arr[i] <= 1000
+ *
+ * Hint 1:
+ *   Check every three consecutive numbers in the array for parity.
+ **
+ * https://leetcode.com/problems/three-consecutive-odds/
+***/
+
+namespace Problems;
+
+public class ThreeConsecutiveOddsSolution
+{
+    public bool ThreeConsecutiveOdds( int[] arr )
+    {
+        int consecutiveOdds = 0;
+
+        for ( int i = 0; i < arr.Length; i++ )
+        {
+            if ( ( arr[i] & 1 ) == 1 )
+            {
+                consecutiveOdds++;
+
+                if ( consecutiveOdds >= 3 )
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                consecutiveOdds = 0;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/ThreeConsecutiveOddsSolutionTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ThreeConsecutiveOddsSolutionTests
+{
+    [TestCase( new int[] { 2, 6, 4, 1 }, ExpectedResult = false )]
+    [TestCase( new int[] { 1, 2, 34, 3, 4, 5, 7, 23, 12 }, ExpectedResult = true )]
+    public bool ThreeConsecutiveOddsTest( int[] arr ) =>
+        new ThreeConsecutiveOddsSolution().ThreeConsecutiveOdds( arr );
+}
