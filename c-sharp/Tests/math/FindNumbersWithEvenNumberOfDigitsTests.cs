7488228643cce19c3eac5c3589ea8bc578cc28de
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-10 18:52:40 +0200

    #1295: Find Numbers with Even Number of Digits; solution & tests

---
+++ c-sharp/Tests/math/FindNumbersWithEvenNumberOfDigitsTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FindNumbersWithEvenNumberOfDigitsTests
+{
+    [TestCase( new int[] { 12, 345, 2, 6, 7896 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { 555, 901, 482, 1771 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 100000 }, ExpectedResult = 1 )]
+    public int FindNumbersTest( int[] nums ) =>
+        new FindNumbersWithEvenNumberOfDigits().FindNumbers( nums );
+}
