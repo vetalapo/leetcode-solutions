9fb1c6070ee9fa9100a8f1f1e56ba15f513e1b81
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-11 14:28:13 +0200

    #1556: Thousand Separator; solution & tests

---
+++ c-sharp/Problems/arrays-and-hashing/ThousandSeparator.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * 1556
+ * Thousand Separator
+ **
+ * Given an integer n,
+ * add a dot (".") as the thousands separator
+ * and return it in string format.
+ *
+ * Example 1:
+ *   Input: n = 987
+ *   Output: "987"
+ *
+ * Example 2:
+ *   Input: n = 1234
+ *   Output: "1.234"
+ *
+ * Constraints:
+ *   • 0 <= n <= 2^31 - 1
+ *
+ * Hint 1:
+ *   Scan from the back of the integer
+ *   and use dots to connect blocks with length 3 except the last block.
+ **
+ * https://leetcode.com/problems/thousand-separator/
+***/
+
+namespace Problems;
+
+public class ThousandSeparatorSolution
+{
+    public string ThousandSeparator( int n )
+    {
+        if ( n <= 999 )
+        {
+            return n.ToString();
+        }
+
+        string result = string.Empty;
+
+        int i = 1;
+
+        while ( n > 0 )
+        {
+            result = ( i % 3 == 0 && n > 10 ? "." : "" ) + n % 10 + result;
+
+            n /= 10;
+            i++;
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/ThousandSeparatorSolutionTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ThousandSeparatorSolutionTests
+{
+    [TestCase( 987, ExpectedResult = "987" )]
+    [TestCase( 1234, ExpectedResult = "1.234" )]
+    [TestCase( 0, ExpectedResult = "0" )]
+    public string ThousandSeparatorTest( int n ) =>
+        new ThousandSeparatorSolution().ThousandSeparator( n );
+}
