da28e5c5490a3d32abf2bdd1220fff6c9ca5d057
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-12 12:36:58 +0200

    #2108: Find First Palindromic String in the Array; solution & tests

---
+++ c-sharp/Problems/two-pointers/FindFirstPalindromicStringInTheArray.cs
@@ -0,0 +1,78 @@
+﻿/**
+ * 2108
+ * Find First Palindromic String in the Array
+ **
+ * Given an array of strings words,
+ * return the first palindromic string in the array.
+ * If there is no such string, return an empty string "".
+ *
+ * A string is palindromic if it reads the same forward and backward.
+ *
+ * Example 1:
+ *   Input: words = ["abc","car","ada","racecar","cool"]
+ *   Output: "ada"
+ *   Explanation: The first string that is palindromic is "ada".
+ *   Note that "racecar" is also palindromic, but it is not the first.
+ *
+ * Example 2:
+ *   Input: words = ["notapalindrome","racecar"]
+ *   Output: "racecar"
+ *   Explanation: The first and only string that is palindromic is "racecar".
+ *
+ * Example 3:
+ *   Input: words = ["def","ghi"]
+ *   Output: ""
+ *   Explanation: There are no palindromic strings, so the empty string is returned.
+ *
+ * Constraints:
+ *   • 1 <= words.length <= 100
+ *   • 1 <= words[i].length <= 100
+ *   • words[i] consists only of lowercase English letters.
+ *
+ * Hint 1:
+ *   Iterate through the elements in order.
+ *   As soon as the current element is a palindrome, return it.
+ *
+ * Hint 2:
+ *   To check if an element is a palindrome,
+ *   can you reverse the string?
+ **
+ * https://leetcode.com/problems/find-first-palindromic-string-in-the-array/
+***/
+
+namespace Problems;
+
+public class FindFirstPalindromicStringInTheArray
+{
+    public string FirstPalindrome( string[] words )
+    {
+        foreach ( string word in words )
+        {
+            if ( IsPalindrome( word ) )
+            {
+                return word;
+            }
+        }
+
+        return string.Empty;
+    }
+
+    private bool IsPalindrome( string word )
+    {
+        int left = 0;
+        int right = word.Length - 1;
+
+        while ( left < right )
+        {
+            if ( word[left] != word[right] )
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
---
+++ c-sharp/Tests/two-pointers/FindFirstPalindromicStringInTheArrayTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FindFirstPalindromicStringInTheArrayTests
+{
+    [TestCase( [new string[] { "abc", "car", "ada", "racecar", "cool" }], ExpectedResult = "ada" )]
+    [TestCase( [new string[] { "notapalindrome", "racecar" }], ExpectedResult = "racecar" )]
+    [TestCase( [new string[] { "def", "ghi" }], ExpectedResult = "" )]
+    public string FirstPalindromeTest( string[] words ) =>
+        new FindFirstPalindromicStringInTheArray().FirstPalindrome( words );
+}
