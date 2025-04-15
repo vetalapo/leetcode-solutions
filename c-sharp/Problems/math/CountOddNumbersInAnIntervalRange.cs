931b1d3a85fe3a704c8a9fcbd0aa6aa2a8ba470f
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-15 10:56:09 +0200

    #1523: Count Odd Numbers in an Interval Range

---
+++ c-sharp/Problems/math/CountOddNumbersInAnIntervalRange.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * 1523
+ * Count Odd Numbers in an Interval Range
+ **
+ * Given two non-negative integers low and high.
+ * Return the count of odd numbers between low and high (inclusive).
+ *
+ * Example 1:
+ *   Input: low = 3, high = 7
+ *   Output: 3
+ *   Explanation: The odd numbers between 3 and 7 are [3,5,7].
+ *
+ * Example 2:
+ *   Input: low = 8, high = 10
+ *   Output: 1
+ *   Explanation: The odd numbers between 8 and 10 are [9].
+ *
+ * Constraints:
+ *   • 0 <= low <= high <= 10^9
+ *
+ * Hint 1:
+ *   If the range (high - low + 1) is even,
+ *   the number of even and odd numbers in this range will be the same.
+ *
+ * Hint 2:
+ *   If the range (high - low + 1) is odd,
+ *   the solution will depend on the parity of high and low.
+ **
+ * https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+***/
+
+namespace Problems;
+
+public class CountOddNumbersInAnIntervalRange
+{
+    public int CountOdds( int low, int high )
+    {
+        return ( high - low ) / 2 + ( ( low & 1 ) | ( high & 1 ) );
+    }
+}
---
+++ c-sharp/Tests/math/CountOddNumbersInAnIntervalRangeTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountOddNumbersInAnIntervalRangeTests
+{
+    [TestCase( 3, 7, ExpectedResult = 3 )]
+    [TestCase( 8, 10, ExpectedResult = 1 )]
+    [TestCase( 14, 17, ExpectedResult = 2 )]
+    public int CountOddsTest( int low, int high ) =>
+        new CountOddNumbersInAnIntervalRange().CountOdds( low, high );
+}
