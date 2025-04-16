92946dad2f993d1a07c6efa67241c5a77130f4a5
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-16 18:43:59 +0200

    #2455: Average Value of Even Numbers That Are Divisible by Three; solution & tests

---
+++ c-sharp/Tests/math/AverageValueOfEvenNumbersThatAreDivisibleByThreeTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class AverageValueOfEvenNumbersThatAreDivisibleByThreeTests
+{
+    [TestCase( new int[] { 1, 3, 6, 10, 12, 15 }, ExpectedResult = 9 )]
+    [TestCase( new int[] { 1, 2, 4, 7, 10 }, ExpectedResult = 0 )]
+    [TestCase( new int[] { 4, 4, 9, 10 }, ExpectedResult = 0 )]
+    public int AverageValueTest( int[] nums ) =>
+        new AverageValueOfEvenNumbersThatAreDivisibleByThree().AverageValue( nums );
+}
