7762bab2ba8f13b5341cc8bad0e83c207b81cb3e
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-19 00:24:16 +0200

    #1961: Check If String Is a Prefix of Array; solution & tests

---
+++ c-sharp/Tests/two-pointers/CheckIfStringIsAPrefixOfArrayTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CheckIfStringIsAPrefixOfArrayTests
+{
+    [TestCase( "iloveleetcode", new string[] { "i", "love", "leetcode", "apples" }, ExpectedResult = true )]
+    [TestCase( "iloveleetcode", new string[] { "apples", "i", "love", "leetcode" }, ExpectedResult = false )]
+    [TestCase( "a", new string[] { "aa", "aaaa", "banana" }, ExpectedResult = false )]
+    [TestCase( "z", new string[] { "z" }, ExpectedResult = true )]
+    [TestCase( "ccccccccc", new string[] { "c", "cc" }, ExpectedResult = false )]
+    public bool IsPrefixStringTest( string s, string[] words ) =>
+        new CheckIfStringIsAPrefixOfArray().IsPrefixString( s, words );
+}
