a6caa15fb48db58a74fd698ef20d5fcef95a0a0b
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-20 00:01:16 +0200

    #1446: Consecutive Characters; solution & tests

---
+++ c-sharp/Problems/arrays-and-hashing/ConsecutiveCharacters.cs
@@ -0,0 +1,62 @@
+﻿/**
+ * 1446
+ * Consecutive Characters
+ **
+ * The power of the string is the maximum length of a non-empty substring
+ * that contains only one unique character.
+ *
+ * Given a string s, return the power of s.
+ *
+ * Example 1:
+ *   Input: s = "leetcode"
+ *   Output: 2
+ *   Explanation:
+ *     The substring "ee" is of length 2 with the character 'e' only.
+ *
+ * Example 2:
+ *   Input: s = "abbcccddddeeeeedcba"
+ *   Output: 5
+ *   Explanation:
+ *     The substring "eeeee" is of length 5 with the character 'e' only.
+ *
+ * Constraints:
+ *   • 1 <= s.length <= 500
+ *   • s consists of only lowercase English letters.
+ *
+ * Hint 1:
+ *   Keep an array power where
+ *   power[i] is the maximum power of the i-th character.
+ *
+ * Hint 2:
+ *   The answer is max(power[i]).
+ **
+ * https://leetcode.com/problems/consecutive-characters/
+***/
+
+using System;
+
+namespace Problems;
+
+public class ConsecutiveCharacters
+{
+    public int MaxPower( string s )
+    {
+        int max = 0;
+
+        for ( int i = 0; i < s.Length; i++ )
+        {
+            int j = i + 1;
+
+            while ( j < s.Length && s[i] == s[j] )
+            {
+                j++;
+            }
+
+            max = Math.Max( max, j - i );
+
+            i = j - 1;
+        }
+
+        return max;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/ConsecutiveCharactersTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ConsecutiveCharactersTests
+{
+    [TestCase( "leetcode", ExpectedResult = 2 )]
+    [TestCase( "abbcccddddeeeeedcba", ExpectedResult = 5 )]
+    [TestCase( "hooraaaaaaaaaaay", ExpectedResult = 11 )]
+    public int MaxPowerTest( string s ) =>
+        new ConsecutiveCharacters().MaxPower( s );
+}
