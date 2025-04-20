37525486a1bfbc233f6add7bd489a27c01aaa675
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-21 01:59:18 +0200

    #13: Roman to Integer; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/RomanToIntegerTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class RomanToIntegerTests
+{
+    [TestCase( "III", ExpectedResult = 3 )]
+    [TestCase( "LVIII", ExpectedResult = 58 )]
+    [TestCase( "MCMXCIV", ExpectedResult = 1994 )]
+    public int RomanToIntTest( string s ) =>
+        new RomanToInteger().RomanToInt( s );
+}
