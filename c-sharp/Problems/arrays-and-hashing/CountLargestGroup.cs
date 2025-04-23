cb14d918ffd5c3233c8bdaadc5523c9aa6e7ce7e
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-23 11:51:54 +0200

    #1399: Count Largest Group; solution & tests

---
+++ c-sharp/Problems/arrays-and-hashing/CountLargestGroup.cs
@@ -0,0 +1,78 @@
+﻿/**
+ * 1399
+ * Count Largest Group
+ **
+ * You are given an integer n.
+ * Each number from 1 to n is grouped according to the sum of its digits.
+ *
+ * Return the number of groups that have the largest size.
+ *
+ * Example 1:
+ *   Input: n = 13
+ *   Output: 4
+ *   Explanation:
+ *     There are 9 groups in total,
+ *     they are grouped according sum of its digits of numbers from 1 to 13:
+ *     [1,10], [2,11], [3,12], [4,13], [5], [6], [7], [8], [9].
+ *     There are 4 groups with largest size.
+ *
+ * Example 2:
+ *   Input: n = 2
+ *   Output: 2
+ *   Explanation:
+ *     There are 2 groups [1], [2] of size 1.
+ *
+ * Constraints:
+ *   1 <= n <= 10^4
+ *
+ * Hint 1:
+ *   Count the digit sum for each integer in the range and find out the largest groups.
+ **
+ * https://leetcode.com/problems/count-largest-group/
+***/
+
+using System;
+
+namespace Problems;
+
+public class CountLargestGroupSolution
+{
+    public int CountLargestGroup( int n )
+    {
+        int[] groups = new int[37];
+
+        while ( n > 0 )
+        {
+            int sum = 0;
+            int temp = n;
+
+            while ( temp > 0 )
+            {
+                sum += temp % 10;
+                temp /= 10;
+            }
+
+            groups[sum]++;
+            n--;
+        }
+
+        int max = 0;
+
+        foreach ( int group in groups )
+        {
+            max = Math.Max( max, group );
+        }
+
+        int result = 0;
+
+        foreach ( int group in groups )
+        {
+            if ( group == max )
+            {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/CountLargestGroupSolutionTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountLargestGroupSolutionTests
+{
+    [TestCase( 13, ExpectedResult = 4 )]
+    [TestCase( 2, ExpectedResult = 2 )]
+    [TestCase( 24, ExpectedResult = 5 )]
+    [TestCase( 9685, ExpectedResult = 1 )]
+    [TestCase( 9999, ExpectedResult = 1 )]
+    public int CountLargestGroupTest( int n ) =>
+        new CountLargestGroupSolution().CountLargestGroup( n );
+}
