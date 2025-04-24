0447095ca369ee57083f02d5f1e2a8d6a5c279b1
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-24 23:06:32 +0200

    #2283: Check if Number Has Equal Digit Count and Digit Value; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/CheckIfNumberHasEqualDigitCountAndDigitValueTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CheckIfNumberHasEqualDigitCountAndDigitValueTests
+{
+    [TestCase( "1210", ExpectedResult = true )]
+    [TestCase( "030", ExpectedResult = false )]
+    public bool DigitCountTest( string num ) =>
+        new CheckIfNumberHasEqualDigitCountAndDigitValue().DigitCount( num );
+}
