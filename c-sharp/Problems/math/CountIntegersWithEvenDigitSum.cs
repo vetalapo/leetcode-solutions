892031662b8a8c8b1eb57ae21637fe8b0eea167a
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-26 12:51:25 +0200

    #2180: Count Integers With Even Digit Sum; solution & tests

---
+++ c-sharp/Problems/math/CountIntegersWithEvenDigitSum.cs
@@ -0,0 +1,49 @@
+﻿/**
+ * 2180
+ * Count Integers With Even Digit Sum
+ **
+ * Given a positive integer num,
+ * return the number of positive integers less than or equal to num
+ * whose digit sums are even.
+ *
+ * The digit sum of a positive integer is the sum of all its digits.
+ *
+ * Example 1:
+ *   Input: num = 4
+ *   Output: 2
+ *   Explanation:
+ *     The only integers less than or equal to 4
+ *     whose digit sums are even are 2 and 4.
+ *
+ * Example 2:
+ *   Input: num = 30
+ *   Output: 14
+ *   Explanation:
+ *     The 14 integers less than or equal to 30
+ *     whose digit sums are even are
+ *     2, 4, 6, 8, 11, 13, 15, 17, 19, 20, 22, 24, 26, and 28.
+ *
+ * Constraints:
+ *   • 1 <= num <= 1000
+ *
+ * Hint 1:
+ *   Iterate through all integers from 1 to num.
+ *
+ * Hint 2:
+ *   For any integer,
+ *   extract the individual digits to compute their sum and check if it is even.
+ **
+ * https://leetcode.com/problems/count-integers-with-even-digit-sum/
+***/
+
+namespace Problems;
+
+public class CountIntegersWithEvenDigitSum
+{
+    public int CountEven( int num )
+    {
+        int sum = num + ( num / 10 ) + ( num / 100 ) + ( num / 1000 );
+
+        return ( num - ( sum & 1 ) ) / 2;
+    }
+}
---
+++ c-sharp/Tests/math/CountIntegersWithEvenDigitSumTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountIntegersWithEvenDigitSumTests
+{
+    [TestCase( 4, ExpectedResult = 2 )]
+    [TestCase( 30, ExpectedResult = 14 )]
+    [TestCase( 38, ExpectedResult = 18 )]
+    [TestCase( 63, ExpectedResult = 31 )]
+    public int CountEvenTest( int num ) =>
+        new CountIntegersWithEvenDigitSum().CountEven( num );
+}
