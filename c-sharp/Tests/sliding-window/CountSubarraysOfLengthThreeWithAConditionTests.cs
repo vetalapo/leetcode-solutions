23a66062a5304307b0c833f7095560029302a83f
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-27 13:44:53 +0200

    #3392: Count Subarrays of Length Three With a Condition; solution & tests

---
+++ c-sharp/Tests/sliding-window/CountSubarraysOfLengthThreeWithAConditionTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountSubarraysOfLengthThreeWithAConditionTests
+{
+    [TestCase( new int[] { 1, 2, 1, 4, 1 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 1, 1, 1 }, ExpectedResult = 0 )]
+    [TestCase( new int[] { 0, -4, -4 }, ExpectedResult = 0 )]
+    [TestCase( new int[] { -1, -4, -1, 4 }, ExpectedResult = 1 )]
+    public int CountSubarraysTest( int[] nums ) =>
+        new CountSubarraysOfLengthThreeWithACondition().CountSubarrays( nums );
+}
