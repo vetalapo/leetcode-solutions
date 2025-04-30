5b11faba86fbbd56fafcc6791be281d21c3ea918
Vitaly Dushkin <vetalapo@gmail.com>
2025-04-30 18:15:03 +0200

    #509: Fibonacci Number; solution & tests

---
+++ c-sharp/Tests/math/FibonacciNumberTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FibonacciNumberTests
+{
+    [TestCase( 2, ExpectedResult = 1 )]
+    [TestCase( 3, ExpectedResult = 2 )]
+    [TestCase( 4, ExpectedResult = 3 )]
+    [TestCase( 0, ExpectedResult = 0 )]
+    public int FibTest( int n ) =>
+        new FibonacciNumber().Fib( n );
+}
