a96a08b79c0480c0f7458f3a5c5c5488d9e6a260
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-01 16:42:23 +0200

    #222: Count Complete Tree Nodes; solution & tests

---
+++ c-sharp/Problems/trees/CountCompleteTreeNodes.cs
@@ -0,0 +1,65 @@
+﻿/**
+ * 222
+ * Count Complete Tree Nodes
+ **
+ * Given the root of a complete binary tree,
+ * return the number of the nodes in the tree.
+ *
+ * According to Wikipedia (https://en.wikipedia.org/wiki/Binary_tree#Types_of_binary_trees),
+ * every level, except possibly the last,
+ * is completely filled in a complete binary tree,
+ * and all nodes in the last level are as far left as possible.
+ * It can have between 1 and 2h nodes inclusive at the last level h.
+ *
+ * Design an algorithm that runs in less than O(n) time complexity.
+ *
+ * Example 1:
+ *   Input: root = [1,2,3,4,5,6]
+ *   Output: 6
+ *
+ * Example 2:
+ *   Input: root = []
+ *   Output: 0
+ *
+ * Example 3:
+ *   Input: root = [1]
+ *   Output: 1
+ *
+ * Constraints:
+ *   • The number of nodes in the tree is in the range [0, 5 * 10^4].
+ *   • 0 <= Node.val <= 5 * 10^4
+ *   • The tree is guaranteed to be complete.
+ **
+ * https://leetcode.com/problems/count-complete-tree-nodes/
+***/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+***/
+
+using Problems.Common;
+
+namespace Problems;
+
+public class CountCompleteTreeNodes
+{
+    public int CountNodes( TreeNode root )
+    {
+        if ( root is null )
+        {
+            return 0;
+        }
+
+        return 1 + CountNodes( root.left ) + CountNodes( root.right );
+    }
+}
---
+++ c-sharp/Tests/trees/CountCompleteTreeNodesTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+using Problems.Common;
+
+public class CountCompleteTreeNodesTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int CountNodesTest( TreeNode root ) =>
+        new CountCompleteTreeNodes().CountNodes( root );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( TreeNode.CreateBST( [1, 2, 3, 4, 5, 6] ) ).Returns( 6 );
+
+            yield return new TestCaseData( TreeNode.CreateBST( [] ) ).Returns( 0 );
+
+            yield return new TestCaseData( TreeNode.CreateBST( [1] ) ).Returns( 1 );
+
+            yield return new TestCaseData( TreeNode.CreateBST( [1, 2, 3, 4] ) ).Returns( 4 );
+        }
+    }
+}
