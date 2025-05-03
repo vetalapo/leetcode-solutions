b403ea15fdaf8612bed10d1904d2d9f98ea150a8
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-04 01:07:19 +0200

    #2423: Remove Letter To Equalize Frequency; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/RemoveLetterToEqualizeFrequencyTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class RemoveLetterToEqualizeFrequencyTests
+{
+    [TestCase( "abcc", ExpectedResult = true )]
+    [TestCase( "aazz", ExpectedResult = false )]
+    [TestCase( "bac", ExpectedResult = true )]
+    [TestCase( "adbc", ExpectedResult = true )]
+    [TestCase( "aca", ExpectedResult = true )]
+    [TestCase( "ddaccb", ExpectedResult = false )]
+    [TestCase( "acbda", ExpectedResult = true )]
+    [TestCase( "abbcc", ExpectedResult = true )]
+    public bool EqualFrequencyTest( string word ) =>
+        new RemoveLetterToEqualizeFrequency().EqualFrequency( word );
+}
