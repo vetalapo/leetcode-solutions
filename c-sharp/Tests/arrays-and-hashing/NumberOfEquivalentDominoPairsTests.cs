70c146fd3729e33187daf45b4c3418e3f71bbaec
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-04 23:59:39 +0200

    #1128: solution

---
+++ c-sharp/Tests/arrays-and-hashing/NumberOfEquivalentDominoPairsTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class NumberOfEquivalentDominoPairsTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int NumEquivDominoPairsTest( int[][] dominoes ) =>
+        new NumberOfEquivalentDominoPairs().NumEquivDominoPairs( dominoes );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return TestCaseData( new int[][] { [] } ).Returns( 0 );
+        }
+    }
+}
