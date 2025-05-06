a64fb3d73774180a93a78c5ef3cb1d2c5dfd5e1f
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-06 16:35:09 +0200

    #1822: Sign of the Product of an Array; solution & tests

---
+++ c-sharp/Problems/arrays-and-hashing/SignOfTheProductOfAnArray.cs
@@ -0,0 +1,69 @@
+﻿/**
+ * 1822
+ * Sign of the Product of an Array
+ **
+ * Implement a function signFunc(x) that returns:
+ *   • 1 if x is positive.
+ *   • -1 if x is negative.
+ *   • 0 if x is equal to 0.
+ *
+ * You are given an integer array nums.
+ * Let product be the product of all values in the array nums.
+ *
+ * Return signFunc(product).
+ *
+ * Example 1:
+ *   Input: nums = [-1,-2,-3,-4,3,2,1]
+ *   Output: 1
+ *   Explanation:
+ *     The product of all values in the array is 144, and signFunc(144) = 1
+ *
+ * Example 2:
+ *   Input: nums = [1,5,0,2,-3]
+ *   Output: 0
+ *   Explanation:
+ *     The product of all values in the array is 0, and signFunc(0) = 0
+ *
+ * Example 3:
+ *   Input: nums = [-1,1,-1,1,-1]
+ *   Output: -1
+ *   Explanation:
+ *     The product of all values in the array is -1, and signFunc(-1) = -1
+ *
+ * Constraints:
+ *   • 1 <= nums.length <= 1000
+ *   • -100 <= nums[i] <= 100
+ *
+ * Hint 1:
+ *   If there is a 0 in the array the answer is 0
+ *
+ * Hint 2:
+ *   To avoid overflow make all the negative numbers -1
+ *   and all positive numbers 1 and calculate the prod
+ **
+ * https://leetcode.com/problems/sign-of-the-product-of-an-array/
+***/
+
+namespace Problems;
+
+public class SignOfTheProductOfAnArray
+{
+    public int ArraySign( int[] nums )
+    {
+        int sign = 1;
+
+        foreach ( int num in nums )
+        {
+            if ( num == 0 )
+            {
+                return 0;
+            }
+            else if ( num < 0 )
+            {
+                sign *= -1;
+            }
+        }
+
+        return sign;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/SignOfTheProductOfAnArrayTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SignOfTheProductOfAnArrayTests
+{
+    [TestCase( new int[] { -1, -2, -3, -4, 3, 2, 1 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 1, 5, 0, 2, -3 }, ExpectedResult = 0 )]
+    [TestCase( new int[] { -1, 1, -1, 1, -1 }, ExpectedResult = -1 )]
+    [TestCase( new int[] { 7, 36, 96, 70, 85, 23, 5, 18, 4, 12, 89, 92, 9, 30, 53, 14, 96, 32, 13, 43, 37, 60, 75, 7, 83, 68, 20, 8, -24, -80, -27, -92, -96, -20, -16, -52, -49, -38 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 9, 72, 34, 29, -49, -22, -77, -17, -66, -75, -44, -30, -24 }, ExpectedResult = -1 )]
+    public int ArraySignTest( int[] nums ) =>
+        new SignOfTheProductOfAnArray().ArraySign( nums );
+}
