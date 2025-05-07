4521ee3bb7420090f9ae5efe000784bf3f6a098e
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-07 23:42:56 +0200

    #520: Detect Capital; solution & tests

---
+++ c-sharp/Problems/arrays-and-hashing/DetectCapital.cs
@@ -0,0 +1,64 @@
+﻿/**
+ * 520
+ * Detect Capital
+ **
+ * We define the usage of capitals in a word to be right
+ * when one of the following cases holds:
+ *   • All letters in this word are capitals, like "USA".
+ *   • All letters in this word are not capitals, like "leetcode".
+ *   • Only the first letter in this word is capital, like "Google".
+ *
+ * Given a string word,
+ * return true if the usage of capitals in it is right.
+ *
+ * Example 1:
+ *   Input: word = "USA"
+ *   Output: true
+ *
+ * Example 2:
+ *   Input: word = "FlaG"
+ *   Output: false
+ *
+ * Constraints:
+ *   • 1 <= word.length <= 100
+ *   • word consists of lowercase and uppercase English letters.
+ **
+ * https://leetcode.com/problems/detect-capital/
+***/
+
+namespace Problems;
+
+public class DetectCapital
+{
+    public bool DetectCapitalUse( string word )
+    {
+        bool isAllCapitals = char.IsUpper( word[0] );
+        bool isAllNotCapitals = char.IsLower( word[0] );
+        bool isFirstOnlyCapital = char.IsUpper( word[0] );
+
+        for ( int i = 1; i < word.Length; i++ )
+        {
+            if ( isAllCapitals && char.IsLower( word[i] ) )
+            {
+                isAllCapitals = false;
+            }
+
+            if ( isAllNotCapitals && char.IsUpper( word[i] ) )
+            {
+                isAllNotCapitals = false;
+            }
+
+            if ( isFirstOnlyCapital && char.IsUpper( word[i] ) )
+            {
+                isFirstOnlyCapital = false;
+            }
+
+            if ( !isAllCapitals && !isAllNotCapitals && !isFirstOnlyCapital )
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/DetectCapitalTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class DetectCapitalTests
+{
+    [TestCase( "USA", ExpectedResult = true )]
+    [TestCase( "FlaG", ExpectedResult = false )]
+    public bool DetectCapitalUseTest( string word ) =>
+        new DetectCapital().DetectCapitalUse( word );
+}
