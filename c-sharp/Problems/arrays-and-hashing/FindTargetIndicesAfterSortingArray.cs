828863fe8e34fe57ca6061775ff5dc4089d3fc86
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-14 00:46:43 +0200

    #2089: Find Target Indices After Sorting Array; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/FindTargetIndicesAfterSortingArray.cs
@@ -0,0 +1,79 @@
+﻿/**
+ * 2089
+ * Find Target Indices After Sorting Array
+ **
+ * You are given a 0-indexed integer array nums and a target element target.
+ * A target index is an index i such that nums[i] == target.
+ *
+ * Return a list of the target indices of nums
+ * after sorting nums in non-decreasing order.
+ * If there are no target indices, return an empty list.
+ * The returned list must be sorted in increasing order.
+ *
+ * Example 1:
+ *   Input: nums = [1,2,5,2,3], target = 2
+ *   Output: [1,2]
+ *   Explanation:
+ *     After sorting, nums is [1,2,2,3,5].
+ *     The indices where nums[i] == 2 are 1 and 2.
+ *
+ * Example 2:
+ *   Input: nums = [1,2,5,2,3], target = 3
+ *   Output: [3]
+ *   Explanation:
+ *     After sorting, nums is [1,2,2,3,5].
+ *     The index where nums[i] == 3 is 3.
+ *
+ * Example 3:
+ *   Input: nums = [1,2,5,2,3], target = 5
+ *   Output: [4]
+ *   Explanation:
+ *     After sorting, nums is [1,2,2,3,5].
+ *     The index where nums[i] == 5 is 4.
+ *
+ * Constraints:
+ *   • 1 <= nums.length <= 100
+ *   • 1 <= nums[i], target <= 100
+ *
+ * Hint 1:
+ *   Try "sorting" the array first.
+ *
+ * Hint 2:
+ *   Now find all indices in the array whose values are equal to target.
+ **
+ * https://leetcode.com/problems/find-target-indices-after-sorting-array/
+***/
+
+using System.Collections.Generic;
+
+namespace Problems;
+
+public class FindTargetIndicesAfterSortingArray
+{
+    public IList<int> TargetIndices( int[] nums, int target )
+    {
+        int lessThanCount = 0;
+        int targetCount = 0;
+
+        foreach ( int num in nums )
+        {
+            if ( num < target )
+            {
+                lessThanCount++;
+            }
+            else if ( num == target )
+            {
+                targetCount++;
+            }
+        }
+
+        int[] result = new int[targetCount];
+
+        for ( int i = 0; i < targetCount; i++ )
+        {
+            result[i] = lessThanCount + i;
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/FindTargetIndicesAfterSortingArrayTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class FindTargetIndicesAfterSortingArrayTests
+{
+    [TestCase( new int[] { 1, 2, 5, 2, 3 }, 2, ExpectedResult = new int[] { 1, 2 } )]
+    [TestCase( new int[] { 1, 2, 5, 2, 3 }, 3, ExpectedResult = new int[] { 3 } )]
+    [TestCase( new int[] { 1, 2, 5, 2, 3 }, 5, ExpectedResult = new int[] { 4 } )]
+    public IList<int> TargetIndicesTest( int[] nums, int target ) =>
+        new FindTargetIndicesAfterSortingArray().TargetIndices( nums, target );
+}
