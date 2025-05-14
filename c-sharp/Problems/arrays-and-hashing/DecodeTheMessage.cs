0071e5a29b936d452a7c0b252e4ff0d71d238f1c
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-14 18:17:39 +0200

    #2325: Decode the Message; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/DecodeTheMessage.cs
@@ -0,0 +1,86 @@
+﻿/**
+ * 2325
+ * Decode the Message
+ **
+ * You are given the strings key and message,
+ * which represent a cipher key and a secret message, respectively.
+ *
+ * The steps to decode message are as follows:
+ *   1. Use the first appearance of all 26 lowercase English letters
+ *      in key as the order of the substitution table.
+ *   2. Align the substitution table with the regular English alphabet.
+ *   3. Each letter in message is then substituted using the table.
+ *   4. Spaces ' ' are transformed to themselves.
+ *
+ * For example,
+ * given key = "happy boy"
+ * (actual key would have at least one instance of each letter in the alphabet),
+ * we have the partial substitution table of
+ * ('h' -> 'a', 'a' -> 'b', 'p' -> 'c', 'y' -> 'd', 'b' -> 'e', 'o' -> 'f').
+ * Return the decoded message.
+ *
+ * Example 1:
+ *   Input: key = "the quick brown fox jumps over the lazy dog", message = "vkbs bs t suepuv"
+ *   Output: "this is a secret"
+ *   Explanation:
+ *     The diagram above shows the substitution table.
+ *     It is obtained by taking the first appearance of each letter
+ *     in "the quick brown fox jumps over the lazy dog".
+ *
+ * Example 2:
+ *   Input: key = "eljuxhpwnyrdgtqkviszcfmabo", message = "zwx hnfx lqantp mnoeius ycgk vcnjrdb"
+ *   Output: "the five boxing wizards jump quickly"
+ *   Explanation:
+ *     The diagram above shows the substitution table.
+ *     It is obtained by taking the first appearance of each letter in "eljuxhpwnyrdgtqkviszcfmabo".
+ *
+ * Constraints:
+ *   • 26 <= key.length <= 2000
+ *   • key consists of lowercase English letters and ' '.
+ *   • key contains every letter in the English alphabet ('a' to 'z') at least once.
+ *   • 1 <= message.length <= 2000
+ *   • message consists of lowercase English letters and ' '.
+ *
+ * Hint 1:
+ *   Iterate through the characters in the key to construct a mapping to the English alphabet.
+ *
+ * Hint 2:
+ *   Make sure to check that the current character is not already in the mapping (only the first appearance is considered).
+ *
+ * Hint 3:
+ *   Map the characters in the message according to the constructed mapping.
+ **
+ * https://leetcode.com/problems/decode-the-message/
+***/
+
+using System;
+
+namespace Problems;
+
+public class DecodeTheMessage
+{
+    public string DecodeMessage( string key, string message )
+    {
+        char[] map = new char[123];
+        map[' '] = ' ';
+
+        char mapIndex = 'a';
+
+        foreach ( char c in key )
+        {
+            if ( map[c] == 0 )
+            {
+                map[c] = mapIndex++;
+            }
+        }
+
+        char[] result = new char[message.Length];
+
+        for ( int i = 0; i < message.Length; i++ )
+        {
+            result[i] = map[message[i]];
+        }
+
+        return new String( result );
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/DecodeTheMessageTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class DecodeTheMessageTests
+{
+    [TestCase( "the quick brown fox jumps over the lazy dog", "vkbs bs t suepuv", ExpectedResult = "this is a secret" )]
+    [TestCase( "eljuxhpwnyrdgtqkviszcfmabo", "zwx hnfx lqantp mnoeius ycgk vcnjrdb", ExpectedResult = "the five boxing wizards jump quickly" )]
+    public string DecodeMessageTest( string key, string message ) =>
+        new DecodeTheMessage().DecodeMessage( key, message );
+}
