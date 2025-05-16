aa91ff3385ef98249625e7b1a10c938ff5fc40c8
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-16 18:55:41 +0200

    #2124: Check if All A's Appears Before All B's; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/CheckIfAllAAppearsBeforeAllB.cs
@@ -0,0 +1,64 @@
+﻿/**
+ * 2124
+ * Check if All A's Appears Before All B's
+ **
+ * Given a string s consisting of only the characters 'a' and 'b',
+ * return true if every 'a' appears before every 'b' in the string.
+ * Otherwise, return false.
+ *
+ * Example 1:
+ *   Input: s = "aaabbb"
+ *   Output: true
+ *   Explanation:
+ *     The 'a's are at indices 0, 1, and 2,
+ *     while the 'b's are at indices 3, 4, and 5.
+ *     Hence, every 'a' appears before every 'b' and
+ *     we return true.
+ *
+ * Example 2:
+ *   Input: s = "abab"
+ *   Output: false
+ *   Explanation:
+ *     There is an 'a' at index 2 and a 'b' at index 1.
+ *     Hence, not every 'a' appears before every 'b' and
+ *     we return false.
+ *
+ * Example 3:
+ *   Input: s = "bbb"
+ *   Output: true
+ *   Explanation:
+ *     There are no 'a's, hence, every 'a' appears before every 'b' and
+ *     we return true.
+ *
+ * Constraints:
+ *   • 1 <= s.length <= 100
+ *   • s[i] is either 'a' or 'b'.
+ *
+ * Hint 1:
+ *   You can check the opposite:
+ *     check if there is a ‘b’ before an ‘a’.
+ *     Then, negate and return that answer.
+ *
+ * Hint 2:
+ *   s should not have any occurrences of “ba” as a substring.
+ **
+ * https://leetcode.com/problems/check-if-all-as-appears-before-all-bs/
+***/
+
+namespace Problems;
+
+public class CheckIfAllAAppearsBeforeAllB
+{
+    public bool CheckString( string s )
+    {
+        for ( int i = 1; i < s.Length; i++ )
+        {
+            if ( s[i - 1] == 'b' && s[i] == 'a' )
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/CheckIfAllAAppearsBeforeAllBTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CheckIfAllAAppearsBeforeAllBTests
+{
+    [TestCase( "aaabbb", ExpectedResult = true )]
+    [TestCase( "abab", ExpectedResult = false )]
+    [TestCase( "bbb", ExpectedResult = true )]
+    [TestCase( "aaa", ExpectedResult = true )]
+    public bool CheckStringTest( string s ) =>
+        new CheckIfAllAAppearsBeforeAllB().CheckString( s );
+}
