546457922ec9bace33805a4d5ae7148eb8caf886
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-19 17:13:32 +0200

    #3487: Maximum Unique Subarray Sum After Deletion; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/MaximumUniqueSubarraySumAfterDeletionTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MaximumUniqueSubarraySumAfterDeletionTests
+{
+    [TestCase( new int[] { 1, 2, 3, 4, 5 }, ExpectedResult = 15 )]
+    [TestCase( new int[] { 1, 1, 0, 1, 1 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 1, 2, -1, -2, 1, 0, -1 }, ExpectedResult = 3 )]
+    public int MaxSumTest( int[] nums ) =>
+        new MaximumUniqueSubarraySumAfterDeletion().MaxSum( nums );
+}
