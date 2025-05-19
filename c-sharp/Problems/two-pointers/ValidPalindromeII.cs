4fb4b3716f07233d8df7a0d1e75ea42ec111ba53
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-19 16:46:48 +0200

    #680: Valid Palindrome II; solution & tests

---
+++ c-sharp/Problems/two-pointers/ValidPalindromeII.cs
@@ -0,0 +1,71 @@
+﻿/**
+ * 680
+ * Valid Palindrome II
+ **
+ * Given a string s,
+ * return true if the s can be palindrome
+ * after deleting at most one character from it.
+ *
+ * Example 1:
+ *   Input: s = "aba"
+ *   Output: true
+ *
+ * Example 2:
+ *   Input: s = "abca"
+ *   Output: true
+ *   Explanation: You could delete the character 'c'.
+ *
+ * Example 3:
+ *   Input: s = "abc"
+ *   Output: false
+ *
+ * Constraints:
+ *   • 1 <= s.length <= 10^5
+ *   • s consists of lowercase English letters.
+ **
+ * https://leetcode.com/problems/valid-palindrome-ii/
+***/
+
+using System;
+
+namespace Problems;
+
+public class ValidPalindromeII
+{
+    public bool ValidPalindrome( ReadOnlySpan<char> s )
+    {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while ( left < right )
+        {
+            if ( s[left] != s[right] )
+            {
+                return IsPalindrome( s, left + 1, right ) || IsPalindrome( s, left, right - 1 );
+            }
+            else
+            {
+                left++;
+                right--;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool IsPalindrome( ReadOnlySpan<char> s, int left, int right )
+    {
+        while ( left < right )
+        {
+            if ( s[left] != s[right] )
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
---
+++ c-sharp/Tests/two-pointers/ValidPalindromeIITests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ValidPalindromeIITests
+{
+    [TestCase( "aba", ExpectedResult = true )]
+    [TestCase( "abca", ExpectedResult = true )]
+    [TestCase( "abc", ExpectedResult = false )]
+    [TestCase( "lcupuupucul", ExpectedResult = true )]
+    [TestCase( "adffdslcupuupuculsdffda", ExpectedResult = true )]
+    [TestCase( "deddde", ExpectedResult = true )]
+    public bool ValidPalindromeTest( string s ) =>
+        new ValidPalindromeII().ValidPalindrome( s );
+}
