6997456a7480fe8a36fdc08b2f6c75c1748308fe
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-20 18:20:19 +0200

    #1805: Number of Different Integers in a String; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/NumberOfDifferentIntegersInAStringTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class NumberOfDifferentIntegersInAStringTests
+{
+    [TestCase( "a123bc34d8ef34", ExpectedResult = 3 )]
+    [TestCase( "leet1234code234", ExpectedResult = 2 )]
+    [TestCase( "a1b01c001", ExpectedResult = 1 )]
+    public int NumDifferentIntegersTest( string word ) =>
+        new NumberOfDifferentIntegersInAString().NumDifferentIntegers( word );
+}
