c5ddb19c81e0d9352bc7d1e71a275bccab4d5080
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-21 19:21:49 +0200

    #1784: Check if Binary String Has at Most One Segment of Ones; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/CheckIfBinaryStringHasAtMostOneSegmentOfOnes.cs
@@ -0,0 +1,50 @@
+﻿/**
+ * 1784
+ * Check if Binary String Has at Most One Segment of Ones
+ **
+ * Given a binary string s ​​​​​without leading zeros,
+ * return true​​​ if s contains at most one contiguous segment of ones.
+ * Otherwise, return false.
+ *
+ * Example 1:
+ *   Input: s = "1001"
+ *   Output: false
+ *   Explanation: The ones do not form a contiguous segment.
+ *
+ * Example 2:
+ *   Input: s = "110"
+ *   Output: true
+ *
+ * Constraints:
+ *   • 1 <= s.length <= 100
+ *   • s[i]​​​​ is either '0' or '1'.
+ *   • s[0] is '1'.
+ *
+ * Hint 1:
+ *   It's guaranteed to have at least one segment
+ *
+ * Hint 2:
+ *   The string size is small
+ *   so you can count all segments of ones
+ *   with no that have no adjacent ones.
+ **
+ * https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/
+***/
+
+namespace Problems;
+
+public class CheckIfBinaryStringHasAtMostOneSegmentOfOnes
+{
+    public bool CheckOnesSegment( string s )
+    {
+        for ( int i = 2; i < s.Length; i++ )
+        {
+            if ( s[i - 1] == '0' && s[i] == '1' )
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/CheckIfBinaryStringHasAtMostOneSegmentOfOnesTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CheckIfBinaryStringHasAtMostOneSegmentOfOnesTests
+{
+    [TestCase( "1001", ExpectedResult = false )]
+    [TestCase( "110", ExpectedResult = true )]
+    public bool CheckOnesSegmentTest( string s ) =>
+        new CheckIfBinaryStringHasAtMostOneSegmentOfOnes().CheckOnesSegment( s );
+}
