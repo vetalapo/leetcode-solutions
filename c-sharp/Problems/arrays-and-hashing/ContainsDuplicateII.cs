12fb337af6d4fe0103b27640935feb5a74523f69
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-23 18:03:42 +0200

    #219: Contains Duplicate II; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/ContainsDuplicateII.cs
@@ -0,0 +1,54 @@
+﻿/**
+ * 219
+ * Contains Duplicate II
+ **
+ * Given an integer array nums and an integer k,
+ * return true if there are two distinct indices i and j in the array
+ * such that nums[i] == nums[j] and abs(i - j) <= k.
+ *
+ * Example 1:
+ *   Input: nums = [1,2,3,1], k = 3
+ *   Output: true
+ *
+ * Example 2:
+ *   Input: nums = [1,0,1,1], k = 1
+ *   Output: true
+ *
+ * Example 3:
+ *   Input: nums = [1,2,3,1,2,3], k = 2
+ *   Output: false
+ *
+ * Constraints:
+ *   • 1 <= nums.length <= 10^5
+ *   • -10^9 <= nums[i] <= 10^9
+ *   • 0 <= k <= 10^5
+ **
+ * https://leetcode.com/problems/contains-duplicate-ii/
+***/
+
+using System.Collections.Generic;
+
+namespace Problems;
+
+public class ContainsDuplicateII
+{
+    public bool ContainsNearbyDuplicate( int[] nums, int k )
+    {
+        Dictionary<int, int> map = [];
+
+        for ( int i = 0; i < nums.Length; i++ )
+        {
+            if ( map.TryGetValue( nums[i], out int j ) )
+            {
+                if ( i - j <= k )
+                {
+                    return true;
+                }
+            }
+
+            map[nums[i]] = i;
+        }
+
+        return false;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/ContainsDuplicateIITests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ContainsDuplicateIITests
+{
+    [TestCase( new int[] { 1, 2, 3, 1 }, 3, ExpectedResult = true )]
+    [TestCase( new int[] { 1, 0, 1, 1 }, 1, ExpectedResult = true )]
+    [TestCase( new int[] { 1, 2, 3, 1, 2, 3 }, 2, ExpectedResult = false )]
+    public bool ContainsNearbyDuplicateTest( int[] nums, int k ) =>
+        new ContainsDuplicateII().ContainsNearbyDuplicate( nums, k );
+}
