5351d58cdcc21e6efbb62da0842d31731399f2cc
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-25 23:56:06 +0200

    #2723: improve formatting

--- javascript/src/2723-add-two-promises.js
+++ javascript/src/2723-add-two-promises.js
@@ -34,7 +34,7 @@
  * @param {Promise} promise1
  * @param {Promise} promise2
  * @return {Promise}
- */
+***/
 var addTwoPromises = async function(promise1, promise2) {
     return await Promise.all([promise1, promise2]).then(([pv1, pv2]) => pv1 + pv2);
 };
