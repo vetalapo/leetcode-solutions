e1f20a9be8c62751a91a15d6d665b1a3c507f672
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-26 23:57:19 +0200

    #1909: Remove One Element to Make the Array Strictly Increasing; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/RemoveOneElementToMakeTheArrayStrictlyIncreasingTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class RemoveOneElementToMakeTheArrayStrictlyIncreasingTests
+{
+    [TestCase( new int[] { 1, 2, 10, 5, 7 }, ExpectedResult = true )]
+    [TestCase( new int[] { 2, 3, 1, 2 }, ExpectedResult = false )]
+    [TestCase( new int[] { 1, 1, 1 }, ExpectedResult = false )]
+    [TestCase( new int[] { 105, 924, 32, 968 }, ExpectedResult = true )]
+    public bool CanBeIncreasingTest( int[] nums ) =>
+        new RemoveOneElementToMakeTheArrayStrictlyIncreasing().CanBeIncreasing( nums );
+}
