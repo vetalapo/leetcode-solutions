29501a252d320b8192ede46d5bd47d6726428cab
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-27 00:21:45 +0200

    #2042: Check if Numbers Are Ascending in a Sencence; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/CheckIfNumbersAreAscendingInASentenceTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CheckIfNumbersAreAscendingInASentenceTests
+{
+    [TestCase( "1 box has 3 blue 4 red 6 green and 12 yellow marbles", ExpectedResult = true )]
+    [TestCase( "hello world 5 x 5", ExpectedResult = false )]
+    [TestCase( "sunset is at 7 51 pm overnight lows will be in the low 50 and 60 s", ExpectedResult = false )]
+    public bool AreNumbersAscendingTest( string s ) =>
+        new CheckIfNumbersAreAscendingInASentence().AreNumbersAscending( s );
+}
