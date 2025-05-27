c628e17b3c7a70023d78c69febba15577e0b8f46
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-27 19:07:28 +0200

    #605: Can Place Flowers; solution & tests.

---
+++ c-sharp/Tests/greedy/CanPlaceFlowersSolutionTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CanPlaceFlowersSolutionTests
+{
+    [TestCase( new int[] { 1, 0, 0, 0, 1 }, 1, ExpectedResult = true )]
+    [TestCase( new int[] { 1, 0, 0, 0, 1 }, 2, ExpectedResult = false )]
+    [TestCase( new int[] { 1, 0, 0, 0, 0, 1 }, 2, ExpectedResult = false )]
+    [TestCase( new int[] { 1, 0, 0, 0, 0, 0, 0, 0, 1 }, 3, ExpectedResult = true )]
+    [TestCase( new int[] { 0, 0, 1, 0, 1 }, 1, ExpectedResult = true )]
+    public bool CanPlaceFlowersTest( int[] flowerbed, int n ) =>
+        new CanPlaceFlowersSolution().CanPlaceFlowers( flowerbed, n );
+}
