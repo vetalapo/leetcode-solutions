d9773ce6fa4d518972d96a97d5c452b5592ac26e
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-28 15:31:06 +0200

    #434: Number of Segments in a String; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/NumberOfSegmentsInAString.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * 434
+ * Number of Segments in a String
+ **
+ * Given a string s,
+ * return the number of segments in the string.
+ * A segment is defined to be a contiguous sequence of non-space characters.
+ *
+ * Example 1:
+ *   Input: s = "Hello, my name is John"
+ *   Output: 5
+ *   Explanation:
+ *     The five segments are ["Hello,", "my", "name", "is", "John"]
+ *
+ * Example 2:
+ *   Input: s = "Hello"
+ *   Output: 1
+ *
+ * Constraints:
+ *   • 0 <= s.length <= 300
+ *   • s consists of lowercase and uppercase English letters, digits,
+ *     or one of the following characters "!@#$%^&*()_+-=',.:".
+ *   • The only space character in s is ' '.
+ **
+ * https://leetcode.com/problems/number-of-segments-in-a-string/
+***/
+
+namespace Problems;
+
+public class NumberOfSegmentsInAString
+{
+    public int CountSegments( string s )
+    {
+        int count = 0;
+
+        for ( int i = 0; i < s.Length; i++ )
+        {
+            if ( s[i] != ' ' )
+            {
+                count++;
+                i++;
+
+                while ( i < s.Length && s[i] != ' ' )
+                {
+                    i++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/NumberOfSegmentsInAStringTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class NumberOfSegmentsInAStringTests
+{
+    [TestCase( "Hello, my name is John", ExpectedResult = 5 )]
+    [TestCase( "Hello", ExpectedResult = 1 )]
+    [TestCase( "                ", ExpectedResult = 0 )]
+    public int CountSegmentsTest( string s ) =>
+        new NumberOfSegmentsInAString().CountSegments( s );
+}
