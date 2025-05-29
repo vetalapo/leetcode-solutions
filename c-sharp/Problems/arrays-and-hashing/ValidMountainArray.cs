3656056657e44a64421b5497b6c4be5eefdbc455
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-29 17:36:03 +0200

    #941: Valid Mountain Array; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/ValidMountainArray.cs
@@ -0,0 +1,67 @@
+﻿/**
+ * 941
+ * Valid Mountain Array
+ **
+ * Given an array of integers arr,
+ * return true if and only if it is a valid mountain array.
+ *
+ * Recall that arr is a mountain array if and only if:
+ *   • arr.length >= 3
+ *   • There exists some i with 0 < i < arr.length - 1 such that:
+ *     - arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+ *     - arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+ *
+ * Example 1:
+ *   Input: arr = [2,1]
+ *   Output: false
+ *
+ * Example 2:
+ *   Input: arr = [3,5,5]
+ *   Output: false
+ *
+ * Example 3:
+ *   Input: arr = [0,3,2,1]
+ *   Output: true
+ *
+ * Constraints:
+ *   • 1 <= arr.length <= 10^4
+ *   • 0 <= arr[i] <= 10^4
+ *
+ * Hint 1:
+ *   It's very easy to keep track of
+ *   a monotonically increasing or decreasing ordering of elements.
+ *   You just need to be able to determine
+ *   the start of the valley in the mountain and from that point onwards,
+ *   it should be a valley i.e. no mini-hills after that.
+ *   Use this information in regards to the values in the array
+ *   and you will be able to come up with a straightforward solution.
+ **
+ * https://leetcode.com/problems/valid-mountain-array/
+***/
+
+namespace Problems;
+
+public class ValidMountainArraySolution
+{
+    public bool ValidMountainArray( int[] arr )
+    {
+        if ( arr.Length < 3 || arr[0] > arr[1] )
+        {
+            return false;
+        }
+
+        bool peak = false;
+
+        for ( int i = 1; i < arr.Length; i++ )
+        {
+            if ( arr[i - 1] == arr[i] || ( peak && arr[i - 1] < arr[i] ) )
+            {
+                return false;
+            }
+
+            peak = arr[i - 1] > arr[i];
+        }
+
+        return peak;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/ValidMountainArraySolutionTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ValidMountainArraySolutionTests
+{
+    [TestCase( new int[] { 2, 1 }, ExpectedResult = false )]
+    [TestCase( new int[] { 3, 5, 5 }, ExpectedResult = false )]
+    [TestCase( new int[] { 0, 3, 2, 1 }, ExpectedResult = true )]
+    [TestCase( new int[] { 2, 1, 2, 3, 5, 7, 9, 10, 12, 14, 15, 16, 18, 14, 13 }, ExpectedResult = false )]
+    [TestCase( new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, ExpectedResult = false )]
+    public bool ValidMountainArrayTest( int[] arr ) =>
+        new ValidMountainArraySolution().ValidMountainArray( arr );
+}
