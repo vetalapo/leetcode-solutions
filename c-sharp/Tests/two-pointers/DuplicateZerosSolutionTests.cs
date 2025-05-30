6a9471264ec41fd38f4117dab310a7c0c4627e94
Vitaly Dushkin <vetalapo@gmail.com>
2025-05-30 23:04:24 +0200

    #1089: Duplicate Zeros; solution & tests.

---
+++ c-sharp/Tests/two-pointers/DuplicateZerosSolutionTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class DuplicateZerosSolutionTests
+{
+    [TestCase( new int[] { 1, 0, 2, 3, 0, 4, 5, 0 }, ExpectedResult = new int[] { 1, 0, 0, 2, 3, 0, 0, 4 } )]
+    [TestCase( new int[] { 1, 2, 3 }, ExpectedResult = new int[] { 1, 2, 3 } )]
+    [TestCase( new int[] { 0, 0, 0, 0, 0, 0, 0 }, ExpectedResult = new int[] { 0, 0, 0, 0, 0, 0, 0 } )]
+    [TestCase( new int[] { 1, 5, 2, 0, 6, 8, 0, 6, 0 }, ExpectedResult = new int[] { 1, 5, 2, 0, 0, 6, 8, 0, 0 } )]
+    public int[] DuplicateZerosTest( int[] arr )
+    {
+        new DuplicateZerosSolution().DuplicateZeros( arr );
+
+        return arr;
+    }
+}
