18225ab3ab88b4374334b3150eff1bc8857ff065
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-01 01:08:39 +0200

    #3120: Count the Number of Special Characters I; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/CountTheNumberOfSpecialCharactersI.cs
@@ -0,0 +1,62 @@
+﻿/**
+ * 3120
+ * Count the Number of Special Characters I
+ **
+ * You are given a string word.
+ * A letter is called special
+ * if it appears both in lowercase and uppercase in word.
+ *
+ * Return the number of special letters in word.
+ *
+ * Example 1:
+ *   Input: word = "aaAbcBC"
+ *   Output: 3
+ *   Explanation:
+ *     The special characters in word are 'a', 'b', and 'c'.
+ *
+ * Example 2:
+ *   Input: word = "abc"
+ *   Output: 0
+ *   Explanation:
+ *     No character in word appears in uppercase.
+ *
+ * Example 3:
+ *   Input: word = "abBCab"
+ *   Output: 1
+ *   Explanation:
+ *     The only special character in word is 'b'.
+ *
+ * Constraints:
+ *   • 1 <= word.length <= 50
+ *   • word consists of only lowercase and uppercase English letters.
+ *
+ * Hint 1:
+ *   The constraints are small.
+ *   For all 52 characters,
+ *   check if they are present in word.
+ **
+ * https://leetcode.com/problems/count-the-number-of-special-characters-i/
+***/
+
+namespace Problems;
+
+public class CountTheNumberOfSpecialCharactersI
+{
+    public int NumberOfSpecialChars( string word )
+    {
+        int count = 0;
+        bool[] set = new bool[123];
+
+        foreach ( char c in word )
+        {
+            if ( !set[c] && set[c ^ 32] )
+            {
+                count++;
+            }
+
+            set[c] = true;
+        }
+
+        return count;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/CountTheNumberOfSpecialCharactersITests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountTheNumberOfSpecialCharactersITests
+{
+    [TestCase( "aaAbcBC", ExpectedResult = 3 )]
+    [TestCase( "abc", ExpectedResult = 0 )]
+    [TestCase( "abBCab", ExpectedResult = 1 )]
+    public int NumberOfSpecialCharsTest( string word ) =>
+        new CountTheNumberOfSpecialCharactersI().NumberOfSpecialChars( word );
+}
