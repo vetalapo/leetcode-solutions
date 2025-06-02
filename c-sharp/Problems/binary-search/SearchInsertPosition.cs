6e97403681fa3cb72665becb1c4ebcd0a55fa194
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-02 16:20:32 +0200

    #35: Search Insert Position; solution & tests.

---
+++ c-sharp/Problems/binary-search/SearchInsertPosition.cs
@@ -0,0 +1,61 @@
+﻿/**
+ * 35
+ * Search Insert Position
+ **
+ * Given a sorted array of distinct integers and a target value,
+ * return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+ *
+ * You must write an algorithm with O(log n) runtime complexity.
+ *
+ * Example 1:
+ *   Input: nums = [1,3,5,6], target = 5
+ *   Output: 2
+ *
+ * Example 2:
+ *   Input: nums = [1,3,5,6], target = 2
+ *   Output: 1
+ *
+ * Example 3:
+ *   Input: nums = [1,3,5,6], target = 7
+ *   Output: 4
+ *
+ * Constraints:
+ *   • 1 <= nums.length <= 10^4
+ *   • -10^4 <= nums[i] <= 10^4
+ *   • nums contains distinct values sorted in ascending order.
+ *   • -10^4 <= target <= 10^4
+ **
+ * https://leetcode.com/problems/search-insert-position/
+***/
+
+namespace Problems;
+
+public class SearchInsertPosition
+{
+    public int SearchInsert( int[] nums, int target )
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while ( left <= right )
+        {
+            int middle = ( right - left ) / 2 + left;
+
+            if ( nums[middle] == target )
+            {
+                return middle;
+            }
+            else if ( nums[middle] > target )
+            {
+                right = middle - 1;
+            }
+            else
+            {
+                left = middle + 1;
+            }
+        }
+
+        return left;
+    }
+}
---
+++ c-sharp/Tests/binary-search/SearchInsertPositionTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SearchInsertPositionTests
+{
+    [TestCase( new int[] { 1, 3, 5, 6 }, 5, ExpectedResult = 2 )]
+    [TestCase( new int[] { 1, 3, 5, 6 }, 2, ExpectedResult = 1 )]
+    [TestCase( new int[] { 1, 3, 5, 6 }, 7, ExpectedResult = 4 )]
+    [TestCase( new int[] { 1, 3 }, 2, ExpectedResult = 1 )]
+    public int SearchInsertTest( int[] nums, int target ) =>
+        new SearchInsertPosition().SearchInsert( nums, target );
+}
