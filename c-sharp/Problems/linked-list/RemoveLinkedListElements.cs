6424d5d26bea19b3b207f3f5931966530eb270d5
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-03 16:08:29 +0200

    #203: Remove Linked List Elements; solution & tests.

---
+++ c-sharp/Problems/linked-list/RemoveLinkedListElements.cs
@@ -0,0 +1,57 @@
+﻿/**
+ * 203
+ * Remove Linked List Elements
+ **
+ * Given the head of a linked list and an integer val,
+ * remove all the nodes of the linked list
+ * that has Node.val == val,
+ * and return the new head.
+ *
+ * Example 1:
+ *   Input: head = [1,2,6,3,4,5,6], val = 6
+ *   Output: [1,2,3,4,5]
+ *
+ * Example 2:
+ *   Input: head = [], val = 1
+ *   Output: []
+ *
+ * Example 3:
+ *   Input: head = [7,7,7,7], val = 7
+ *   Output: []
+ *
+ * Constraints:
+ *   • The number of nodes in the list is in the range [0, 104].
+ *   • 1 <= Node.val <= 50
+ *   • 0 <= val <= 50
+ **
+ * https://leetcode.com/problems/remove-linked-list-elements/
+***/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+***/
+
+namespace Problems;
+
+public class RemoveLinkedListElements
+{
+    public ListNode RemoveElements( ListNode head, int val )
+    {
+        if ( head is null )
+        {
+            return head;
+        }
+
+        head.next = RemoveElements( head.next, val );
+
+        return head.val == val ? head.next : head;
+    }
+}
---
+++ c-sharp/Tests/linked-list/SolutionTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class RemoveLinkedListElementsTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public ListNode RemoveElementsTest( ListNode head, int val ) =>
+        new RemoveLinkedListElements().RemoveElements( head, val );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( ListNode.Create( [1, 2, 6, 3, 4, 5, 6] ), 6 )
+                .Returns( ListNode.Create( [1, 2, 3, 4, 5] ) );
+
+            yield return new TestCaseData( ListNode.Create( [] ), 1 )
+                .Returns( ListNode.Create( [] ) );
+
+            yield return new TestCaseData( ListNode.Create( [7, 7, 7, 7] ), 7 )
+                .Returns( ListNode.Create( [] ) );
+
+            yield return new TestCaseData( ListNode.Create( [1, 2, 2, 1] ), 2 )
+                .Returns( ListNode.Create( [1, 1] ) );
+        }
+    }
+}
