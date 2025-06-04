5aa53c7526c5884e2e1b403d0dc86a8a0c93e0eb
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-05 00:45:55 +0200

    #3174: Clear Digits; solution & tests.

---
+++ c-sharp/Tests/stack/ClearDigitsSolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class ClearDigitsSolutionTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public string ClearDigitsTest( string s ) =>
+        new ClearDigitsSolution().ClearDigits( s );
+
+    [TestCaseSource( nameof( TestCases ) )]
+    public string ClearDigitsStackTest( string s ) =>
+        new ClearDigitsSolution().ClearDigitsStack( s );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( "abc" ).Returns( "abc" );
+
+            yield return new TestCaseData( "cb34" ).Returns( string.Empty );
+
+            yield return new TestCaseData( "a0id5" ).Returns( "i" );
+        }
+    }
+}
