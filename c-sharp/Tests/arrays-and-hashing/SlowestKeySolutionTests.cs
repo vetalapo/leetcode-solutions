3e3e360b63b71f0c15816aa6f1f0b5f12f5081dd
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-05 18:39:24 +0200

    #1629: Slowest Key; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/SlowestKeySolutionTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SlowestKeySolutionTests
+{
+    [TestCase( new int[] { 9, 29, 49, 50 }, "cbcd", ExpectedResult = 'c' )]
+    [TestCase( new int[] { 12, 23, 36, 46, 62 }, "spuda", ExpectedResult = 'a' )]
+    public char SlowestKeyTest( int[] releaseTimes, string keysPressed ) =>
+        new SlowestKeySolution().SlowestKey( releaseTimes, keysPressed );
+}
