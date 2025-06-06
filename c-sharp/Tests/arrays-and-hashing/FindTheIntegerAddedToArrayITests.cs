8fd33fe41495930bb6873a44265c1f99b0013ff7
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-06 17:48:31 +0200

    #3131: Find the Integer Added to Array I; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/FindTheIntegerAddedToArrayITests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FindTheIntegerAddedToArrayITests
+{
+    [TestCase( new int[] { 2, 6, 4 }, new int[] { 9, 7, 5 }, ExpectedResult = 3 )]
+    [TestCase( new int[] { 10 }, new int[] { 5 }, ExpectedResult = -5 )]
+    [TestCase( new int[] { 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1 }, ExpectedResult = 0 )]
+    public int AddedIntegerTest( int[] nums1, int[] nums2 ) =>
+        new FindTheIntegerAddedToArrayI().AddedInteger( nums1, nums2 );
+}
