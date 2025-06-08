6345aaa697e4c293861494e4f83b358c0b396ee7
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-08 19:41:54 +0200

    2129: Capitalize the Title; solution & tests.

---
+++ c-sharp/Problems/two-pointers/CapitalizeTheTitle.cs
@@ -0,0 +1,92 @@
+﻿/**
+ * 2129
+ * Capitalize the Title
+ **
+ * You are given a string title
+ * consisting of one or more words separated by a single space,
+ * where each word consists of English letters.
+ * Capitalize the string by changing the capitalization of each word such that:
+ *   • If the length of the word is 1 or 2 letters, change all letters to lowercase.
+ *   • Otherwise, change the first letter to uppercase and the remaining letters to lowercase.
+ *
+ * Return the capitalized title.
+ *
+ * Example 1:
+ *   Input: title = "capiTalIze tHe titLe"
+ *   Output: "Capitalize The Title"
+ *   Explanation:
+ *     Since all the words have a length of at least 3,
+ *     the first letter of each word is uppercase,
+ *     and the remaining letters are lowercase.
+ *
+ * Example 2:
+ *   Input: title = "First leTTeR of EACH Word"
+ *   Output: "First Letter of Each Word"
+ *   Explanation:
+ *     The word "of" has length 2, so it is all lowercase.
+ *     The remaining words have a length of at least 3,
+ *     so the first letter of each remaining word is uppercase,
+ *     and the remaining letters are lowercase.
+ *
+ * Example 3:
+ *   Input: title = "i lOve leetcode"
+ *   Output: "i Love Leetcode"
+ *   Explanation:
+ *     The word "i" has length 1, so it is lowercase.
+ *     The remaining words have a length of at least 3,
+ *     so the first letter of each remaining word is uppercase,
+ *     and the remaining letters are lowercase.
+ *
+ * Constraints:
+ *   • 1 <= title.length <= 100
+ *   • title consists of words separated by a single space without any leading or trailing spaces.
+ *   • Each word consists of uppercase and lowercase English letters and is non-empty.
+ *
+ * Hint 1:
+ *   Firstly, try to find all the words present in the string.
+ *
+ * Hint 2:
+ *   On the basis of each word's lengths,
+ *   simulate the process explained in Problem.
+ **
+ * https://leetcode.com/problems/capitalize-the-title/
+***/
+
+using System;
+
+namespace Problems;
+
+public class CapitalizeTheTitle
+{
+    public string CapitalizeTitle( string title )
+    {
+        char[] result = new char[title.Length];
+        int left = 0;
+
+        for ( int right = 0; right < result.Length; right++ )
+        {
+            if ( title[right] == ' ' )
+            {
+                result[right] = title[right];
+
+                if ( right - left > 2 )
+                {
+                    result[left] = Char.ToUpper( title[left] );
+                }
+
+                left = right + 1;
+            }
+            else
+            {
+                result[right] = Char.ToLower( title[right] );
+            }
+        }
+
+        if ( result.Length - left > 2 )
+        {
+            result[left] = Char.ToUpper( title[left] );
+        }
+
+        return new String( result );
+    }
+}
---
+++ c-sharp/Tests/two-pointers/CapitalizeTheTitleTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CapitalizeTheTitleTests
+{
+    [TestCase( "capiTalIze tHe titLe", ExpectedResult = "Capitalize The Title" )]
+    [TestCase( "First leTTeR of EACH Word", ExpectedResult = "First Letter of Each Word" )]
+    [TestCase( "i lOve leetcode", ExpectedResult = "i Love Leetcode" )]
+    [TestCase( "L hV", ExpectedResult = "l hv" )]
+    [TestCase( "ZW Cl pyR uoC", ExpectedResult = "zw cl Pyr Uoc" )]
+    public string CapitalizeTitleTest( string title ) =>
+        new CapitalizeTheTitle().CapitalizeTitle( title );
+}
