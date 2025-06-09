c2ced79a41da36f73febd4a1645ecc278b5e08ad
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-09 22:58:33 +0200

    #3541: Find Most Frequent Vowel and Consonant; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/FindMostFrequentVowelAndConsonant.cs
@@ -0,0 +1,90 @@
+﻿/**
+ * 3541
+ * Find Most Frequent Vowel and Consonant
+ **
+ * You are given a string s consisting of lowercase English letters ('a' to 'z').
+ *
+ * Your task is to:
+ *   • Find the vowel (one of 'a', 'e', 'i', 'o', or 'u') with the maximum frequency.
+ *   • Find the consonant (all other letters excluding vowels) with the maximum frequency.
+ *
+ * Return the sum of the two frequencies.
+ *
+ * Note:
+ *   If multiple vowels or consonants have the same maximum frequency,
+ *   you may choose any one of them.
+ *   If there are no vowels or no consonants in the string,
+ *   consider their frequency as 0.
+ *
+ * The frequency of a letter x is the number of times it occurs in the string.
+ *
+ * Example 1:
+ *   Input: s = "successes"
+ *   Output: 6
+ *   Explanation:
+ *     The vowels are: 'u' (frequency 1), 'e' (frequency 2).
+ *     The maximum frequency is 2.
+ *     The consonants are: 's' (frequency 4), 'c' (frequency 2).
+ *     The maximum frequency is 4.
+ *     The output is 2 + 4 = 6.
+ *
+ * Example 2:
+ *   Input: s = "aeiaeia"
+ *   Output: 3
+ *   Explanation:
+ *     The vowels are: 'a' (frequency 3), 'e' ( frequency 2), 'i' (frequency 2).
+ *     The maximum frequency is 3.
+ *     There are no consonants in s.
+ *     Hence, maximum consonant frequency = 0.
+ *     The output is 3 + 0 = 3.
+ *
+ * Constraints:
+ *   • 1 <= s.length <= 100
+ *   • s consists of lowercase English letters only.
+ *
+ * Hint 1:
+ *   Use a hashmap
+ *
+ * Hint 2:
+ *   Simulate as described
+ **
+ * https://leetcode.com/problems/find-most-frequent-vowel-and-consonant/
+***/
+
+using System;
+
+namespace Problems;
+
+public class FindMostFrequentVowelAndConsonant
+{
+    private static readonly bool[] vowelsMap = [
+        true, false, false, false, true, false, false, false, true,
+        false, false, false, false, false, true, false, false, false,
+        false, false, true, false, false, false, false, false
+    ];
+
+    public int MaxFreqSum( string s )
+    {
+        int[] freqMap = new int[26];
+        int maxVowelFreq = 0;
+        int maxConsonantFreq = 0;
+
+        foreach ( char c in s )
+        {
+            int i = c - 'a';
+
+            freqMap[i]++;
+
+            if ( vowelsMap[i] )
+            {
+                maxVowelFreq = Math.Max( maxVowelFreq, freqMap[i] );
+            }
+            else
+            {
+                maxConsonantFreq = Math.Max( maxConsonantFreq, freqMap[i] );
+            }
+        }
+
+        return maxVowelFreq + maxConsonantFreq;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/FindMostFrequentVowelAndConsonantTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+public class FindMostFrequentVowelAndConsonantTests
+{
+    [TestCase( "successes", ExpectedResult = 6 )]
+    [TestCase( "aeiaeia", ExpectedResult = 3 )]
+    [TestCase( "cz", ExpectedResult = 1 )]
+    public int MaxFreqSumTest( string s ) =>
+        new FindMostFrequentVowelAndConsonant().MaxFreqSum( s );
+}
