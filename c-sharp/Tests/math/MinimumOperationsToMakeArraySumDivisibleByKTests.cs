118b8bf39e51c0b166071974874615e0b168f4bb
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-10 15:19:44 +0200

    #3512: Minimum Operations to Make Array Sum Divisible by K; solution & tests.

---
+++ c-sharp/Tests/math/MinimumOperationsToMakeArraySumDivisibleByKTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MinimumOperationsToMakeArraySumDivisibleByKTests
+{
+    [TestCase( new int[] { 3, 9, 7 }, 5, ExpectedResult = 4 )]
+    [TestCase( new int[] { 4, 1, 3 }, 4, ExpectedResult = 0 )]
+    [TestCase( new int[] { 3, 2 }, 6, ExpectedResult = 5 )]
+    public int MinOperationsTest( int[] nums, int k ) =>
+        new MinimumOperationsToMakeArraySumDivisibleByK().MinOperations( nums, k );
+}
