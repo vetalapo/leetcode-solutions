2ded4595929d8cd155d11fcd8ec26c35fbdc0a4c
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-10 16:01:58 +0200

    #1832: improve formatting

--- c-sharp/Problems/arrays-and-hashing/CheckIfTheSentenceIsPangram.cs
+++ c-sharp/Problems/arrays-and-hashing/CheckIfTheSentenceIsPangram.cs
@@ -2,9 +2,11 @@
  * 1832
  * Check if the Sentence Is Pangram
  **
- * A pangram is a sentence where every letter of the English alphabet appears at least once.
+ * A pangram is a sentence
+ * where every letter of the English alphabet appears at least once.
  * Given a string sentence containing only lowercase English letters,
- * return true if sentence is a pangram, or false otherwise.
+ * return true if sentence is a pangram,
+ * or false otherwise.
  *
  * Example 1:
  *   Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
