b221e0180c372df2892ef190f29af004980f8be2
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-10 15:51:36 +0200

    #2283: improve formatting

--- c-sharp/Problems/arrays-and-hashing/CheckIfNumberHasEqualDigitCountAndDigitValue.cs
+++ c-sharp/Problems/arrays-and-hashing/CheckIfNumberHasEqualDigitCountAndDigitValue.cs
@@ -15,7 +15,8 @@
  *     num[1] = '2'. The digit 1 occurs twice in num.
  *     num[2] = '1'. The digit 2 occurs once in num.
  *     num[3] = '0'. The digit 3 occurs zero times in num.
- *     The condition holds true for every index in "1210", so return true.
+ *     The condition holds true for every index in "1210",
+ *     so return true.
  *
  * Example 2:
  *   Input: num = "030"
