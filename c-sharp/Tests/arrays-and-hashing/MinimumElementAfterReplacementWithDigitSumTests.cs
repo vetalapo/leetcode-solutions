11069eb87d716781e6cb53a2138e9efd10bb97b7
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-11 18:44:16 +0200

    #3300: Minimum Element After Replacement With Digit Sum; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/MinimumElementAfterReplacementWithDigitSumTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MinimumElementAfterReplacementWithDigitSumTests
+{
+    [TestCase( new int[] { 10, 12, 13, 14 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 1, 2, 3, 4 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 999, 19, 199 }, ExpectedResult = 10 )]
+    public int MinElementTest( int[] nums ) =>
+        new MinimumElementAfterReplacementWithDigitSum().MinElement( nums );
+}
