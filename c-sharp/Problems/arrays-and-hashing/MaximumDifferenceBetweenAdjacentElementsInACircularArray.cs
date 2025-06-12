ba7ef6c1cb2819ef4fd8ea7d2c7fad2a3ad0e043
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-12 11:58:02 +0200

    #3423: Maximum Difference Between Adjacent Elements in a Cirular Array; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/MaximumDifferenceBetweenAdjacentElementsInACircularArray.cs
@@ -0,0 +1,59 @@
+﻿/**
+ * 3423
+ * Maximum Difference Between Adjacent Elements in a Circular Array
+ **
+ * Given a circular array nums,
+ * find the maximum absolute difference between adjacent elements.
+ *
+ * Note:
+ *   In a circular array,
+ *   the first and last elements are adjacent.
+ *
+ * Example 1:
+ *   Input: nums = [1,2,4]
+ *   Output: 3
+ *   Explanation:
+ *     Because nums is circular,
+ *     nums[0] and nums[2] are adjacent.
+ *     They have the maximum absolute difference of |4 - 1| = 3.
+ *
+ * Example 2:
+ *   Input: nums = [-5,-10,-5]
+ *   Output: 5
+ *   Explanation:
+ *     The adjacent elements nums[0] and nums[1]
+ *     have the maximum absolute difference of |-5 - (-10)| = 5.
+ *
+ * Constraints:
+ *   • 2 <= nums.length <= 100
+ *   • -100 <= nums[i] <= 100
+ *
+ * Hint 1:
+ *   Traverse from the second element to the last element
+ *   and check the difference of every adjacent pair.
+ *
+ * Hint 2:
+ *   The edge case is to check the difference between
+ *   the first and last elements.
+ **
+ * https://leetcode.com/problems/maximum-difference-between-adjacent-elements-in-a-circular-array/
+***/
+
+using System;
+
+namespace Problems;
+
+public class MaximumDifferenceBetweenAdjacentElementsInACircularArray
+{
+    public int MaxAdjacentDistance( int[] nums )
+    {
+        int max = Math.Abs( nums[0] - nums[^1] );
+
+        for ( int i = 1; i < nums.Length; i++ )
+        {
+            max = Math.Max( max, Math.Abs( nums[i - 1] - nums[i] ) );
+        }
+
+        return max;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/MaximumDifferenceBetweenAdjacentElementsInACircularArrayTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MaximumDifferenceBetweenAdjacentElementsInACircularArrayTests
+{
+    [TestCase( new int[] { 1, 2, 4 }, ExpectedResult = 3 )]
+    [TestCase( new int[] { -5, -10, -5 }, ExpectedResult = 5 )]
+    public int MaxAdjacentDistanceTest( int[] nums ) =>
+        new MaximumDifferenceBetweenAdjacentElementsInACircularArray().MaxAdjacentDistance( nums );
+}
