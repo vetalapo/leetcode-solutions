f29abb6fe96d9ab8b5cf056d17ccc61e1bbe9466
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-13 15:39:28 +0200

    #2951: Find the Peaks; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/FindThePeaksTests.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class FindThePeaksTests
+{
+    [TestCase( new int[] { 2, 4, 4 }, ExpectedResult = new int[] { } )]
+    [TestCase( new int[] { 1, 4, 3, 8, 5 }, ExpectedResult = new int[] { 1, 3 } )]
+    public IList<int> FindPeaksTest( int[] mountain ) =>
+        new FindThePeaks().FindPeaks( mountain );
+}
