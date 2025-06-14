8831784ff0ec9de6238838767a69c0a4d7625663
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-15 01:27:07 +0200

    #2404: Most Frequent Even Element; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/MostFrequentEvenElementTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MostFrequentEvenElementTests
+{
+    [TestCase( new int[] { 0, 1, 2, 2, 4, 4, 1 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { 4, 4, 4, 9, 2, 4 }, ExpectedResult = 4 )]
+    [TestCase( new int[] { 29, 47, 21, 41, 13, 37, 25, 7 }, ExpectedResult = -1 )]
+    public int MostFrequentEvenTest( int[] nums ) =>
+        new MostFrequentEvenElement().MostFrequentEven( nums );
+}
