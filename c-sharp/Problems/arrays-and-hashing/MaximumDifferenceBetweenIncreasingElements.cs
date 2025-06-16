2e0a2a5ee18657157d072edd025c1a0b5e47ade8
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-16 17:31:42 +0200

    #2016: Maximum Difference Between Increasing Elements; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/MaximumDifferenceBetweenIncreasingElements.cs
@@ -0,0 +1,77 @@
+﻿/**
+ * 2016
+ * Maximum Difference Between Increasing Elements
+ **
+ * Given a 0-indexed integer array nums of size n,
+ * find the maximum difference between nums[i] and nums[j] (i.e., nums[j] - nums[i]),
+ * such that 0 <= i < j < n and nums[i] < nums[j].
+ *
+ * Return the maximum difference.
+ * If no such i and j exists, return -1.
+ *
+ * Example 1:
+ *   Input: nums = [7,1,5,4]
+ *   Output: 4
+ *   Explanation:
+ *     The maximum difference occurs
+ *     with i = 1 and j = 2, nums[j] - nums[i] = 5 - 1 = 4.
+ *     Note that with i = 1 and j = 0,
+ *     the difference nums[j] - nums[i] = 7 - 1 = 6,
+ *     but i > j, so it is not valid.
+ *
+ * Example 2:
+ *   Input: nums = [9,4,3,2]
+ *   Output: -1
+ *   Explanation:
+ *     There is no i and j such that
+ *     i < j and nums[i] < nums[j].
+ *
+ * Example 3:
+ *   Input: nums = [1,5,2,10]
+ *   Output: 9
+ *   Explanation:
+ *     The maximum difference occurs
+ *     with i = 0 and j = 3,
+ *     nums[j] - nums[i] = 10 - 1 = 9.
+ *
+ * Constraints:
+ *   • n == nums.length
+ *   • 2 <= n <= 1000
+ *   • 1 <= nums[i] <= 10^9
+ *
+ * Hint 1:
+ *   Could you keep track of the minimum element visited while traversing?
+ *
+ * Hint 2:
+ *   We have a potential candidate for the answer
+ *   if the prefix min is lesser than nums[i].
+ **
+ * https://leetcode.com/problems/maximum-difference-between-increasing-elements/
+***/
+
+using System;
+
+namespace Problems;
+
+public class MaximumDifferenceBetweenIncreasingElements
+{
+    public int MaximumDifference( int[] nums )
+    {
+        int maxDiff = -1;
+        int min = nums[0];
+
+        for ( int i = 1; i < nums.Length; i++ )
+        {
+            if ( nums[i] > min )
+            {
+                maxDiff = Math.Max( maxDiff, nums[i] - min );
+            }
+            else
+            {
+                min = nums[i];
+            }
+        }
+
+        return maxDiff;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/MaximumDifferenceBetweenIncreasingElementsTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MaximumDifferenceBetweenIncreasingElementsTests
+{
+    [TestCase( new int[] { 7, 1, 5, 4 }, ExpectedResult = 4 )]
+    [TestCase( new int[] { 9, 4, 3, 2 }, ExpectedResult = -1 )]
+    [TestCase( new int[] { 1, 5, 2, 10 }, ExpectedResult = 9 )]
+    [TestCase( new int[] { 999, 997, 980, 976, 735, 45, 26, 26, 23, 15 }, ExpectedResult = -1 )]
+    public int MaximumDifferenceTest( int[] nums ) =>
+        new MaximumDifferenceBetweenIncreasingElements().MaximumDifference( nums );
+}
