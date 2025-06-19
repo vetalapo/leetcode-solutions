40c202cd188cbb9eb65539fc321f687b4a41718d
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-19 23:24:43 +0200

    #3550: Smallest Index With Digit Sum Equal to Index; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/SmallestIndexWithDigitSumEqualToIndexTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SmallestIndexWithDigitSumEqualToIndexTests
+{
+    [TestCase( new int[] { 1, 3, 2 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { 1, 10, 11 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 1, 2, 3 }, ExpectedResult = -1 )]
+    public int SmallestIndexTest( int[] nums ) =>
+        new SmallestIndexWithDigitSumEqualToIndex().SmallestIndex( nums );
+}
