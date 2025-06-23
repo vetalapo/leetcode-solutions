2082b889d08020af209ea5446e271867820b4ccc
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-23 18:25:41 +0200

    #3065: Minimum Operations to Exceed Threshold Value I

---
+++ c-sharp/Tests/arrays-and-hashing/MinimumOperationsToExceedThresholdValueITests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MinimumOperationsToExceedThresholdValueITests
+{
+    [TestCase( new int[] { 2, 11, 10, 1, 3 }, 10, ExpectedResult = 3 )]
+    [TestCase( new int[] { 1, 1, 2, 4, 9 }, 1, ExpectedResult = 0 )]
+    [TestCase( new int[] { 1, 1, 2, 4, 9 }, 9, ExpectedResult = 4 )]
+    public int MinOperationsTest( int[] nums, int k ) =>
+        new MinimumOperationsToExceedThresholdValueI().MinOperations( nums, k );
+}
