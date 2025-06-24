e1b818895b3a5b3140da5567b05b45d614770ca7
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-24 17:36:48 +0200

    #1313: Decompress Run-Length Encoded List; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/DecompressRunLengthEncodedListTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class DecompressRunLengthEncodedListTests
+{
+    [TestCase( new int[] { 1, 2, 3, 4 }, ExpectedResult = new int[] { 2, 4, 4, 4 } )]
+    [TestCase( new int[] { 1, 1, 2, 3 }, ExpectedResult = new int[] { 1, 3, 3 } )]
+    public int[] DecompressRLElistTest( int[] nums ) =>
+        new DecompressRunLengthEncodedList().DecompressRLElist( nums );
+}
