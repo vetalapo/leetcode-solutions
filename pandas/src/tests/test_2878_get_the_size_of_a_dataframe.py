e79f58d61b8b36752e4b2bffc2ec81bff29e4a57
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-27 18:52:18 +0200

    #2878: Get the Size of a DataFrame; solution & tests

---
+++ pandas/src/tests/test_2878_get_the_size_of_a_dataframe.py
@@ -0,0 +1,29 @@
+﻿import sys
+
+# Preventing creation of __pycache__
+sys.dont_write_bytecode = True
+
+import importlib
+import unittest
+import pandas as pd
+
+# Python really doesn't like paths with "-" and "/"
+# I don't like Python not liking it so
+# have to use importlib to import modules
+unit = importlib.import_module("src.2878-get-the-size-of-a-dataframe")
+
+df = pd.DataFrame({
+    "player_id": [846, 749, 155, 583, 388, 883, 355, 247, 761, 642],
+    "name": ["Mason", "Riley", "Bob", "Isabella", "Zachary", "Ava", "Violet", "Thomas", "Jack", "Charlie"],
+    "age": [21, 30, 38, 32, 24, 23, 18, 27, 33, 36],
+    "position": ["Forward", "Winger", "Striker", "Goalkeeper", "Midfielder", "Defender", "Striker", "Striker", "Midfielder", "Center-back"],
+    "team": ["RealMadrid", "Barcelona", "ManchesterUnited", "Liverpool", "BayernMunich", "Chelsea", "Juventus", "ParisSaint-Germain", "ManchesterCity", "Arsenal"]
+})
+
+class TestConcatenateTables(unittest.TestCase):
+    def test_getDataframeSize(self):
+        unitResult = unit.getDataframeSize(df)
+        self.assertListEqual(unitResult, [10, 5])
+
+if __name__ == "__main__":
+    unittest.main()
