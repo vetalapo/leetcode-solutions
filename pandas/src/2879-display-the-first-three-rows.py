f905ff41d9c6b1fb0b7ae2b06c1e4774c8d31f40
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-28 20:47:25 +0200

    #2879: improve formatting

--- pandas/src/2879-display-the-first-three-rows.py
+++ pandas/src/2879-display-the-first-three-rows.py
@@ -16,29 +16,29 @@
 # 
 # Example 1:
 #   Input:
-#   DataFrame employees
-#   +-------------+-----------+-----------------------+--------+
-#   | employee_id | name      | department            | salary |
-#   +-------------+-----------+-----------------------+--------+
-#   | 3           | Bob       | Operations            | 48675  |
-#   | 90          | Alice     | Sales                 | 11096  |
-#   | 9           | Tatiana   | Engineering           | 33805  |
-#   | 60          | Annabelle | InformationTechnology | 37678  |
-#   | 49          | Jonathan  | HumanResources        | 23793  |
-#   | 43          | Khaled    | Administration        | 40454  |
-#   +-------------+-----------+-----------------------+--------+
+#     DataFrame employees
+#     +-------------+-----------+-----------------------+--------+
+#     | employee_id | name      | department            | salary |
+#     +-------------+-----------+-----------------------+--------+
+#     | 3           | Bob       | Operations            | 48675  |
+#     | 90          | Alice     | Sales                 | 11096  |
+#     | 9           | Tatiana   | Engineering           | 33805  |
+#     | 60          | Annabelle | InformationTechnology | 37678  |
+#     | 49          | Jonathan  | HumanResources        | 23793  |
+#     | 43          | Khaled    | Administration        | 40454  |
+#     +-------------+-----------+-----------------------+--------+
 #
 #   Output:
-#   +-------------+---------+-------------+--------+
-#   | employee_id | name    | department  | salary |
-#   +-------------+---------+-------------+--------+
-#   | 3           | Bob     | Operations  | 48675  |
-#   | 90          | Alice   | Sales       | 11096  |
-#   | 9           | Tatiana | Engineering | 33805  |
-#   +-------------+---------+-------------+--------+
+#     +-------------+---------+-------------+--------+
+#     | employee_id | name    | department  | salary |
+#     +-------------+---------+-------------+--------+
+#     | 3           | Bob     | Operations  | 48675  |
+#     | 90          | Alice   | Sales       | 11096  |
+#     | 9           | Tatiana | Engineering | 33805  |
+#     +-------------+---------+-------------+--------+
 #
 #   Explanation: 
-#   Only the first 3 rows are displayed.
+#     Only the first 3 rows are displayed.
 ##
 # https://leetcode.com/problems/display-the-first-three-rows/
 ##
