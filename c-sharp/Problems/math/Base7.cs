54579070d3be95735128e7c43bf2468cfec77361
Vitaly Dushkin <vetalapo@gmail.com>
2025-06-29 23:58:36 +0200

    #504: Base7; solution & tests.

---
+++ c-sharp/Problems/math/Base7.cs
@@ -0,0 +1,47 @@
+﻿/**
+ * 504
+ * Base 7
+ **
+ * Given an integer num, return a string of its base 7 representation.
+ *
+ * Example 1:
+ *   Input: num = 100
+ *   Output: "202"
+ *
+ * Example 2:
+ *   Input: num = -7
+ *   Output: "-10"
+ *
+ * Constraints:
+ *   -10^7 <= num <= 10^7
+ **
+ * https://leetcode.com/problems/base-7/
+***/
+
+using System;
+
+namespace Problems;
+
+public class Base7
+{
+    public string ConvertToBase7( int num )
+    {
+        if ( num == 0 )
+        {
+            return "0";
+        }
+
+        bool isNegative = num < 0;
+        num = Math.Abs( num );
+
+        string result = string.Empty;
+
+        while ( num != 0 )
+        {
+            result = ( num % 7 ).ToString() + result;
+            num /= 7;
+        }
+
+        return isNegative ? $"-{result}" : result;
+    }
+}
---
+++ c-sharp/Tests/math/Base7Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class Base7Tests
+{
+    [TestCase( 100, ExpectedResult = "202" )]
+    [TestCase( -7, ExpectedResult = "-10" )]
+    [TestCase( 0, ExpectedResult = "0" )]
+    [TestCase( -8, ExpectedResult = "-11" )]
+    public string ConvertToBase7Test( int num ) =>
+        new Base7().ConvertToBase7( num );
+}
