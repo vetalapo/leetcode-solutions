918a1a04eacebdec640d8ca0f224d9a92f9d7aff
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-01 15:16:07 +0200

    #3330: Find the Original Typed String I; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/FindTheOriginalTypedStringITests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FindTheOriginalTypedStringITests
+{
+    [TestCase( "abbcccc", ExpectedResult = 5 )]
+    [TestCase( "abcd", ExpectedResult = 1 )]
+    [TestCase( "aaaa", ExpectedResult = 4 )]
+    public int PossibleStringCountTest( string word ) =>
+        new FindTheOriginalTypedStringI().PossibleStringCount( word );
+}
