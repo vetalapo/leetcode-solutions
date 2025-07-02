02080623e32487a689763640dc56f4b2d2d38b06
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-02 20:55:27 +0200

    #806: Number of Lines To Write String; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/NumberOfLinesToWriteStringTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class NumberOfLinesToWriteStringTests
+{
+    [TestCase( new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 }, "abcdefghijklmnopqrstuvwxyz", ExpectedResult = new int[] { 3, 60 } )]
+    [TestCase( new int[] { 4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 }, "bbbcccdddaaa", ExpectedResult = new int[] { 2, 4 } )]
+    public int[] NumberOfLinesTest( int[] widths, string s ) =>
+        new NumberOfLinesToWriteString().NumberOfLines( widths, s );
+}
