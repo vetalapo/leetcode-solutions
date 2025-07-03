d3c7f4e78568d0c0bff66a04a100475c163955b3
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-03 23:44:18 +0200

    #3304: Find the K-th Character in String Game I; solution & tests.

---
+++ c-sharp/Problems/math/FindTheKThCharacterInStringGameI.cs
@@ -0,0 +1,71 @@
+﻿/**
+ * 3304
+ * Find the K-th Character in String Game I
+ **
+ * Alice and Bob are playing a game.
+ * Initially,
+ * Alice has a string word = "a".
+ *
+ * You are given a positive integer k.
+ *
+ * Now Bob will ask Alice to perform the following operation forever:
+ *   • Generate a new string
+ *     by changing each character in word
+ *     to its next character in the English alphabet,
+ *     and append it to the original word.
+ *
+ * For example,
+ * performing the operation on "c" generates "cd"
+ * and performing the operation on "zb" generates "zbac".
+ *
+ * Return the value of the kth character in word,
+ * after enough operations have been done for word
+ * to have at least k characters.
+ *
+ * Note that the character 'z' can be changed to 'a' in the operation.
+ *
+ * Example 1:
+ *   Input: k = 5
+ *   Output: "b"
+ *   Explanation:
+ *     Initially, word = "a". We need to do the operation three times:
+ *     Generated string is "b", word becomes "ab".
+ *     Generated string is "bc", word becomes "abbc".
+ *     Generated string is "bccd", word becomes "abbcbccd".
+ *
+ * Example 2:
+ *   Input: k = 10
+ *   Output: "c"
+ *
+ * Constraints:
+ *   • 1 <= k <= 500
+ *
+ * Hint 1:
+ *   The constraints are small.
+ *   Construct the string by simulating the operations.
+ **
+ * https://leetcode.com/problems/find-the-k-th-character-in-string-game-i/
+***/
+
+namespace Problems;
+
+public class FindTheKThCharacterInStringGameI
+{
+    public char KthCharacter( int k )
+    {
+        return (char)( 'a' + PopCount( k - 1 ) );
+    }
+
+    private int PopCount( int num )
+    {
+        int count = 0;
+
+        while ( num > 0 )
+        {
+            count += ( num & 1 );
+            num >>= 1;
+        }
+
+        return count;
+    }
+}
---
+++ c-sharp/Tests/math/FindTheKThCharacterInStringGameITests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FindTheKThCharacterInStringGameITests
+{
+    [TestCase( 5, ExpectedResult = 'b' )]
+    [TestCase( 10, ExpectedResult = 'c' )]
+    public char KthCharacterTest( int k ) =>
+        new FindTheKThCharacterInStringGameI().KthCharacter( k );
+}
