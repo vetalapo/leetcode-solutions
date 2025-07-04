86b93ad1b784a8964351d0d0f4314b867cad8f92
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-05 01:32:54 +0200

    #2923: Find Champion I; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/FindChampionITests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class FindChampionITests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int FindChampionTest( int[][] grid ) =>
+        new FindChampionI().FindChampion( grid );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( [new int[][] { [0, 1], [0, 0] }] )
+                .Returns( 0 );
+
+            yield return new TestCaseData( [new int[][] { [0, 0, 1], [1, 0, 1], [0, 0, 0] }] )
+                .Returns( 1 );
+        }
+    }
+}
