0b324d630265ea7e26670e4242af319ea07c5717
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-06 20:27:03 +0200

    #2432: The Employee That Worked on the Longest Task; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/TheEmployeeThatWorkedOnTheLongestTask.cs
@@ -0,0 +1,108 @@
+﻿/**
+ * 2432
+ * The Employee That Worked on the Longest Task
+ **
+ * There are n employees,
+ * each with a unique id from 0 to n - 1.
+ *
+ * You are given a 2D integer array logs
+ * where logs[i] = [id^i, leaveTime^i] where:
+ *   • idi is the id of the employee that worked on the ith task, and
+ *   • leaveTime^i is the time at which the employee finished the ith task.
+ *     All the values leaveTime^i are unique.
+ *
+ * Note that the ith task starts the moment right after the (i - 1)th task ends,
+ * and the 0th task starts at time 0.
+ *
+ * Return the id of the employee
+ * that worked the task with the longest time.
+ * If there is a tie between two or more employees,
+ * return the smallest id among them.
+ *
+ * Example 1:
+ *   Input:
+ *     n = 10,
+ *     logs = [[0,3],[2,5],[0,9],[1,15]]
+ *   Output: 1
+ *   Explanation:
+ *     - Task 0 started at 0 and ended at 3 with 3 units of times.
+ *     - Task 1 started at 3 and ended at 5 with 2 units of times.
+ *     - Task 2 started at 5 and ended at 9 with 4 units of times.
+ *     - Task 3 started at 9 and ended at 15 with 6 units of times.
+ *     The task with the longest time is task 3 and the employee with id 1
+ *     is the one that worked on it, so we return 1.
+ *
+ * Example 2:
+ *   Input:
+ *     n = 26,
+ *     logs = [[1,1],[3,7],[2,12],[7,17]]
+ *   Output: 3
+ *   Explanation:
+ *     - Task 0 started at 0 and ended at 1 with 1 unit of times.
+ *     - Task 1 started at 1 and ended at 7 with 6 units of times.
+ *     - Task 2 started at 7 and ended at 12 with 5 units of times.
+ *     - Task 3 started at 12 and ended at 17 with 5 units of times.
+ *     The tasks with the longest time is task 1.
+ *     The employee that worked on it is 3, so we return 3.
+ *
+ * Example 3:
+ *   Input: n = 2, logs = [[0,10],[1,20]]
+ *   Output: 0
+ *   Explanation:
+ *     - Task 0 started at 0 and ended at 10 with 10 units of times.
+ *     - Task 1 started at 10 and ended at 20 with 10 units of times.
+ *     The tasks with the longest time are tasks 0 and 1.
+ *     The employees that worked on them are 0 and 1,
+ *     so we return the smallest id 0.
+ *
+ * Constraints:
+ *   • 2 <= n <= 500
+ *   • 1 <= logs.length <= 500
+ *   • logs[i].length == 2
+ *   • 0 <= id^i <= n - 1
+ *   • 1 <= leaveTime^i <= 500
+ *   • idi != id^i+1
+ *   • leaveTimei are sorted in a strictly increasing order.
+ *
+ * Hint 1:
+ *   Find the time of the longest task
+ *
+ * Hint 2:
+ *   Store each employee’s longest task time in a hash table
+ *
+ * Hint 3:
+ *   For employees that have the same longest task time,
+ *   we only need the employee with the smallest ID
+ **
+ * https://leetcode.com/problems/the-employee-that-worked-on-the-longest-task/
+***/
+
+using System;
+
+namespace Problems;
+
+public class TheEmployeeThatWorkedOnTheLongestTask
+{
+    public int HardestWorker( int n, int[][] logs )
+    {
+        int max = logs[0][1];
+        int id = logs[0][0];
+
+        for ( int i = 1; i < logs.Length; i++ )
+        {
+            int currTime = logs[i][1] - logs[i - 1][1];
+
+            if ( currTime == max )
+            {
+                id = Math.Min( id, logs[i][0] );
+            }
+            else if ( currTime > max )
+            {
+                max = currTime;
+                id = logs[i][0];
+            }
+        }
+
+        return id;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/TheEmployeeThatWorkedOnTheLongestTaskTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class TheEmployeeThatWorkedOnTheLongestTaskTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int HardestWorkerTest( int n, int[][] logs ) =>
+        new TheEmployeeThatWorkedOnTheLongestTask().HardestWorker( n, logs );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( 10, new int[][] { [0, 3], [2, 5], [0, 9], [1, 15] } )
+                .Returns( 1 );
+
+            yield return new TestCaseData( 26, new int[][] { [1, 1], [3, 7], [2, 12], [7, 17] } )
+                .Returns( 3 );
+
+            yield return new TestCaseData( 2, new int[][] { [0, 10], [1, 20] } )
+                .Returns( 0 );
+
+            yield return new TestCaseData( 70, new int[][] { [36, 3], [1, 5], [12, 8], [25, 9], [53, 11], [29, 12], [52, 14] } )
+                .Returns( 12 );
+        }
+    }
+}
