9c4e37fc4008c1a4d51b312c0950d5944d3ad8de
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-07 17:32:07 +0200

    #551: Student Attendance Record I; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/StudentAttendanceRecordITests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class StudentAttendanceRecordITests
+{
+    [TestCase( "PPALLP", ExpectedResult = true )]
+    [TestCase( "PPALLL", ExpectedResult = false )]
+    [TestCase( "AA", ExpectedResult = false )]
+    public bool CheckRecordTest( string s ) =>
+        new StudentAttendanceRecordI().CheckRecord( s );
+}
