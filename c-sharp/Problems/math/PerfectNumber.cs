23993c496c21ebd49905df744902d048b27ebc91
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-09 23:33:30 +0200

    #507: Perfect Number; solution & tests.

---
+++ c-sharp/Problems/math/PerfectNumber.cs
@@ -0,0 +1,61 @@
+﻿/**
+ * 507
+ * Perfect Number
+ **
+ * A perfect number (https://en.wikipedia.org/wiki/Perfect_number)
+ * is a positive integer that is equal to the sum of its positive divisors,
+ * excluding the number itself.
+ * A divisor of an integer x is an integer that can divide x evenly.
+ *
+ * Given an integer n,
+ * return true if n is a perfect number, otherwise return false.
+ *
+ * Example 1:
+ *   Input: num = 28
+ *   Output: true
+ *   Explanation:
+ *     28 = 1 + 2 + 4 + 7 + 14
+ *     1, 2, 4, 7, and 14 are all divisors of 28.
+ *
+ * Example 2:
+ *   Input: num = 7
+ *   Output: false
+ *
+ * Constraints:
+ *   • 1 <= num <= 10^8
+ **
+ * https://leetcode.com/problems/perfect-number/
+***/
+
+namespace Problems;
+
+public class PerfectNumber
+{
+    public bool CheckPerfectNumber( int num )
+    {
+        return num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336;
+    }
+
+    public bool CheckPerfectNumberBF( int num )
+    {
+        if ( ( num & 1 ) != 0 )
+        {
+            return false;
+        }
+
+        int divisorSum = num >> 1;
+        int tmp = divisorSum - 1;
+
+        while ( tmp > 0 )
+        {
+            if ( num % tmp == 0 )
+            {
+                divisorSum += tmp;
+            }
+
+            tmp--;
+        }
+
+        return divisorSum == num;
+    }
+}
---
+++ c-sharp/Tests/math/PerfectNumberTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class PerfectNumberTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public bool CheckPerfectNumberTest( int num ) =>
+        new PerfectNumber().CheckPerfectNumber( num );
+
+    [TestCaseSource( nameof( TestCases ) )]
+    public bool CheckPerfectNumberBFTest( int num ) =>
+        new PerfectNumber().CheckPerfectNumberBF( num );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( 28 ).Returns( true );
+
+            yield return new TestCaseData( 6 ).Returns( true );
+
+            yield return new TestCaseData( 7 ).Returns( false );
+
+            yield return new TestCaseData( 9 ).Returns( false );
+        }
+    }
+}
