931e9e2bafaad2c40f46973aea3e81645bfec93c
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-12 23:34:46 +0200

    #747: Largest Number At Least Twice of Others; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/LargestNumberAtLeastTwiceOfOthersTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class LargestNumberAtLeastTwiceOfOthersTests
+{
+    [TestCase( new int[] { 3, 6, 1, 0 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 1, 2, 3, 4 }, ExpectedResult = -1 )]
+    [TestCase( new int[] { 0, 0, 0, 1 }, ExpectedResult = 3 )]
+    public int DominantIndexTest( int[] nums ) =>
+        new LargestNumberAtLeastTwiceOfOthers().DominantIndex( nums );
+}
