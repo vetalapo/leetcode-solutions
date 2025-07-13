bea758495782ca7391cb4e3c50af3184c06b9f2f
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-13 16:17:57 +0200

    #415: Add Strings; solution & tests.

---
+++ c-sharp/Problems/math/AddStrings.cs
@@ -0,0 +1,77 @@
+﻿/**
+ * 415
+ * Add Strings
+ **
+ * Given two non-negative integers, num1 and num2 represented as string,
+ * return the sum of num1 and num2 as a string.
+ *
+ * You must solve the problem without using any built-in library
+ * for handling large integers (such as BigInteger).
+ * You must also not convert the inputs to integers directly.
+ *
+ * Example 1:
+ *   Input: num1 = "11", num2 = "123"
+ *   Output: "134"
+ *
+ * Example 2:
+ *   Input: num1 = "456", num2 = "77"
+ *   Output: "533"
+ *
+ * Example 3:
+ *   Input: num1 = "0", num2 = "0"
+ *   Output: "0"
+ *
+ * Constraints:
+ *   • 1 <= num1.length, num2.length <= 10^4
+ *   • num1 and num2 consist of only digits.
+ *   • num1 and num2 don't have any leading zeros except for the zero itself.
+ **
+ * https://leetcode.com/problems/add-strings/
+***/
+
+using System;
+
+namespace Problems;
+
+public class AddStringsSolution
+{
+    public string AddStrings( string num1, string num2 )
+    {
+        if ( num1.Length < num2.Length )
+        {
+            return AddStrings( num2, num1 );
+        }
+
+        char[] result = new char[num1.Length];
+        bool carry = false;
+
+        for ( int i = num1.Length - 1; i >= 0; i-- )
+        {
+            int a = num1[i] - '0';
+            int b = num1.Length - i - 1 < num2.Length
+                ? num2[num2.Length - 1 - ( num1.Length - i - 1 )] - '0'
+                : 0;
+
+            int sum = a + b;
+
+            if ( carry )
+            {
+                sum++;
+            }
+
+            if ( sum > 9 )
+            {
+                carry = true;
+                sum %= 10;
+            }
+            else
+            {
+                carry = false;
+            }
+
+            result[i] = (char)( '0' + sum );
+        }
+
+        return ( carry ? "1" : "" ) + new String( result );
+    }
+}
---
+++ c-sharp/Tests/math/AddStringsSolutionTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class AddStringsSolutionTests
+{
+    [TestCase( "11", "123", ExpectedResult = "134" )]
+    [TestCase( "456", "77", ExpectedResult = "533" )]
+    [TestCase( "0", "0", ExpectedResult = "0" )]
+    [TestCase( "1", "9", ExpectedResult = "10" )]
+    [TestCase( "123", "456", ExpectedResult = "579" )]
+    public string AddStringsTest( string num1, string num2 ) =>
+        new AddStringsSolution().AddStrings( num1, num2 );
+}
