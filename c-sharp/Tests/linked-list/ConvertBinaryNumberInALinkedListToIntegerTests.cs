5aa1ede00977f1e21706c73eea1f5bedcdd7f692
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-14 19:52:35 +0200

    #1290: Convert Binary Number in a Linked List to Integer; solution & tests.

---
+++ c-sharp/Tests/linked-list/ConvertBinaryNumberInALinkedListToIntegerTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class ConvertBinaryNumberInALinkedListToIntegerTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int GetDecimalValueTest( ListNode head ) =>
+        new ConvertBinaryNumberInALinkedListToInteger().GetDecimalValue( head );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( ListNode.Create( [1, 0, 1] ) ).Returns( 5 );
+
+            yield return new TestCaseData( ListNode.Create( [0] ) ).Returns( 0 );
+
+            yield return new TestCaseData( ListNode.Create( [1, 0, 0, 0] ) ).Returns( 8 );
+        }
+    }
+}
