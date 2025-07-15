3bb97cc4bcd8d65e7a8e3f05c212afd4b90056d3
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-15 19:04:03 +0200

    #2729: Check if The Number is Fascinating; solution & tests.

---
+++ c-sharp/Problems/math/CheckIfTheNumberIsFascinating.cs
@@ -0,0 +1,82 @@
+﻿/**
+ * 2729
+ * Check if The Number is Fascinating
+ **
+ * You are given an integer n that consists of exactly 3 digits.
+ *
+ * We call the number n fascinating if, after the following modification,
+ * the resulting number contains all the digits from 1 to 9 exactly once
+ * and does not contain any 0's:
+ *   • Concatenate n with the numbers 2 * n and 3 * n.
+ *
+ * Return true if n is fascinating, or false otherwise.
+ *
+ * Concatenating two numbers means joining them together.
+ * For example, the concatenation of 121 and 371 is 121371.
+ *
+ * Example 1:
+ *   Input: n = 192
+ *   Output: true
+ *   Explanation:
+ *     We concatenate the numbers n = 192 and 2 * n = 384 and 3 * n = 576.
+ *     The resulting number is 192384576.
+ *     This number contains all the digits from 1 to 9 exactly once.
+ *
+ * Example 2:
+ *   Input: n = 100
+ *   Output: false
+ *   Explanation:
+ *     We concatenate the numbers n = 100 and 2 * n = 200 and 3 * n = 300.
+ *     The resulting number is 100200300.
+ *     This number does not satisfy any of the conditions.
+ *
+ * Constraints:
+ *   • 100 <= n <= 999
+ *
+ * Hint 1:
+ *   Consider changing the number to the way it is described in the statement.
+ *
+ * Hint 2:
+ *   Check if the resulting number contains all the digits from 1 to 9 exactly once.
+ **
+ * https://leetcode.com/problems/check-if-the-number-is-fascinating/
+***/
+
+namespace Problems;
+
+public class CheckIfTheNumberIsFascinating
+{
+    public bool IsFascinating( int n )
+    {
+        if ( n > 333 )
+        {
+            return false;
+        }
+
+        int n2 = n * 2;
+        int n3 = n * 3;
+
+        int[] countMap = new int[10];
+
+        while ( n > 0 )
+        {
+            countMap[n % 10]++;
+            countMap[n2 % 10]++;
+            countMap[n3 % 10]++;
+
+            n /= 10;
+            n2 /= 10;
+            n3 /= 10;
+        }
+
+        for ( int i = 1; i < 10; i++ )
+        {
+            if ( countMap[i] != 1 )
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ c-sharp/Tests/math/CheckIfTheNumberIsFascinatingTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CheckIfTheNumberIsFascinatingTests
+{
+    [TestCase( 192, ExpectedResult = true )]
+    [TestCase( 100, ExpectedResult = false )]
+    [TestCase( 219, ExpectedResult = true )]
+    [TestCase( 250, ExpectedResult = false )]
+    [TestCase( 273, ExpectedResult = true )]
+    [TestCase( 300, ExpectedResult = false )]
+    [TestCase( 327, ExpectedResult = true )]
+    [TestCase( 999, ExpectedResult = false )]
+    public bool IsFascinatingTest( int n ) =>
+        new CheckIfTheNumberIsFascinating().IsFascinating( n );
+}
