fe4097e5a59a03546b9a6c1c5e286e610867fc39
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-16 15:35:43 +0200

    #2481: Minimum Cuts to Divide a Circle; solution & tests.

---
+++ c-sharp/Tests/math/MinimumCutsToDivideACircleTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MinimumCutsToDivideACircleTests
+{
+    [TestCase( 4, ExpectedResult = 2 )]
+    [TestCase( 3, ExpectedResult = 3 )]
+    [TestCase( 1, ExpectedResult = 0 )]
+    public int NumberOfCutsTest( int n ) =>
+        new MinimumCutsToDivideACircle().NumberOfCuts( n );
+}
