ba071e8930bbaa3b6c3aa2a42b60df3947d9dbf7
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-17 20:40:08 +0200

    #2582: Pass the Pillow; solution & tests.

---
+++ c-sharp/Tests/math/PassThePillowSolutionTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class PassThePillowSolutionTests
+{
+    [TestCase( 4, 5, ExpectedResult = 2 )]
+    [TestCase( 3, 2, ExpectedResult = 3 )]
+    [TestCase( 8, 9, ExpectedResult = 6 )]
+    [TestCase( 9, 4, ExpectedResult = 5 )]
+    [TestCase( 18, 38, ExpectedResult = 5 )]
+    [TestCase( 6, 8, ExpectedResult = 3 )]
+    [TestCase( 2, 341, ExpectedResult = 2 )]
+    [TestCase( 33, 218, ExpectedResult = 27 )]
+    public int PassThePillowTest( int n, int time ) =>
+        new PassThePillowSolution().PassThePillow( n, time );
+}
