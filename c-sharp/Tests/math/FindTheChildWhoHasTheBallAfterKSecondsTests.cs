b29187670c09813633acdbc62986f22986f5230d
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-17 21:01:01 +0200

    #3178: Find the Child Who Has the Ball After K Seconds; solution & tests.

---
+++ c-sharp/Tests/math/FindTheChildWhoHasTheBallAfterKSecondsTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FindTheChildWhoHasTheBallAfterKSecondsTests
+{
+    [TestCase( 3, 5, ExpectedResult = 1 )]
+    [TestCase( 5, 6, ExpectedResult = 2 )]
+    [TestCase( 4, 2, ExpectedResult = 2 )]
+    public int NumberOfChildTest( int n, int time ) =>
+        new FindTheChildWhoHasTheBallAfterKSeconds().NumberOfChild( n, time );
+}
