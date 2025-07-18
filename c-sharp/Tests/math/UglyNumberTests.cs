ae07ca1076769500ebcebd92a96ee6f855493e68
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-18 18:53:06 +0200

    #263: Ugly Number; solution & tests.

---
+++ c-sharp/Tests/math/UglyNumberTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class UglyNumberTests
+{
+    [TestCase( 6, ExpectedResult = true )]
+    [TestCase( 1, ExpectedResult = true )]
+    [TestCase( 14, ExpectedResult = false )]
+    [TestCase( -2147483648, ExpectedResult = false )]
+    public bool IsUglyTest( int n ) =>
+        new UglyNumber().IsUgly( n );
+}
