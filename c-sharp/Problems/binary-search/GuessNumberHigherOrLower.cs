d561926266a99a3ad853e378ec607891eb3676e9
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-20 23:56:58 +0200

    #374: Guess Number Higher or Lower; solution & tests.

---
+++ c-sharp/Problems/binary-search/GuessNumberHigherOrLower.cs
@@ -0,0 +1,98 @@
+﻿/**
+ * 374
+ * Guess Number Higher or Lower
+ **
+ * We are playing the Guess Game.
+ *
+ * The game is as follows:
+ * I pick a number from 1 to n.
+ * You have to guess which number I picked.
+ * Every time you guess wrong,
+ * I will tell you whether the number I picked
+ * is higher or lower than your guess.
+ *
+ * You call a pre-defined API int guess(int num),
+ * which returns three possible results:
+ *   • -1: Your guess is higher than the number I picked (i.e. num > pick).
+ *   • 1: Your guess is lower than the number I picked (i.e. num < pick).
+ *   • 0: your guess is equal to the number I picked (i.e. num == pick).
+ *
+ * Return the number that I picked.
+ *
+ * Example 1:
+ *   Input: n = 10, pick = 6
+ *   Output: 6
+ *
+ * Example 2:
+ *   Input: n = 1, pick = 1
+ *   Output: 1
+ *
+ * Example 3:
+ *   Input: n = 2, pick = 1
+ *   Output: 1
+ *
+ * Constraints:
+ *   • 1 <= n <= 2^31 - 1
+ *   • 1 <= pick <= n
+ **
+ * https://leetcode.com/problems/guess-number-higher-or-lower/
+***/
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+***/
+
+namespace Problems;
+
+public class GuessNumberHigherOrLower( int num ) : GuessGame( num )
+{
+    public int GuessNumber( int n )
+    {
+        int left = 1;
+
+        while ( left < n )
+        {
+            int mid = ( n - left ) / 2 + left;
+            int guessRes = guess( mid );
+
+            if ( guessRes == 0 )
+            {
+                return mid;
+            }
+            else if ( guessRes == 1 )
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                n = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
+
+public class GuessGame( int num )
+{
+    private readonly int _secretNum = num;
+
+    public int guess( int num )
+    {
+        if ( num == this._secretNum )
+        {
+            return 0;
+        }
+        else if ( num < this._secretNum )
+        {
+            return 1;
+        }
+
+        return -1;
+    }
+}
---
+++ c-sharp/Tests/binary-search/GuessNumberHigherOrLowerTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class GuessNumberHigherOrLowerTests
+{
+    [TestCase( 10, 6, ExpectedResult = 6 )]
+    [TestCase( 1, 1, ExpectedResult = 1 )]
+    [TestCase( 2, 1, ExpectedResult = 1 )]
+    public int GuessNumberTest( int n, int pick ) =>
+        new GuessNumberHigherOrLower( pick ).GuessNumber( n );
+}
