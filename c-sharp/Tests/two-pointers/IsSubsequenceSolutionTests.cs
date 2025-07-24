f42902a632411896ce9cf5a58459b563600525cb
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-24 17:30:45 +0200

    #392: Is Subsequence; solution & tests.

---
+++ c-sharp/Tests/two-pointers/IsSubsequenceSolutionTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class IsSubsequenceSolutionTests
+{
+    [TestCase( "abc", "ahbgdc", ExpectedResult = true )]
+    [TestCase( "axc", "ahbgdc", ExpectedResult = false )]
+    [TestCase( "acb", "ahbgdc", ExpectedResult = false )]
+    [TestCase( "", "ahbgdc", ExpectedResult = true )]
+    public bool IsSubsequenceTest( string s, string t ) =>
+        new IsSubsequenceSolution().IsSubsequence( s, t );
+}
