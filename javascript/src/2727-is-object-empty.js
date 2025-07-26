178b1989f1eafd962090c78ac2ac24c3b76e7530
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-26 23:39:37 +0200

    #2727: improve formatting

--- javascript/src/2727-is-object-empty.js
+++ javascript/src/2727-is-object-empty.js
@@ -11,17 +11,20 @@
  * Example 1:
  *   Input: obj = {"x": 5, "y": 42}
  *   Output: false
- *   Explanation: The object has 2 key-value pairs so it is not empty.
+ *   Explanation:
+ *     The object has 2 key-value pairs so it is not empty.
  *
  * Example 2:
  *   Input: obj = {}
  *   Output: true
- *   Explanation: The object doesn't have any key-value pairs so it is empty.
+ *   Explanation:
+ *     The object doesn't have any key-value pairs so it is empty.
  *
  * Example 3:
  *   Input: obj = [null, false, 0]
  *   Output: false
- *   Explanation: The array has 3 elements so it is not empty.
+ *   Explanation:
+ *     The array has 3 elements so it is not empty.
  *
  * Constraints:
  *   • obj is a valid JSON object or array
