d4b122f605a4b6b7f22b39bb0c99de7e6bdbdd0e
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-27 22:38:21 +0200

    #2619: Array Prototype Last.

--- javascript/src/2619-array-prototype-last.js
+++ javascript/src/2619-array-prototype-last.js
@@ -12,12 +12,14 @@
  * Example 1:
  *   Input: nums = [null, {}, 3]
  *   Output: 3
- *   Explanation: Calling nums.last() should return the last element: 3.
+ *   Explanation:
+ *     Calling nums.last() should return the last element: 3.
  *
  * Example 2:
  *   Input: nums = []
  *   Output: -1
- *   Explanation: Because there are no elements, return -1.
+ *   Explanation:
+ *     Because there are no elements, return -1.
  *
  * Constraints:
  *   • arr is a valid JSON array
