e2d3a281e4a0969abc496c13f5fd3f37402c4325
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-28 21:42:21 +0200

    #1365: How Many Numbers Are Smaller Than the Current Number; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/HowManyNumbersAreSmallerThanTheCurrentNumberTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class HowManyNumbersAreSmallerThanTheCurrentNumberTests
+{
+    [TestCase( new int[] { 8, 1, 2, 2, 3 }, ExpectedResult = new int[] { 4, 0, 1, 1, 3 } )]
+    [TestCase( new int[] { 6, 5, 4, 8 }, ExpectedResult = new int[] { 2, 1, 0, 3 } )]
+    [TestCase( new int[] { 7, 7, 7, 7 }, ExpectedResult = new int[] { 0, 0, 0, 0 } )]
+    public int[] SmallerNumbersThanCurrentTest( int[] nums ) =>
+        new HowManyNumbersAreSmallerThanTheCurrentNumber().SmallerNumbersThanCurrent( nums );
+}
