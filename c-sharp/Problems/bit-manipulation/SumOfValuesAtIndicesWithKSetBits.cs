f235fe2bb979a7ee327df782f3bd79f6dfc21704
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-29 12:19:30 +0200

    #2859: Sum of Values at Indices With K Set Bits; solution & tests.

---
+++ c-sharp/Problems/bit-manipulation/SumOfValuesAtIndicesWithKSetBits.cs
@@ -0,0 +1,91 @@
+﻿/**
+ * 2859
+ * Sum of Values at Indices With K Set Bits
+ **
+ * You are given a 0-indexed integer array nums and an integer k.
+ *
+ * Return an integer that denotes the sum of elements in nums
+ * whose corresponding indices have exactly k set bits
+ * in their binary representation.
+ *
+ * The set bits in an integer are the 1's present
+ * when it is written in binary.
+ *
+ * For example,
+ * the binary representation of 21 is 10101,
+ * which has 3 set bits.
+ *
+ * Example 1:
+ *   Input: nums = [5,10,1,5,2], k = 1
+ *   Output: 13
+ *   Explanation:
+ *     The binary representation of the indices are:
+ *       0 = 0002
+ *       1 = 0012
+ *       2 = 0102
+ *       3 = 0112
+ *       4 = 1002
+ *     Indices 1, 2, and 4 have k = 1 set bits
+ *     in their binary representation.
+ *     Hence,
+ *     the answer is nums[1] + nums[2] + nums[4] = 13.
+ *
+ * Example 2:
+ *   Input: nums = [4,3,2,1], k = 2
+ *   Output: 1
+ *   Explanation:
+ *     The binary representation of the indices are:
+ *       0 = 002
+ *       1 = 012
+ *       2 = 102
+ *       3 = 112
+ *     Only index 3 has k = 2 set bits
+ *     in its binary representation.
+ *     Hence,
+ *     the answer is nums[3] = 1.
+ *
+ * Constraints:
+ *   • 1 <= nums.length <= 1000
+ *   • 1 <= nums[i] <= 10^5
+ *   • 0 <= k <= 10
+ *
+ * Hint 1:
+ *   Iterate through the indices i in the range [0, n - 1],
+ *   for each index i count the number of bits in its binary representation.
+ *   If it is k, add nums[i] to the result.
+ **
+ * https://leetcode.com/problems/sum-of-values-at-indices-with-k-set-bits/
+***/
+
+using System.Collections.Generic;
+
+namespace Problems;
+
+public class SumOfValuesAtIndicesWithKSetBits
+{
+    public int SumIndicesWithKSetBits( IList<int> nums, int k )
+    {
+        int sum = 0;
+
+        for ( int i = ( 1 << k ) - 1; i < nums.Count; i = GetNextSamePopCount( i ) )
+        {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+
+    // Hacker's Delight book approach
+    private int GetNextSamePopCount( int num )
+    {
+        if ( num == 0 )
+        {
+            return int.MaxValue;
+        }
+
+        int c = ( num & -num );
+        int r = num + c;
+
+        return ( ( ( r ^ num ) >> 2 ) / c ) | r;
+    }
+}
---
+++ c-sharp/Tests/bit-manipulation/SumOfValuesAtIndicesWithKSetBitsTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class SumOfValuesAtIndicesWithKSetBitsTests
+{
+    [TestCase( new int[] { 5, 10, 1, 5, 2 }, 1, ExpectedResult = 13 )]
+    [TestCase( new int[] { 4, 3, 2, 1 }, 2, ExpectedResult = 1 )]
+    [TestCase( new int[] { 1 }, 0, ExpectedResult = 1 )]
+    public int SumIndicesWithKSetBitsTest( IList<int> nums, int k ) =>
+        new SumOfValuesAtIndicesWithKSetBits().SumIndicesWithKSetBits( nums, k );
+}
