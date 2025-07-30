e8fb65f55f200f3aa63634d43c6ae2ee564b50aa
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-30 16:47:02 +0200

    #2006: Count Number of Pairs With Absolute Difference K; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/CountNumberOfPairsWithAbsoluteDifferenceK.cs
@@ -0,0 +1,74 @@
+﻿/**
+ * 2006
+ * Count Number of Pairs With Absolute Difference K
+ **
+ * Given an integer array nums and an integer k,
+ * return the number of pairs (i, j) where i < j
+ * such that |nums[i] - nums[j]| == k.
+ *
+ * The value of |x| is defined as:
+ *   • x if x >= 0.
+ *   • -x if x < 0.
+ *
+ * Example 1:
+ *   Input: nums = [1,2,2,1], k = 1
+ *   Output: 4
+ *   Explanation:
+ *     The pairs with an absolute difference of 1 are:
+ *       - [1.,2.,2, 1 ]
+ *       - [1.,2, 2.,1 ]
+ *       - [1, 2.,2, 1.]
+ *       - [1, 2, 2.,1.]
+ *
+ * Example 2:
+ *   Input: nums = [1,3], k = 3
+ *   Output: 0
+ *   Explanation:
+ *     There are no pairs with an absolute difference of 3.
+ *
+ * Example 3:
+ *   Input: nums = [3,2,1,5,4], k = 2
+ *   Output: 3
+ *   Explanation:
+ *     The pairs with an absolute difference of 2 are:
+ *       - [3.,2, 1.,5, 4 ]
+ *       - [3.,2, 1, 5.,4 ]
+ *       - [3, 2.,1, 5, 4.]
+ *
+ * Constraints:
+ *   • 1 <= nums.length <= 200
+ *   • 1 <= nums[i] <= 100
+ *   • 1 <= k <= 99
+ *
+ * Hint 1:
+ *   Can we check every possible pair?
+ *
+ * Hint 2:
+ *   Can we use a nested for loop to solve this problem?
+ **
+ * https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/
+***/
+
+namespace Problems;
+
+public class CountNumberOfPairsWithAbsoluteDifferenceK
+{
+    public int CountKDifference( int[] nums, int k )
+    {
+        int[] countMap = new int[101];
+
+        foreach ( int num in nums )
+        {
+            countMap[num]++;
+        }
+
+        int count = 0;
+
+        for ( int i = k; i < countMap.Length; i++ )
+        {
+            count += countMap[i] * countMap[i - k];
+        }
+
+        return count;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/CountNumberOfPairsWithAbsoluteDifferenceKTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountNumberOfPairsWithAbsoluteDifferenceKTests
+{
+    [TestCase( new int[] { 1, 2, 2, 1 }, 1, ExpectedResult = 4 )]
+    [TestCase( new int[] { 1, 3 }, 3, ExpectedResult = 0 )]
+    [TestCase( new int[] { 3, 2, 1, 5, 4 }, 2, ExpectedResult = 3 )]
+    public int CountKDifferenceTest( int[] nums, int k ) =>
+        new CountNumberOfPairsWithAbsoluteDifferenceK().CountKDifference( nums, k );
+}
