f571398b61fb5d13141701dd3e746794b6612856
Vitaly Dushkin <vetalapo@gmail.com>
2025-07-31 19:08:54 +0200

    #1356: Sort Integers by The Number of 1 Bits; solution & tests.

---
+++ c-sharp/Tests/bit-manipulation/SortIntegersByTheNumberOf1BitsTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SortIntegersByTheNumberOf1BitsTests
+{
+    [TestCase( new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 }, ExpectedResult = new int[] { 0, 1, 2, 4, 8, 3, 5, 6, 7 } )]
+    [TestCase( new int[] { 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 }, ExpectedResult = new int[] { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 } )]
+    public int[] SortByBitsTest( int[] arr ) =>
+        new SortIntegersByTheNumberOf1Bits().SortByBits( arr );
+}
