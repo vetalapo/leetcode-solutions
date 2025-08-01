8cf8eee2811fc4ab561261a924c7886a42286ec8
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-01 18:54:49 +0200

    #118: Pascal's Triangle; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/PascalsTriangle.cs
@@ -0,0 +1,51 @@
+﻿/**
+ * 118
+ * Pascal's Triangle
+ **
+ * Given an integer numRows,
+ * return the first numRows of Pascal's triangle.
+ *
+ * In Pascal's triangle,
+ * each number is the sum of the two numbers directly above.
+ *
+ * Example 1:
+ *   Input: numRows = 5
+ *   Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+ *
+ * Example 2:
+ *   Input: numRows = 1
+ *   Output: [[1]]
+ *
+ * Constraints:
+ *   • 1 <= numRows <= 30
+ **
+ * https://leetcode.com/problems/pascals-triangle/
+***/
+
+using System.Collections.Generic;
+
+namespace Problems;
+
+public class PascalsTriangle
+{
+    public IList<IList<int>> Generate( int numRows )
+    {
+        int[][] result = new int[numRows][];
+
+        result[0] = [1];
+
+        for ( int i = 1; i < numRows; i++ )
+        {
+            result[i] = new int[i + 1];
+            result[i][0] = 1;
+            result[i][^1] = 1;
+
+            for ( int j = 1; j < i; j++ )
+            {
+                result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+            }
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/PascalsTriangleTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class PascalsTriangleTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public IList<IList<int>> GenerateTest( int numRows ) =>
+        new PascalsTriangle().Generate( numRows );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( 5 ).Returns( new int[][] { [1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1] } );
+
+            yield return new TestCaseData( 1 ).Returns( new int[][] { [1] } );
+        }
+    }
+}
