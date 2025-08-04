080e9816820d154106c0e8696bba09f7c92a6e42
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-04 15:39:47 +0200

    #1979: Find Greatest Common Divisor of Array

---
+++ c-sharp/Problems/math/FindGreatestCommonDivisorOfArray.cs
@@ -0,0 +1,85 @@
+﻿/**
+ * 1979
+ * Find Greatest Common Divisor of Array
+ **
+ * Given an integer array nums,
+ * return the greatest common divisor of the smallest number
+ * and largest number in nums.
+ *
+ * The greatest common divisor of two numbers
+ * is the largest positive integer that evenly divides both numbers.
+ *
+ * Example 1:
+ *   Input: nums = [2,5,6,9,10]
+ *   Output: 2
+ *   Explanation:
+ *     The smallest number in nums is 2.
+ *     The largest number in nums is 10.
+ *     The greatest common divisor of 2 and 10 is 2.
+ *
+ * Example 2:
+ *   Input: nums = [7,5,6,8,3]
+ *   Output: 1
+ *   Explanation:
+ *     The smallest number in nums is 3.
+ *     The largest number in nums is 8.
+ *     The greatest common divisor of 3 and 8 is 1.
+ *
+ * Example 3:
+ *   Input: nums = [3,3]
+ *   Output: 3
+ *   Explanation:
+ *     The smallest number in nums is 3.
+ *     The largest number in nums is 3.
+ *     The greatest common divisor of 3 and 3 is 3.
+ *
+ * Constraints:
+ *   • 2 <= nums.length <= 1000
+ *   • 1 <= nums[i] <= 1000
+ *
+ * Hint 1:
+ *   Find the minimum and maximum in one iteration.
+ *   Let them be mn and mx.
+ *
+ * Hint 2:
+ *   Try all the numbers in the range [1, mn]
+ *   and check the largest number which divides both of them.
+ **
+ * https://leetcode.com/problems/find-greatest-common-divisor-of-array/
+***/
+
+using System;
+
+namespace Problems;
+
+public class FindGreatestCommonDivisorOfArray
+{
+    public int FindGCD( int[] nums )
+    {
+        int min = nums[0];
+        int max = nums[0];
+
+        for ( int i = 1; i < nums.Length; i++ )
+        {
+            min = Math.Min( min, nums[i] );
+            max = Math.Max( max, nums[i] );
+        }
+
+        return GCD( min, max );
+    }
+
+    private int GCD( int a, int b )
+    {
+        if ( b == 0 )
+        {
+            return a;
+        }
+
+        if ( a > b )
+        {
+            return GCD( b, a );
+        }
+
+        return GCD( a, b % a );
+    }
+}
---
+++ c-sharp/Tests/math/FindGreatestCommonDivisorOfArrayTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FindGreatestCommonDivisorOfArrayTests
+{
+    [TestCase( new int[] { 2, 5, 6, 9, 10 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { 7, 5, 6, 8, 3 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 3, 3 }, ExpectedResult = 3 )]
+    public int FindGCDTest( int[] nums ) =>
+        new FindGreatestCommonDivisorOfArray().FindGCD( nums );
+}
