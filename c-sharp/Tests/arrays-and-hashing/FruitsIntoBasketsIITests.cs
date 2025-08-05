9c0bc4e409a716c4b960d75dffb7d0a43bfdf1af
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-05 18:58:27 +0200

    #3477: Fruits Into Baskets II; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/FruitsIntoBasketsIITests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FruitsIntoBasketsIITests
+{
+    [TestCase( new int[] { 4, 2, 5 }, new int[] { 3, 5, 4 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 3, 6, 1 }, new int[] { 6, 4, 7 }, ExpectedResult = 0 )]
+    public int NumOfUnplacedFruitsTest( int[] fruits, int[] baskets ) =>
+        new FruitsIntoBasketsII().NumOfUnplacedFruits( fruits, baskets );
+}
