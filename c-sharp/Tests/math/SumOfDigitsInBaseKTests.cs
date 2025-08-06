5cd5d8927a6065a3669a3917f187ba4ca0cccace
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-06 18:46:59 +0200

    #1837: Sum of Digits in Base K; solution & tests.

---
+++ c-sharp/Tests/math/SumOfDigitsInBaseKTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SumOfDigitsInBaseKTests
+{
+    [TestCase( 34, 6, ExpectedResult = 9 )]
+    [TestCase( 10, 10, ExpectedResult = 1 )]
+    public int SumBaseTest( int n, int k ) =>
+        new SumOfDigitsInBaseK().SumBase( n, k );
+}
