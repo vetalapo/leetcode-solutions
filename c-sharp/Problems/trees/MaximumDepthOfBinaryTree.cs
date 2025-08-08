f20816bf9c54109452bd0beefbafbd970754f205
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-08 18:48:07 +0200

    #104: Maximum Depth of Binary Tree; solution & tests.

---
+++ c-sharp/Problems/trees/MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,58 @@
+﻿/**
+ * 104
+ * Maximum Depth of Binary Tree
+ **
+ * Given the root of a binary tree,
+ * return its maximum depth.
+ *
+ * A binary tree's maximum depth is
+ * the number of nodes along the longest path
+ * from the root node down to the farthest leaf node.
+ *
+ * Example 1:
+ *   Input: root = [3,9,20,null,null,15,7]
+ *   Output: 3
+ *
+ * Example 2:
+ *   Input: root = [1,null,2]
+ *   Output: 2
+ *
+ * Constraints:
+ *   • The number of nodes in the tree is in the range [0, 10^4].
+ *   • -100 <= Node.val <= 100
+ **
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/
+***/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+***/
+
+using System;
+
+using Problems.Common;
+
+namespace Problems;
+
+public class MaximumDepthOfBinaryTree
+{
+    public int MaxDepth( TreeNode root )
+    {
+        if ( root is null )
+        {
+            return 0;
+        }
+
+        return 1 + Math.Max( MaxDepth( root.left ), MaxDepth( root.right ) );
+    }
+}
---
+++ c-sharp/Tests/trees/MaximumDepthOfBinaryTreeTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+using Problems.Common;
+
+public class MaximumDepthOfBinaryTreeTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int MaxDepthTest( TreeNode root ) =>
+        new MaximumDepthOfBinaryTree().MaxDepth( root );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( TreeNode.Create( [3, 9, 20, null, null, 15, 7] ) ).Returns( 3 );
+
+            yield return new TestCaseData( TreeNode.Create( [1, null, 2] ) ).Returns( 2 );
+
+            yield return new TestCaseData( TreeNode.Create( [] ) ).Returns( 0 );
+        }
+    }
+}
