0ff3d65ea880608b6e55aec7c62e090de0cca70c
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-13 16:03:10 +0200

    #326: Power of Three; solution & tests.

---
+++ c-sharp/Tests/math/PowerOfThreeTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class PowerOfThreeTests
+{
+    [TestCase( 27, ExpectedResult = true )]
+    [TestCase( 0, ExpectedResult = false )]
+    [TestCase( -1, ExpectedResult = false )]
+    [TestCase( 45, ExpectedResult = false )]
+    [TestCase( 1, ExpectedResult = true )]
+    [TestCase( 243, ExpectedResult = true )]
+    public bool IsPowerOfThreeTest( int n ) =>
+        new PowerOfThree().IsPowerOfThree( n );
+}
