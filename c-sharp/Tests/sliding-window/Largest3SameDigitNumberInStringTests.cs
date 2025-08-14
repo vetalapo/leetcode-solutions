a464a4e66457778cf46f2475a8812b9882553dd6
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-14 20:22:38 +0200

    #2264: Largest 3-Same-Digit Number in String; solution & tests.

---
+++ c-sharp/Tests/sliding-window/Largest3SameDigitNumberInStringTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class Largest3SameDigitNumberInStringTests
+{
+    [TestCase( "6777133339", ExpectedResult = "777" )]
+    [TestCase( "2300019", ExpectedResult = "000" )]
+    [TestCase( "42352338", ExpectedResult = "" )]
+    public string LargestGoodIntegerTest( string num ) =>
+        new Largest3SameDigitNumberInString().LargestGoodInteger( num );
+}
