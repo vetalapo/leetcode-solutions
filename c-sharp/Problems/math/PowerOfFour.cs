1b6cb46feae42f148e2d8af58b42294353009cf1
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-15 17:23:26 +0200

    #242: Power of Four; solution & tests.

---
+++ c-sharp/Problems/math/PowerOfFour.cs
@@ -0,0 +1,43 @@
+﻿/**
+ * 342
+ * Power of Four
+ **
+ * Given an integer n,
+ * return true if it is a power of four.
+ * Otherwise, return false.
+ *
+ * An integer n is a power of four,
+ * if there exists an integer x such that n == 4x.
+ *
+ * Example 1:
+ *   Input: n = 16
+ *   Output: true
+ *
+ * Example 2:
+ *   Input: n = 5
+ *   Output: false
+ *
+ * Example 3:
+ *   Input: n = 1
+ *   Output: true
+ *
+ * Constraints:
+ *   • -2^31 <= n <= 2^31 - 1
+ *
+ * Follow up:
+ *   Could you solve it without loops/recursion?
+ **
+ * https://leetcode.com/problems/power-of-four/
+***/
+
+namespace Problems;
+
+public class PowerOfFour
+{
+    public bool IsPowerOfFour( int n )
+    {
+        return n > 0 &&
+            ( n & ( n - 1 ) ) == 0 &&
+            ( n & 0b_1010101010101010101010101010101 ) != 0;
+    }
+}
---
+++ c-sharp/Tests/math/PowerOfFourTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class PowerOfFourTests
+{
+    [TestCase( 16, ExpectedResult = true )]
+    [TestCase( 5, ExpectedResult = false )]
+    [TestCase( 1, ExpectedResult = true )]
+    [TestCase( -128, ExpectedResult = false )]
+    [TestCase( 1024, ExpectedResult = true )]
+    [TestCase( 1048576, ExpectedResult = true )]
+    public bool IsPowerOfFourTest( int n ) =>
+        new PowerOfFour().IsPowerOfFour( n );
+}
