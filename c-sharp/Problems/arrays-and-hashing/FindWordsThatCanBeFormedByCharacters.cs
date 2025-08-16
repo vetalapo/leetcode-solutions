60fea3002c8e67e8b303bbe7ac5e3fcdcafebfb5
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-16 17:16:45 +0200

    #1160: Find Words That Can Be Formed By Characters; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/FindWordsThatCanBeFormedByCharacters.cs
@@ -0,0 +1,93 @@
+﻿/**
+ * 1160
+ * Find Words That Can Be Formed by Characters
+ **
+ * You are given an array of strings words and a string chars.
+ * A string is good
+ * if it can be formed by characters from chars
+ * (each character can only be used once for each word in words).
+ *
+ * Return the sum of lengths of all good strings in words.
+ *
+ * Example 1:
+ *   Input: words = ["cat","bt","hat","tree"], chars = "atach"
+ *   Output: 6
+ *   Explanation:
+ *     The strings that can be formed are
+ *     "cat" and "hat"
+ *     so the answer is 3 + 3 = 6.
+ *
+ * Example 2:
+ *   Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+ *   Output: 10
+ *   Explanation:
+ *     The strings that can be formed are
+ *     "hello" and "world"
+ *     so the answer is 5 + 5 = 10.
+ *
+ * Constraints:
+ *   • 1 <= words.length <= 1000
+ *   • 1 <= words[i].length, chars.length <= 100
+ *   • words[i] and chars consist of lowercase English letters.
+ *
+ * Hint 1:
+ *   Solve the problem for each string in words independently.
+ *
+ * Hint 2:
+ *   Now try to think in frequency of letters.
+ *
+ * Hint 3:
+ *   Count how many times each character occurs in string chars.
+ *
+ * Hint 4:
+ *   To form a string using characters from chars, the frequency of each character in chars must be greater than or equal the frequency of that character in the string to be formed.
+ **
+ * https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
+***/
+
+using System;
+
+namespace Problems;
+
+public class FindWordsThatCanBeFormedByCharacters
+{
+    public int CountCharacters( string[] words, string chars )
+    {
+        int[] charMap = new int[26];
+
+        foreach ( char c in chars )
+        {
+            charMap[c - 'a']++;
+        }
+
+        int sum = 0;
+
+        int[] currWordMap = new int[26];
+
+        foreach ( string word in words )
+        {
+            bool isGood = true;
+
+            foreach ( char c in word )
+            {
+                currWordMap[c - 'a']++;
+
+                if ( currWordMap[c - 'a'] > charMap[c - 'a'] )
+                {
+                    isGood = false;
+
+                    break;
+                }
+            }
+
+            if ( isGood )
+            {
+                sum += word.Length;
+            }
+
+            Array.Clear( currWordMap );
+        }
+
+        return sum;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/FindWordsThatCanBeFormedByCharactersTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FindWordsThatCanBeFormedByCharactersTests
+{
+    [TestCase( [new string[] { "cat", "bt", "hat", "tree" }, "atach"], ExpectedResult = 6 )]
+    [TestCase( [new string[] { "hello", "world", "leetcode" }, "welldonehoneyr"], ExpectedResult = 10 )]
+    public int CountCharactersTest( string[] words, string chars ) =>
+        new FindWordsThatCanBeFormedByCharacters().CountCharacters( words, chars );
+}
