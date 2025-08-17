280e61ef4f8d69781c17f5c124f42e20b5a18a24
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-17 10:38:07 +0200

    #2562: Find the Array Concatenation Value; solution & tests.

---
+++ c-sharp/Tests/two-pointers/FindTheArrayConcatenationValueTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FindTheArrayConcatenationValueTests
+{
+    [TestCase( new int[] { 7, 52, 2, 4 }, ExpectedResult = 596 )]
+    [TestCase( new int[] { 5, 14, 13, 8, 12 }, ExpectedResult = 673 )]
+    public long FindTheArrayConcValTest( int[] nums ) =>
+        new FindTheArrayConcatenationValue().FindTheArrayConcVal( nums );
+}
