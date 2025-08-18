38845f83f03fe78d39651a904eaca5baaf0d366d
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-18 11:41:08 +0200

    #100: Same Tree; solution & tests.

---
+++ c-sharp/Problems/trees/SameTree.cs
@@ -0,0 +1,65 @@
+﻿/**
+ * 100
+ * Same Tree
+ **
+ * Given the roots of two binary trees p and q,
+ * write a function to check if they are the same or not.
+ *
+ * Two binary trees are considered the same
+ * if they are structurally identical,
+ * and the nodes have the same value.
+ *
+ * Example 1:
+ *   Input: p = [1,2,3], q = [1,2,3]
+ *   Output: true
+ *
+ * Example 2:
+ *   Input: p = [1,2], q = [1,null,2]
+ *   Output: false
+ *
+ * Example 3:
+ *   Input: p = [1,2,1], q = [1,1,2]
+ *   Output: false
+ *
+ * Constraints:
+ *   • The number of nodes in both trees is in the range [0, 100].
+ *   • -10^4 <= Node.val <= 10^4
+ **
+ * https://leetcode.com/problems/same-tree/
+***/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+***/
+
+using Problems.Common;
+
+namespace Problems;
+
+public class SameTree
+{
+    public bool IsSameTree( TreeNode p, TreeNode q )
+    {
+        if ( p is null && q is null )
+        {
+            return true;
+        }
+
+        if ( p is null || q is null || p.val != q.val )
+        {
+            return false;
+        }
+
+        return IsSameTree( p.left, q.left ) && IsSameTree( p.right, q.right );
+    }
+}
---
+++ c-sharp/Tests/trees/SameTreeTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+using Problems.Common;
+
+public class SameTreeTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public bool IsSameTreeTest( TreeNode p, TreeNode q ) =>
+        new SameTree().IsSameTree( p, q );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( TreeNode.Create( [1, 2, 3] ), TreeNode.Create( [1, 2, 3] ) )
+                .Returns( true );
+
+            yield return new TestCaseData( TreeNode.Create( [1, 2] ), TreeNode.Create( [1, null, 2] ) )
+                .Returns( false );
+
+            yield return new TestCaseData( TreeNode.Create( [1, 2, 1] ), TreeNode.Create( [1, 1, 2] ) )
+                .Returns( false );
+
+            yield return new TestCaseData( TreeNode.Create( [10, 5, 15] ), TreeNode.Create( [10, 5, null, null, 15] ) )
+                .Returns( false );
+        }
+    }
+}
