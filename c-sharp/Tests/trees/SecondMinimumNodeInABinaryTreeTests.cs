ac9bbecd7904528cdf80af764755b8fc6d423cfb
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-19 19:10:40 +0200

    #671: Second Minimum Node In a Binary Tree; solution & tests.

---
+++ c-sharp/Tests/trees/SecondMinimumNodeInABinaryTreeTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+using Problems.Common;
+
+public class SecondMinimumNodeInABinaryTreeTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int FindSecondMinimumValueTest( TreeNode root ) =>
+        new SecondMinimumNodeInABinaryTree().FindSecondMinimumValue( root );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( TreeNode.Create( [2, 2, 5, null, null, 5, 7] ) ).Returns( 5 );
+
+            yield return new TestCaseData( TreeNode.Create( [2, 2, 2] ) ).Returns( -1 );
+
+            yield return new TestCaseData( TreeNode.Create( [2, 2, 2147483647] ) ).Returns( 2147483647 );
+        }
+    }
+}
