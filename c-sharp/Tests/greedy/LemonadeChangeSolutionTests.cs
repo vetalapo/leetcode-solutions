337a119a7129d9349cb47fafa0d47b0d1915bbf2
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-22 13:03:21 +0200

    #860: Lemonade change; solution & tests.

---
+++ c-sharp/Tests/greedy/LemonadeChangeSolutionTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class LemonadeChangeSolutionTests
+{
+    [TestCase( new int[] { 5, 5, 5, 10, 20 }, ExpectedResult = true )]
+    [TestCase( new int[] { 5, 5, 10, 10, 20 }, ExpectedResult = false )]
+    public bool LemonadeChangeTest( int[] bills ) =>
+        new LemonadeChangeSolution().LemonadeChange( bills );
+}
