7b66276be4269b3d31c894e85387368202365a44
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-23 14:58:57 +0200

    #202: Happy Number; solution & tests.

---
+++ c-sharp/Tests/math/HappyNumberTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class HappyNumberTests
+{
+    [TestCase( 19, ExpectedResult = true )]
+    [TestCase( 2, ExpectedResult = false )]
+    [TestCase( 3, ExpectedResult = false )]
+    public bool IsHappyTest( int n ) =>
+        new HappyNumber().IsHappy( n );
+}
