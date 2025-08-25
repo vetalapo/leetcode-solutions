fe14b12690d214b726d65ac4aa62d87fe96b9022
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-25 19:13:51 +0200

    #1844: Replace All Digits with Characters; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/ReplaceAllDigitsWithCharactersTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ReplaceAllDigitsWithCharactersTests
+{
+    [TestCase( "a1c1e1", ExpectedResult = "abcdef" )]
+    [TestCase( "a1b2c3d4e", ExpectedResult = "abbdcfdhe" )]
+    public string ReplaceDigitsTest( string s ) =>
+        new ReplaceAllDigitsWithCharacters().ReplaceDigits( s );
+}
