e601ed7a0c7bd7fb927f92faf11a269b193d82b9
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-26 16:03:56 +0200

    #3000: Maximum Area of Longest Diagonal Rectangle; solution & tests.

---
+++ c-sharp/Problems/math/MaximumAreaOfLongestDiagonalRectangle.cs
@@ -0,0 +1,73 @@
+﻿/**
+ * 3000
+ * Maximum Area of Longest Diagonal Rectangle
+ **
+ * You are given a 2D 0-indexed integer array dimensions.
+ *
+ * For all indices i, 0 <= i < dimensions.length,
+ * dimensions[i][0] represents the length
+ * and dimensions[i][1] represents the width of the rectangle i.
+ *
+ * Return the area of the rectangle having the longest diagonal.
+ * If there are multiple rectangles with the longest diagonal,
+ * return the area of the rectangle having the maximum area.
+ *
+ * Example 1:
+ *   Input: dimensions = [[9,3],[8,6]]
+ *   Output: 48
+ *   Explanation:
+ *     For index = 0, length = 9 and width = 3.
+ *     Diagonal length = sqrt(9 * 9 + 3 * 3) = sqrt(90) ≈ 9.487.
+ *     For index = 1, length = 8 and width = 6.
+ *     Diagonal length = sqrt(8 * 8 + 6 * 6) = sqrt(100) = 10.
+ *     So,the rectangle at index 1
+ *     has a greater diagonal length
+ *     therefore we return area = 8 * 6 = 48.
+ *
+ * Example 2:
+ *   Input: dimensions = [[3,4],[4,3]]
+ *   Output: 12
+ *   Explanation:
+ *     Length of diagonal is the same for both which is 5,
+ *     so maximum area = 12.
+ *
+ * Constraints:
+ *   • 1 <= dimensions.length <= 100
+ *   • dimensions[i].length == 2
+ *   • 1 <= dimensions[i][0], dimensions[i][1] <= 100
+ *
+ * Hint 1:
+ *   Diagonal of rectangle is sqrt(length2 + width2).
+ **
+ * https://leetcode.com/problems/maximum-area-of-longest-diagonal-rectangle/
+***/
+
+using System;
+
+namespace Problems;
+
+public class MaximumAreaOfLongestDiagonalRectangle
+{
+    public int AreaOfMaxDiagonal( int[][] dimensions )
+    {
+        int maxDiagonalSq = 0;
+        int maxArea = 0;
+
+        foreach ( int[] rect in dimensions )
+        {
+            int currDiagonalSq = rect[0] * rect[0] + rect[1] * rect[1];
+
+            if ( currDiagonalSq > maxDiagonalSq )
+            {
+                maxDiagonalSq = currDiagonalSq;
+                maxArea = rect[0] * rect[1];
+            }
+            else if ( currDiagonalSq == maxDiagonalSq )
+            {
+                maxArea = Math.Max( maxArea, rect[0] * rect[1] );
+            }
+        }
+
+        return maxArea;
+    }
+}
---
+++ c-sharp/Tests/math/MaximumAreaOfLongestDiagonalRectangleTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class MaximumAreaOfLongestDiagonalRectangleTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int AreaOfMaxDiagonalTest( int[][] dimensions ) =>
+        new MaximumAreaOfLongestDiagonalRectangle().AreaOfMaxDiagonal( dimensions );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( [new int[][] { [9, 3], [8, 6] }] )
+                .Returns( 48 );
+
+            yield return new TestCaseData( [new int[][] { [3, 4], [4, 3] }] )
+                .Returns( 12 );
+
+            yield return new TestCaseData( [new int[][] { [2, 6], [5, 1], [3, 10], [8, 4] }] )
+                .Returns( 30 );
+        }
+    }
+}
