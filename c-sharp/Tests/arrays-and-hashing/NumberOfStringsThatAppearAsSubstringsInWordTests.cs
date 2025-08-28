cfcb9d909090ecacdd48c8ba51817908c5ea182a
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-28 18:13:28 +0200

    #1967: Number of Strings That Appear as Substrings in Word; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/NumberOfStringsThatAppearAsSubstringsInWordTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class NumberOfStringsThatAppearAsSubstringsInWordTests
+{
+    [TestCase( [new string[] { "a", "abc", "bc", "d" }, "abc"], ExpectedResult = 3 )]
+    [TestCase( [new string[] { "a", "b", "c" }, "aaaaabbbbb"], ExpectedResult = 2 )]
+    [TestCase( [new string[] { "a", "a", "a" }, "ab"], ExpectedResult = 3 )]
+    [TestCase( [new string[] { "hvzoo", "r", "hyjcrgzr", "yvqahvz", "org", "yvqahvz", "org", "oor", "gxe", "zo", "ahvzoo", "ahvzo", "r", "o", "caviikty", "pkxlcaams" }, "tyvqahvzoorg"], ExpectedResult = 12 )]
+    public int NumOfStringsTest( string[] patterns, string word ) =>
+        new NumberOfStringsThatAppearAsSubstringsInWord().NumOfStrings( patterns, word );
+}
