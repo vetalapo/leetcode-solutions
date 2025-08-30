70be99524f3d9c14b685cc3ddc492cb4454efe2a
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-30 18:42:07 +0200

    #2932: Maximum Strong Pair XOR I; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/MaximumStrongPairXorITests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MaximumStrongPairXorITests
+{
+    [TestCase( new int[] { 1, 2, 3, 4, 5 }, ExpectedResult = 7 )]
+    [TestCase( new int[] { 10, 100 }, ExpectedResult = 0 )]
+    [TestCase( new int[] { 5, 6, 25, 30 }, ExpectedResult = 7 )]
+    public int MaximumStrongPairXorTest( int[] nums ) =>
+        new MaximumStrongPairXorI().MaximumStrongPairXor( nums );
+}
