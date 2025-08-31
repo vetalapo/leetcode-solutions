e9ad797eee24db47c21550d89d1f7fb01fe0d89f
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-31 17:01:54 +0200

    #476: Number Complement; solution & tests.

---
+++ c-sharp/Tests/bit-manipulation/NumberComplementTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class NumberComplementTests
+{
+    [TestCase( 0, ExpectedResult = 1 )]
+    [TestCase( 5, ExpectedResult = 2 )]
+    [TestCase( 1, ExpectedResult = 0 )]
+    public int FindComplementTest( int num ) =>
+        new NumberComplement().FindComplement( num );
+}
