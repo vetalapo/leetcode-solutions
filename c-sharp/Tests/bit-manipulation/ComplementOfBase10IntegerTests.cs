c070194cdc4f135a671ec9dc6ac59b370e41f35f
Vitaly Dushkin <vetalapo@gmail.com>
2025-08-31 16:44:16 +0200

    #1009: Complement of Base 10 Integer; solution & tests.

---
+++ c-sharp/Tests/bit-manipulation/ComplementOfBase10IntegerTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class ComplementOfBase10IntegerTests
+{
+    [TestCase( 0, ExpectedResult = 1 )]
+    [TestCase( 5, ExpectedResult = 2 )]
+    [TestCase( 7, ExpectedResult = 0 )]
+    [TestCase( 10, ExpectedResult = 5 )]
+    public int BitwiseComplementTest( int n ) =>
+        new ComplementOfBase10Integer().BitwiseComplement( n );
+}
