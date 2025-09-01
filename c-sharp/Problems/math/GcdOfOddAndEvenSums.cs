5bca5d133278e4426489b1085c8fe2708d62e93c
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-01 18:40:26 +0200

    #3658: GCD of Odd and Even Sums; solution & tests.

---
+++ c-sharp/Problems/math/GcdOfOddAndEvenSums.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * 3658
+ * GCD of Odd and Even Sums
+ **
+ * You are given an integer n.
+ * Your task is to compute the GCD (greatest common divisor)
+ * of two values:
+ *   • sumOdd: the sum of the first n odd numbers.
+ *   • sumEven: the sum of the first n even numbers.
+ *
+ * Return the GCD of sumOdd and sumEven.
+ *
+ * Example 1:
+ *   Input: n = 4
+ *   Output: 4
+ *   Explanation:
+ *     • Sum of the first 4 odd numbers sumOdd = 1 + 3 + 5 + 7 = 16
+ *     • Sum of the first 4 even numbers sumEven = 2 + 4 + 6 + 8 = 20
+ *     Hence, GCD(sumOdd, sumEven) = GCD(16, 20) = 4.
+ *
+ * Example 2:
+ *   Input: n = 5
+ *   Output: 5
+ *   Explanation:
+ *     • Sum of the first 5 odd numbers sumOdd = 1 + 3 + 5 + 7 + 9 = 25
+ *     • Sum of the first 5 even numbers sumEven = 2 + 4 + 6 + 8 + 10 = 30
+ *     Hence, GCD(sumOdd, sumEven) = GCD(25, 30) = 5.
+ *
+ * Constraints:
+ *   • 1 <= n <= 10​​​​​​​00
+ *
+ * Hint 1:
+ *   The first n odd numbers sum to n * n
+ *
+ * Hint 2:
+ *   First n even numbers sum to n * (n + 1)
+ *
+ * Hint 3:
+ *   gcd(n, n + 1) = 1, so the answer is n
+ **
+ * https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+***/
+
+namespace Problems;
+
+public class GcdOfOddAndEvenSums
+{
+    public int GcdOfOddEvenSums( int n )
+    {
+        return n;
+    }
+}
---
+++ c-sharp/Tests/math/GcdOfOddAndEvenSumsTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+using Problems;
+public class GcdOfOddAndEvenSumsTests
+{
+    [TestCase( 4, ExpectedResult = 4 )]
+    [TestCase( 5, ExpectedResult = 5 )]
+    public int GcdOfOddEvenSumsTest( int n ) =>
+        new GcdOfOddAndEvenSums().GcdOfOddEvenSums( n );
+}
