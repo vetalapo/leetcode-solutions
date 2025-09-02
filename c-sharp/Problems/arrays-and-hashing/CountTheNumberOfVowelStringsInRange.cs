9e3b306794cf83b04c3aaea14723f9f200530ae2
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-02 15:16:11 +0200

    #2586: Count the Number of Vowel Strings in Range; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/CountTheNumberOfVowelStringsInRange.cs
@@ -0,0 +1,73 @@
+﻿/**
+ * 2586
+ * Count the Number of Vowel Strings in Range
+ **
+ * You are given a 0-indexed array of string words
+ * and two integers left and right.
+ *
+ * A string is called a vowel string
+ * if it starts with a vowel character
+ * and ends with a vowel character
+ * where vowel characters are 'a', 'e', 'i', 'o', and 'u'.
+ *
+ * Return the number of vowel strings words[i]
+ * where i belongs to the inclusive range [left, right].
+ *
+ * Example 1:
+ *   Input: words = ["are","amy","u"], left = 0, right = 2
+ *   Output: 2
+ *   Explanation:
+ *     - "are" is a vowel string because it starts with 'a' and ends with 'e'.
+ *     - "amy" is not a vowel string because it does not end with a vowel.
+ *     - "u" is a vowel string because it starts with 'u' and ends with 'u'.
+ *     The number of vowel strings in the mentioned range is 2.
+ *
+ * Example 2:
+ *   Input: words = ["hey","aeo","mu","ooo","artro"], left = 1, right = 4
+ *   Output: 3
+ *   Explanation:
+ *     - "aeo" is a vowel string because it starts with 'a' and ends with 'o'.
+ *     - "mu" is not a vowel string because it does not start with a vowel.
+ *     - "ooo" is a vowel string because it starts with 'o' and ends with 'o'.
+ *     - "artro" is a vowel string because it starts with 'a' and ends with 'o'.
+ *     The number of vowel strings in the mentioned range is 3.
+ *
+ * Constraints:
+ *   • 1 <= words.length <= 1000
+ *   • 1 <= words[i].length <= 10
+ *   • words[i] consists of only lowercase English letters.
+ *   • 0 <= left <= right < words.length
+ *
+ * Hint 1:
+ *   consider iterating over all strings from left to right
+ *   and use an if condition to check
+ *   if the first character and last character are vowels.
+ **
+ * https://leetcode.com/problems/count-the-number-of-vowel-strings-in-range/
+***/
+
+namespace Problems;
+
+public class CountTheNumberOfVowelStringsInRange
+{
+    public int VowelStrings( string[] words, int left, int right )
+    {
+        int count = 0;
+
+        for ( ; left <= right; left++ )
+        {
+            if ( IsVowel( words[left][0] ) && IsVowel( words[left][^1] ) )
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private bool IsVowel( char c ) => c switch
+    {
+        'a' or 'e' or 'i' or 'o' or 'u' => true,
+        _ => false,
+    };
+}
---
+++ c-sharp/Tests/arrays-and-hashing/CountTheNumberOfVowelStringsInRangeTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountTheNumberOfVowelStringsInRangeTests
+{
+    [TestCase( [new string[] { "are", "amy", "u" }, 0, 2], ExpectedResult = 2 )]
+    [TestCase( [new string[] { "hey", "aeo", "mu", "ooo", "artro" }, 1, 4], ExpectedResult = 3 )]
+    public int VowelStringsTest( string[] words, int left, int right ) =>
+        new CountTheNumberOfVowelStringsInRange().VowelStrings( words, left, right );
+}
