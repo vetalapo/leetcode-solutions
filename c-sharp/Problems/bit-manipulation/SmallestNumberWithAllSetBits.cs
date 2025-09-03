496abec87733e295671fdc5df5ae5877c7128082
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-03 18:33:26 +0200

    #3370: Smallest Number With All Set Bits; solution & tests.

---
+++ c-sharp/Problems/bit-manipulation/SmallestNumberWithAllSetBits.cs
@@ -0,0 +1,55 @@
+﻿/**
+ * 3370
+ * Smallest Number With All Set Bits
+ **
+ * You are given a positive number n.
+ *
+ * Return the smallest number x greater than or equal to n,
+ * such that the binary representation of x contains only set bits
+ *
+ * Example 1:
+ *   Input: n = 5
+ *   Output: 7
+ *   Explanation:
+ *     The binary representation of 7 is "111".
+ *
+ * Example 2:
+ *   Input: n = 10
+ *   Output: 15
+ *   Explanation:
+ *     The binary representation of 15 is "1111".
+ *
+ * Example 3:
+ *   Input: n = 3
+ *   Output: 3
+ *   Explanation:
+ *     The binary representation of 3 is "11".
+ *
+ * Constraints:
+ *   • 1 <= n <= 1000
+ *
+ * Hint 1:
+ *   Find the strictly greater power of 2,
+ *   and subtract 1 from it.
+ **
+ * https://leetcode.com/problems/smallest-number-with-all-set-bits/
+***/
+
+namespace Problems;
+
+public class SmallestNumberWithAllSetBits
+{
+    public int SmallestNumber( int n )
+    {
+        int result = 0;
+
+        while ( n > 0 )
+        {
+            n >>= 1;
+
+            result = result * 2 + 1;
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/bit-manipulation/SmallestNumberWithAllSetBitsTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SmallestNumberWithAllSetBitsTests
+{
+    [TestCase( 5, ExpectedResult = 7 )]
+    [TestCase( 10, ExpectedResult = 15 )]
+    [TestCase( 3, ExpectedResult = 3 )]
+    public int SmallestNumberTest( int n ) =>
+        new SmallestNumberWithAllSetBits().SmallestNumber( n );
+}
