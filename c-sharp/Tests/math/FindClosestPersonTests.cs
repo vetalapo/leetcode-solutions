11643314a7f6ea377b90b6fdd4d84539ed6beb4a
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-04 14:26:58 +0200

    #3516: Find Closest Person; solution & tests.

---
+++ c-sharp/Tests/math/FindClosestPersonTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class FindClosestPersonTests
+{
+    [TestCase( 2, 7, 4, ExpectedResult = 1 )]
+    [TestCase( 2, 5, 6, ExpectedResult = 2 )]
+    [TestCase( 1, 5, 3, ExpectedResult = 0 )]
+    public int FindClosestTest( int x, int y, int z ) =>
+        new FindClosestPerson().FindClosest( x, y, z );
+}
