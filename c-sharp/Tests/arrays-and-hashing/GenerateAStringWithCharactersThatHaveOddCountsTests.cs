5f305ee2bb38a047a8f8b239a3f5efe096aced6f
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-05 10:18:20 +0200

    #1374: Generate a String With Characters That Have Odd Counts; solution & tests;

---
+++ c-sharp/Tests/arrays-and-hashing/GenerateAStringWithCharactersThatHaveOddCountsTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class GenerateAStringWithCharactersThatHaveOddCountsTests
+{
+    [TestCase( 4 )]
+    [TestCase( 2 )]
+    [TestCase( 7 )]
+    public void GenerateTheStringTest( int n )
+    {
+        string result = new GenerateAStringWithCharactersThatHaveOddCounts()
+            .GenerateTheString( n );
+
+        Assert.AreEqual( result.Length, n );
+
+        Dictionary<char, int> countMap = [];
+
+        foreach ( char c in result )
+        {
+            if ( !countMap.TryAdd( c, 1 ) )
+            {
+                countMap[c]++;
+            }
+        }
+
+        foreach ( int count in countMap.Values )
+        {
+            Assert.AreEqual( count & 1, 1 );
+        }
+    }
+}
