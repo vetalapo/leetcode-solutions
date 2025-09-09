8fa70cf5501377a2b6c5ed4c81db00151e685309
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-09 17:22:51 +0200

    #2960: Count Tested Devices After Test Operations; solution & tests.

---
+++ c-sharp/Tests/math/CountTestedDevicesAfterTestOperationsTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountTestedDevicesAfterTestOperationsTests
+{
+    [TestCase( new int[] { 1, 1, 2, 1, 3 }, ExpectedResult = 3 )]
+    [TestCase( new int[] { 0, 1, 2 }, ExpectedResult = 2 )]
+    public int CountTestedDevicesTest( int[] batteryPercentages ) =>
+        new CountTestedDevicesAfterTestOperations().CountTestedDevices( batteryPercentages );
+}
