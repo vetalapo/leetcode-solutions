62104954d026f1d78b808656515cf3e016d8a77a
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-10 18:02:15 +0200

    #2651: Calculate Delayed Arrival Time; solution & tests.

---
+++ c-sharp/Tests/math/CalculateDelayedArrivalTimeTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CalculateDelayedArrivalTimeTests
+{
+    [TestCase( 15, 5, ExpectedResult = 20 )]
+    [TestCase( 13, 11, ExpectedResult = 0 )]
+    public int FindDelayedArrivalTimeTest( int arrivalTime, int delayedTime ) =>
+        new CalculateDelayedArrivalTime().FindDelayedArrivalTime( arrivalTime, delayedTime );
+}
