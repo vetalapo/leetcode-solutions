43afd86e0e20d271c1f2c6e15a7bb3d3987e1064
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-11 20:47:04 +0200

    #2085: count Common Words With One Occurrence; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/CountCommonWordsWithOneOccurrenceTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class CountCommonWordsWithOneOccurrenceTests
+{
+    [TestCase( new string[] { "leetcode", "is", "amazing", "as", "is" }, new string[] { "amazing", "leetcode", "is" }, ExpectedResult = 2 )]
+    [TestCase( new string[] { "b", "bb", "bbb" }, new string[] { "a", "aa", "aaa" }, ExpectedResult = 0 )]
+    [TestCase( new string[] { "a", "ab" }, new string[] { "a", "a", "a", "ab" }, ExpectedResult = 1 )]
+    public int CountWordsTest( string[] words1, string[] words2 ) =>
+        new CountCommonWordsWithOneOccurrence().CountWords( words1, words2 );
+}
