d16c8ec52b5709dd80bfd9ae2b1f4950366b8f02
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-12 23:42:02 +0200

    #2022: Convert 1D Array Into 2D Array; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/Convert1DArrayInto2DArrayTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+public class Convert1DArrayInto2DArrayTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int[][] Construct2DArrayTest( int[] original, int m, int n ) =>
+        new Convert1DArrayInto2DArray().Construct2DArray( original, m, n );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( new int[] { 1, 2, 3, 4 }, 2, 2 )
+                .Returns( new int[][] { [1, 2], [3, 4] } );
+
+            yield return new TestCaseData( new int[] { 1, 2, 3 }, 1, 3 )
+                .Returns( new int[][] { [1, 2, 3] } );
+
+            yield return new TestCaseData( new int[] { 1, 2 }, 1, 1 )
+                .Returns( Array.Empty<int[]>() );
+        }
+    }
+}
