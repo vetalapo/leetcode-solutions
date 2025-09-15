4a4e350ecc8e2f61b47d99922e88914d218580ff
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-15 23:52:39 +0200

    #1935: Maximum Number of Words You Can Type; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/MaximumNumberOfWordsYouCanTypeTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class MaximumNumberOfWordsYouCanTypeTests
+{
+    [TestCase( "hello world", "ad", ExpectedResult = 1 )]
+    [TestCase( "leet code", "lt", ExpectedResult = 1 )]
+    [TestCase( "leet code", "e", ExpectedResult = 0 )]
+    public int CanBeTypedWordsTest( string text, string brokenLetters ) =>
+        new MaximumNumberOfWordsYouCanType().CanBeTypedWords( text, brokenLetters );
+}
