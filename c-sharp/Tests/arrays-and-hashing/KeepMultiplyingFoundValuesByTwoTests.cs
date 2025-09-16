701025b12f08d139a4de02174957f6ad2410909f
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-16 23:39:33 +0200

    #2154: Keep Multiplying Found Values by Two; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/KeepMultiplyingFoundValuesByTwoTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class KeepMultiplyingFoundValuesByTwoTests
+{
+    [TestCase( new int[] { 5, 3, 6, 1, 12 }, 3, ExpectedResult = 24 )]
+    [TestCase( new int[] { 2, 7, 9 }, 4, ExpectedResult = 4 )]
+    [TestCase( new int[] { 161, 28, 640, 264, 81, 561, 320, 2, 61, 244, 183, 108, 773, 61, 976, 122, 988, 2, 370, 392, 488, 375, 349, 432, 713, 563 }, 61, ExpectedResult = 1952 )]
+    public int FindFinalValueTest( int[] nums, int original ) =>
+        new KeepMultiplyingFoundValuesByTwo().FindFinalValue( nums, original );
+}
