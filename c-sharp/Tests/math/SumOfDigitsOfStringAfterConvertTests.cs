a7f4eeab92426eea2cf90d8e5681a8d746097202
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-17 14:49:41 +0200

    #1945: Sum of Digits of String After Convert; solution & tests.

---
+++ c-sharp/Tests/math/SumOfDigitsOfStringAfterConvertTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SumOfDigitsOfStringAfterConvertTests
+{
+    [TestCase( "iiii", 1, ExpectedResult = 36 )]
+    [TestCase( "leetcode", 2, ExpectedResult = 6 )]
+    [TestCase( "zbax", 2, ExpectedResult = 8 )]
+    public int GetLuckyTest( string s, int k ) =>
+        new SumOfDigitsOfStringAfterConvert().GetLucky( s, k );
+}
