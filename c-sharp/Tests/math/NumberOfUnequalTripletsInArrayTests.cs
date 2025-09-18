0b48d4a0540d651bbfdc5d681ff6c088b69d6ac3
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-18 12:36:34 +0200

    #2475: Number of Unequal Triplets in Array; solution & tests.

---
+++ c-sharp/Tests/math/NumberOfUnequalTripletsInArrayTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class NumberOfUnequalTripletsInArrayTests
+{
+    [TestCase( new int[] { 4, 4, 2, 4, 3 }, ExpectedResult = 3 )]
+    [TestCase( new int[] { 1, 1, 1, 1, 1 }, ExpectedResult = 0 )]
+    [TestCase( new int[] { 1, 2, 3 }, ExpectedResult = 1 )]
+    public int UnequalTripletsTest( int[] nums ) =>
+        new NumberOfUnequalTripletsInArray().UnequalTriplets( nums );
+}
