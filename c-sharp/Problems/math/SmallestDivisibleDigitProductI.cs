7780cf255337b69836eb2a668361b50fb77e228b
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-20 20:41:28 +0200

    #3345: Smallest Divisible Digit Product I; solution & tests.

---
+++ c-sharp/Problems/math/SmallestDivisibleDigitProductI.cs
@@ -0,0 +1,66 @@
+﻿/**
+ * 3345
+ * Smallest Divisible Digit Product I
+ **
+ * You are given two integers n and t.
+ * Return the smallest number greater than or equal to n
+ * such that the product of its digits is divisible by t.
+ *
+ * Example 1:
+ *   Input: n = 10, t = 2
+ *   Output: 10
+ *   Explanation:
+ *     The digit product of 10 is 0,
+ *     which is divisible by 2,
+ *     making it the smallest number greater than or equal to 10
+ *     that satisfies the condition.
+ *
+ * Example 2:
+ *   Input: n = 15, t = 3
+ *   Output: 16
+ *   Explanation:
+ *     The digit product of 16 is 6,
+ *     which is divisible by 3,
+ *     making it the smallest number greater than or equal to 15
+ *     that satisfies the condition.
+ *
+ * Constraints:
+ *   • 1 <= n <= 100
+ *   • 1 <= t <= 10
+ *
+ * Hint 1:
+ *   You have to check at most 10 numbers.
+ *
+ * Hint 2:
+ *   Apply a brute-force approach by checking each possible number.
+ **
+ * https://leetcode.com/problems/smallest-divisible-digit-product-i/
+***/
+
+namespace Problems;
+
+public class SmallestDivisibleDigitProductI
+{
+    public int SmallestNumber( int n, int t )
+    {
+        while ( DigitsProduct( n ) % t != 0 )
+        {
+            n++;
+        }
+
+        return n;
+    }
+
+    private int DigitsProduct( int n )
+    {
+        int product = 1;
+
+        while ( n > 0 )
+        {
+            product *= n % 10;
+            n /= 10;
+        }
+
+        return product;
+    }
+}
---
+++ c-sharp/Tests/math/SmallestDivisibleDigitProductITests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SmallestDivisibleDigitProductITests
+{
+    [TestCase( 10, 2, ExpectedResult = 10 )]
+    [TestCase( 15, 3, ExpectedResult = 16 )]
+    [TestCase( 1, 1, ExpectedResult = 1 )]
+    public int SmallestNumberTest( int n, int t ) =>
+        new SmallestDivisibleDigitProductI().SmallestNumber( n, t );
+}
