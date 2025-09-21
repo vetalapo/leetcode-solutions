84f52904b8aed571bc9c87b52cea555ea9c623f8
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-21 14:06:47 +0200

    #3678: Smallest Absest Positive Greater Than Average; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/SmallestAbsentPositiveGreaterThanAverageTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+public class SmallestAbsentPositiveGreaterThanAverageTests
+{
+    [TestCase( new int[] { 3, 5 }, ExpectedResult = 6 )]
+    [TestCase( new int[] { -1, 1, 2 }, ExpectedResult = 3 )]
+    [TestCase( new int[] { 4, -1 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { -34 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 98, 100 }, ExpectedResult = 101 )]
+    public int SmallestAbsentTest( int[] nums ) =>
+        new SmallestAbsentPositiveGreaterThanAverage().SmallestAbsent( nums );
+}
