1e70a1b4cf6a6260d08c1e03376386926fe56f75
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-22 16:37:34 +0200

    #2996: Smallest Missing Integer Greater Than Sequential Prefix Sum; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/SmallestMissingIntegerGreaterThanSequentialPrefixSum.cs
@@ -0,0 +1,86 @@
+﻿/**
+ * 2996
+ * Smallest Missing Integer Greater Than Sequential Prefix Sum
+ **
+ * You are given a 0-indexed array of integers nums.
+ * A prefix nums[0..i] is sequential if,
+ * for all 1 <= j <= i, nums[j] = nums[j - 1] + 1.
+ * In particular,
+ * the prefix consisting only of nums[0] is sequential.
+ *
+ * Return the smallest integer x missing from nums
+ * such that x is greater than or equal
+ * to the sum of the longest sequential prefix.
+ *
+ * Example 1:
+ *   Input: nums = [1,2,3,2,5]
+ *   Output: 6
+ *   Explanation:
+ *     The longest sequential prefix of nums is [1,2,3] with a sum of 6.
+ *     6 is not in the array,
+ *     therefore 6 is the smallest missing integer
+ *     greater than or equal to the sum of the longest sequential prefix.
+ *
+ * Example 2:
+ *   Input: nums = [3,4,5,1,12,14,13]
+ *   Output: 15
+ *   Explanation:
+ *     The longest sequential prefix of nums is [3,4,5] with a sum of 12.
+ *     12, 13, and 14 belong to the array while 15 does not.
+ *     Therefore 15 is the smallest missing integer
+ *     greater than or equal to the sum of the longest sequential prefix.
+ *
+ * Constraints:
+ *   • 1 <= nums.length <= 50
+ *   • 1 <= nums[i] <= 50
+ *
+ * Hint 1:
+ *   To find the longest sequential prefix,
+ *   iterate from left to right.
+ *   For a fixed i,
+ *   if nums[i] != nums[i - 1] + 1
+ *   then the longest sequential prefix ends at i - 1.
+ **
+ * https://leetcode.com/problems/smallest-missing-integer-greater-than-sequential-prefix-sum/
+***/
+
+using System.Collections.Generic;
+
+namespace Problems;
+
+public class SmallestMissingIntegerGreaterThanSequentialPrefixSum
+{
+    public int MissingInteger( int[] nums )
+    {
+        int prefixSum = nums[0];
+
+        bool isSeq = true;
+        Dictionary<int, bool> map = [];
+        map[nums[0]] = true;
+
+        for ( int i = 1; i < nums.Length; i++ )
+        {
+            if ( isSeq && nums[i] == nums[i - 1] + 1 )
+            {
+                prefixSum += nums[i];
+            }
+            else
+            {
+                if ( prefixSum > 50 )
+                {
+                    return prefixSum;
+                }
+
+                map[nums[i]] = true;
+                isSeq = false;
+            }
+        }
+
+        while ( map.ContainsKey( prefixSum ) )
+        {
+            prefixSum++;
+        }
+
+        return prefixSum;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/SmallestMissingIntegerGreaterThanSequentialPrefixSumTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Problems.ArraysAndHashing;
+
+public class SmallestMissingIntegerGreaterThanSequentialPrefixSumTests
+{
+    [TestCase( new int[] { 1, 2, 3, 2, 5 }, ExpectedResult = 6 )]
+    [TestCase( new int[] { 3, 4, 5, 1, 12, 14, 13 }, ExpectedResult = 15 )]
+    [TestCase( new int[] { 29, 30, 31, 32, 33, 34, 35, 36, 37 }, ExpectedResult = 297 )]
+    [TestCase( new int[] { 38 }, ExpectedResult = 39 )]
+    public int MissingIntegerTest( int[] nums ) =>
+        new SmallestMissingIntegerGreaterThanSequentialPrefixSum().MissingInteger( nums );
+}
