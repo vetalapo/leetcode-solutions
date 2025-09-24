c296820009fdf966e91a10be4b680398efba7fc2
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-24 23:24:42 +0200

    #2269: Find the K-Beauty of a Number; solution & tests.

---
+++ c-sharp/Tests/sliding-window/FindTheKBeautyOfANumberTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace SlidingWindow;
+
+public class FindTheKBeautyOfANumberTests
+{
+    [TestCase( 240, 2, ExpectedResult = 2 )]
+    [TestCase( 430043, 2, ExpectedResult = 2 )]
+    [TestCase( 10, 2, ExpectedResult = 1 )]
+    [TestCase( 2, 1, ExpectedResult = 1 )]
+    public int DivisorSubstringsTest( int num, int k ) =>
+        new FindTheKBeautyOfANumber().DivisorSubstrings( num, k );
+}
