d79c8256380d3e9b0916a00552b7e82389f30d3b
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-25 13:33:02 +0200

    #1773: Count Items Matching a Rule; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/CountItemsMatchingARule.cs
@@ -0,0 +1,71 @@
+﻿/**
+ * 1773
+ * Count Items Matching a Rule
+ **
+ * You are given an array items,
+ * where each items[i] = [typei, colori, namei]
+ * describes the type, color, and name of the ith item.
+ * You are also given a rule represented by two strings,
+ * ruleKey and ruleValue.
+ *
+ * The ith item is said to match the rule if one of the following is true:
+ *   • ruleKey == "type" and ruleValue == typei.
+ *   • ruleKey == "color" and ruleValue == colori.
+ *   • ruleKey == "name" and ruleValue == namei.
+ *
+ * Return the number of items that match the given rule.
+ *
+ * Example 1:
+ *   Input:
+ *     items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]],
+ *     ruleKey = "color",
+ *     ruleValue = "silver"
+ *   Output: 1
+ *   Explanation:
+ *     There is only one item matching the given rule,
+ *     which is ["computer","silver","lenovo"].
+ *
+ * Example 2:
+ *   Input:
+ *     items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]],
+ *     ruleKey = "type",
+ *     ruleValue = "phone"
+ *   Output: 2
+ *   Explanation:
+ *     There are only two items matching the given rule,
+ *     which are ["phone","blue","pixel"] and ["phone","gold","iphone"].
+ *     Note that the item ["computer","silver","phone"] does not match.
+ *
+ * Constraints:
+ *   • 1 <= items.length <= 10^4
+ *   • 1 <= typei.length, colori.length, namei.length, ruleValue.length <= 10
+ *   • ruleKey is equal to either "type", "color", or "name".
+ *   • All strings consist only of lowercase letters.
+ *
+ * Hint 1:
+ *   Iterate on each item,
+ *   and check if each one matches the rule according to the statement.
+ **
+ * https://leetcode.com/problems/count-items-matching-a-rule/
+***/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems;
+
+public class CountItemsMatchingARule
+{
+    public int CountMatches( IList<IList<string>> items, string ruleKey, string ruleValue )
+    {
+        int ruleId = ruleKey switch
+        {
+            "type" => 0,
+            "color" => 1,
+            "name" => 2,
+            _ => -1
+        };
+
+        return items.Count( item => item[ruleId] == ruleValue );
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/CountItemsMatchingARuleTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class CountItemsMatchingARuleTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int CountMatchesTest( IList<IList<string>> items, string ruleKey, string ruleValue ) =>
+        new CountItemsMatchingARule().CountMatches( items, ruleKey, ruleValue );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(
+                new string[][] { ["phone", "blue", "pixel"], ["computer", "silver", "lenovo"], ["phone", "gold", "iphone"] },
+                "color",
+                "silver"
+            ).Returns( 1 );
+
+            yield return new TestCaseData(
+                new string[][] { ["phone", "blue", "pixel"], ["computer", "silver", "phone"], ["phone", "gold", "iphone"] },
+                "type",
+                "phone"
+            ).Returns( 2 );
+        }
+    }
+}
