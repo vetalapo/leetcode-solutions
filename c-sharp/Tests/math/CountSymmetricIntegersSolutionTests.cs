3e78b3c00ea039b7fa46e41704c5bbaffa3884f9
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-26 17:49:45 +0200

    #2843: Count Symmetric Integers; solution & tests.

---
+++ c-sharp/Tests/math/CountSymmetricIntegersSolutionTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class CountSymmetricIntegersSolutionTests
+{
+    [TestCase( 1, 100, ExpectedResult = 9 )]
+    [TestCase( 1200, 1230, ExpectedResult = 4 )]
+    [TestCase( 100, 1000, ExpectedResult = 0 )]
+    public int CountSymmetricIntegersTest( int low, int high ) =>
+        new CountSymmetricIntegersSolution().CountSymmetricIntegers( low, high );
+}
