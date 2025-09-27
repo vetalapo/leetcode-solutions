a5fcfae8fbbf080dca06ad8add9491c195e14302
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-27 20:18:23 +0200

    #812: Largest Triangle Area; solution & tests.

---
+++ c-sharp/Problems/math/LargestTriangleArea.cs
@@ -0,0 +1,60 @@
+﻿/**
+ * 812
+ * Largest Triangle Area
+ **
+ * Given an array of points on the X-Y plane points
+ * where points[i] = [xi, yi],
+ * return the area of the largest triangle
+ * that can be formed by any three different points.
+ * Answers within 10-5 of the actual answer will be accepted.
+ *
+ * Example 1:
+ *   Input: points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
+ *   Output: 2.00000
+ *   Explanation:
+ *     The five points are shown in the above figure.
+ *     The red triangle is the largest.
+ *
+ * Example 2:
+ *   Input: points = [[1,0],[0,0],[0,1]]
+ *   Output: 0.50000
+ *
+ * Constraints:
+ *   • 3 <= points.length <= 50
+ *   • -50 <= xi, yi <= 50
+ *   • All the given points are unique.
+ **
+ * https://leetcode.com/problems/largest-triangle-area/
+***/
+
+using System;
+
+namespace Problems;
+
+public class LargestTriangleAreaSolution
+{
+    public double LargestTriangleArea( int[][] points )
+    {
+        double maxArea = 0;
+
+        for ( int i = 0; i < points.Length - 2; i++ )
+        {
+            for ( int j = i + 1; j < points.Length - 1; j++ )
+            {
+                for ( int k = j + 1; k < points.Length; k++ )
+                {
+                    // Shoelace formula
+                    double currArea = 0.5 * Math.Abs(
+                        points[i][0] * ( points[j][1] - points[k][1] ) +
+                        points[j][0] * ( points[k][1] - points[i][1] ) +
+                        points[k][0] * ( points[i][1] - points[j][1] )
+                    );
+
+                    maxArea = Math.Max( maxArea, currArea );
+                }
+            }
+        }
+
+        return maxArea;
+    }
+}
---
+++ c-sharp/Tests/math/LargestTriangleAreaSolutionTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class LargestTriangleAreaSolutionTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public double LargestTriangleAreaTest( int[][] points ) =>
+        new LargestTriangleAreaSolution().LargestTriangleArea( points );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( [new int[][] { [0, 0], [0, 1], [1, 0], [0, 2], [2, 0] }] )
+                .Returns( 2.0 );
+
+            yield return new TestCaseData( [new int[][] { [1, 0], [0, 0], [0, 1] }] )
+                .Returns( 0.5 );
+
+            yield return new TestCaseData( [new int[][] { [4, 6], [6, 5], [3, 1] }] )
+                .Returns( 5.5 );
+        }
+    }
+}
