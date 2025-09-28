a2b8590858bfecbe442e2c21e299bd71e1aa6bfc
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-28 12:03:42 +0200

    #976: Largest Perimeter Triangle; solution & tests.

---
+++ c-sharp/Problems/math/LargestPerimeterTriangle.cs
@@ -0,0 +1,54 @@
+﻿/**
+ * 976
+ * Largest Perimeter Triangle
+ **
+ * Given an integer array nums,
+ * return the largest perimeter of a triangle with a non-zero area,
+ * formed from three of these lengths.
+ * If it is impossible to form any triangle of a non-zero area,
+ * return 0.
+ *
+ * Example 1:
+ *   Input: nums = [2,1,2]
+ *   Output: 5
+ *   Explanation:
+ *     You can form a triangle with three side lengths: 1, 2, and 2.
+ *
+ * Example 2:
+ *   Input: nums = [1,2,1,10]
+ *   Output: 0
+ *   Explanation:
+ *     You cannot use the side lengths 1, 1, and 2 to form a triangle.
+ *     You cannot use the side lengths 1, 1, and 10 to form a triangle.
+ *     You cannot use the side lengths 1, 2, and 10 to form a triangle.
+ *     As we cannot use any three side lengths to form a triangle of non-zero area,
+ *     we return 0.
+ *
+ * Constraints:
+ *   • 3 <= nums.length <= 10^4
+ *   • 1 <= nums[i] <= 10^6
+ **
+ * https://leetcode.com/problems/largest-perimeter-triangle/
+***/
+
+using System;
+
+namespace Problems;
+
+public class LargestPerimeterTriangle
+{
+    public int LargestPerimeter( int[] nums )
+    {
+        Array.Sort( nums, ( a, b ) => b.CompareTo( a ) );
+
+        for ( int i = 0; i < nums.Length - 2; i++ )
+        {
+            if ( nums[i] < nums[i + 1] + nums[i + 2] )
+            {
+                return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ c-sharp/Tests/math/LargestPerimeterTriangleTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class LargestPerimeterTriangleTests
+{
+    [TestCase( new int[] { 2, 1, 2 }, ExpectedResult = 5 )]
+    [TestCase( new int[] { 1, 2, 1, 10 }, ExpectedResult = 0 )]
+    [TestCase( new int[] { 3, 6, 2, 3 }, ExpectedResult = 8 )]
+    public int LargestPerimeterTest( int[] nums ) =>
+        new LargestPerimeterTriangle().LargestPerimeter( nums );
+}
