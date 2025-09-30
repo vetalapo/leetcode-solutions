8a5be8a24cf7b38b70995e59a16922c1272c0c1d
Vitaly Dushkin <vetalapo@gmail.com>
2025-09-30 22:08:37 +0200

    #2760: Longest Even Odd Subarray With Threshold; solution & tests.

---
+++ c-sharp/Tests/sliding-window/LongestEvenOddSubarrayWithThresholdTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace SlidingWindow;
+
+public class LongestEvenOddSubarrayWithThresholdTests
+{
+    [TestCase( new int[] { 3, 2, 5, 4 }, 5, ExpectedResult = 3 )]
+    [TestCase( new int[] { 1, 2 }, 2, ExpectedResult = 1 )]
+    [TestCase( new int[] { 2, 3, 4, 5 }, 4, ExpectedResult = 3 )]
+    public int LongestAlternatingSubarrayTest( int[] nums, int threshold ) =>
+        new LongestEvenOddSubarrayWithThreshold().LongestAlternatingSubarray( nums, threshold );
+}
