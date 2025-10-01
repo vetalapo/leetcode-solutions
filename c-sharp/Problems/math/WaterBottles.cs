d93d864e1d75fc1770054e7b0e9bb353ac53159c
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-01 14:30:22 +0200

    #1518: Water Bottles; solution & tests.

---
+++ c-sharp/Problems/math/WaterBottles.cs
@@ -0,0 +1,65 @@
+﻿/**
+ * 1518
+ * Water Bottles
+ **
+ * There are numBottles water bottles
+ * that are initially full of water.
+ * You can exchange numExchange empty water bottles
+ * from the market with one full water bottle.
+ *
+ * The operation of drinking a full water bottle
+ * turns it into an empty bottle.
+ *
+ * Given the two integers numBottles and numExchange,
+ * return the maximum number of water bottles you can drink.
+ *
+ * Example 1:
+ *   Input: numBottles = 9, numExchange = 3
+ *   Output: 13
+ *   Explanation:
+ *     You can exchange 3 empty bottles to get 1 full water bottle.
+ *     Number of water bottles you can drink: 9 + 3 + 1 = 13.
+ *
+ * Example 2:
+ *   Input: numBottles = 15, numExchange = 4
+ *   Output: 19
+ *   Explanation:
+ *     You can exchange 4 empty bottles to get 1 full water bottle. 
+ *     Number of water bottles you can drink: 15 + 3 + 1 = 19.
+ *
+ * Constraints:
+ *   • 1 <= numBottles <= 100
+ *   • 2 <= numExchange <= 100
+ *
+ * Hint 1:
+ *   Simulate the process until
+ *   there are not enough empty bottles
+ *   for even one full bottle of water.
+ **
+ * https://leetcode.com/problems/water-bottles/
+***/
+
+namespace Problems;
+
+public class WaterBottles
+{
+    public int NumWaterBottles( int numBottles, int numExchange )
+    {
+        return numBottles + ( ( numBottles - 1 ) / ( numExchange - 1 ) );
+    }
+
+    public int NumWaterBottlesIterative( int numBottles, int numExchange )
+    {
+        int result = numBottles;
+
+        while ( numBottles >= numExchange )
+        {
+            int refill = numBottles / numExchange;
+            result += refill;
+            numBottles -= refill * numExchange;
+            numBottles += refill;
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/math/WaterBottlesTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class WaterBottlesTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int NumWaterBottlesTest( int numBottles, int numExchange ) =>
+        new WaterBottles().NumWaterBottles( numBottles, numExchange );
+
+
+    [TestCaseSource( nameof( TestCases ) )]
+    public int NumWaterBottlesIterativeTest( int numBottles, int numExchange ) =>
+        new WaterBottles().NumWaterBottlesIterative( numBottles, numExchange );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( 9, 3 ).Returns( 13 );
+
+            yield return new TestCaseData( 15, 4 ).Returns( 19 );
+
+            yield return new TestCaseData( 5, 5 ).Returns( 6 );
+        }
+    }
+}
