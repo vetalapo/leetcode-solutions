2f2260c1189c97980f6e576ac8ea2f709b025901
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-02 23:58:47 +0200

    #1534: Count Good Triplets; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/CountGoodTriplets.cs
@@ -0,0 +1,77 @@
+﻿/**
+ * 1534
+ * Count Good Triplets
+ **
+ * Given an array of integers arr, and three integers a, b and c.
+ * You need to find the number of good triplets.
+ *
+ * A triplet (arr[i], arr[j], arr[k]) is good
+ * if the following conditions are true:
+ *   • 0 <= i < j < k < arr.length
+ *   • |arr[i] - arr[j]| <= a
+ *   • |arr[j] - arr[k]| <= b
+ *   • |arr[i] - arr[k]| <= c
+ *
+ * Where |x| denotes the absolute value of x.
+ *
+ * Return the number of good triplets.
+ *
+ * Example 1:
+ *   Input: arr = [3,0,1,1,9,7], a = 7, b = 2, c = 3
+ *   Output: 4
+ *   Explanation:
+ *     There are 4 good triplets:
+ *     [(3,0,1), (3,0,1), (3,1,1), (0,1,1)].
+ *
+ * Example 2:
+ *   Input: arr = [1,1,2,2,3], a = 0, b = 0, c = 1
+ *   Output: 0
+ *   Explanation:
+ *     No triplet satisfies all conditions.
+ *
+ * Constraints:
+ *   • 3 <= arr.length <= 100
+ *   • 0 <= arr[i] <= 1000
+ *   • 0 <= a, b, c <= 1000
+ *
+ * Hint 1:
+ *   Notice that the constraints are small enough
+ *   for a brute force solution to pass.
+ *
+ * Hint 2:
+ *   Loop through all triplets,
+ *   and count the ones that are good.
+ **
+ * https://leetcode.com/problems/count-good-triplets/
+***/
+
+using System;
+
+namespace Problems;
+
+public class CountGoodTripletsSolution
+{
+    public int CountGoodTriplets( int[] arr, int a, int b, int c )
+    {
+        int count = 0;
+
+        for ( int i = 0; i < arr.Length - 2; i++ )
+        {
+            for ( int j = i + 1; j < arr.Length - 1; j++ )
+            {
+                if ( Math.Abs( arr[i] - arr[j] ) <= a )
+                {
+                    for ( int k = j + 1; k < arr.Length; k++ )
+                    {
+                        if ( Math.Abs( arr[j] - arr[k] ) <= b && Math.Abs( arr[i] - arr[k] ) <= c )
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/CountGoodTripletsSolutionTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class CountGoodTripletsSolutionTests
+{
+    [TestCase( new int[] { 3, 0, 1, 1, 9, 7 }, 7, 2, 3, ExpectedResult = 4 )]
+    [TestCase( new int[] { 1, 1, 2, 2, 3 }, 0, 0, 1, ExpectedResult = 0 )]
+    public int CountGoodTripletsTest( int[] arr, int a, int b, int c ) =>
+        new CountGoodTripletsSolution().CountGoodTriplets( arr, a, b, c );
+}
