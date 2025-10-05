9f8babd3b490a55bf7fe5310d9b16f5e41f182f3
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-05 21:19:04 +0200

    #1920: improve namespacing

--- c-sharp/Tests/arrays-and-hashing/BuildArrayFromPermutationTests.cs
+++ c-sharp/Tests/arrays-and-hashing/BuildArrayFromPermutationTests.cs
@@ -2,6 +2,8 @@
 
 using Problems;
 
+namespace ArraysAndHashing;
+
 public class BuildArrayFromPermutationTests
 {
     [TestCase( new int[] { 0, 2, 1, 5, 3, 4 }, ExpectedResult = new int[] { 0, 1, 2, 4, 5, 3 } )]
