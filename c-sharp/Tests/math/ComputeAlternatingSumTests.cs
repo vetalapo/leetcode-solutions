1e9f021ae4f52e03bb24594a5f8c7f1801141b70
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-06 23:38:40 +0200

    #3701: Comput Alternating Sum; solution & tests.

---
+++ c-sharp/Tests/math/ComputeAlternatingSumTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class ComputeAlternatingSumTests
+{
+    [TestCase( new int[] { 1, 3, 5, 7 }, ExpectedResult = -4 )]
+    [TestCase( new int[] { 100 }, ExpectedResult = 100 )]
+    public int AlternatingSumTest( int[] nums ) =>
+        new ComputeAlternatingSum().AlternatingSum( nums );
+}
