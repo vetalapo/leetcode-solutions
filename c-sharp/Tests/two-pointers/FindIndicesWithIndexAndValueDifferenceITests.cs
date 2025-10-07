8e7df7cecf351df1b2b0c2f6b9d72fbc05894195
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-07 22:06:23 +0200

    #2903: Find Indices With Index and Value Difference I; solution & tests.

---
+++ c-sharp/Tests/two-pointers/FindIndicesWithIndexAndValueDifferenceITests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace TwoPointers;
+
+public class FindIndicesWithIndexAndValueDifferenceITests
+{
+    [TestCase( new int[] { 5, 1, 4, 1 }, 2, 4, ExpectedResult = new int[] { 0, 3 } )]
+    [TestCase( new int[] { 2, 1 }, 0, 0, ExpectedResult = new int[] { 0, 0 } )]
+    [TestCase( new int[] { 1, 2, 3 }, 2, 4, ExpectedResult = new int[] { -1, -1 } )]
+    public int[] FindIndicesTest( int[] nums, int indexDifference, int valueDifference ) =>
+        new FindIndicesWithIndexAndValueDifferenceI().FindIndices( nums, indexDifference, valueDifference );
+}
