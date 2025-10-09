5e0d5e0b76c2e40be539b921848a0b0fb3043eb2
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-09 11:56:40 +0200

    #2160: Minimum Sum of Four Digit Number After Splitting Digits; solution & tests.

---
+++ c-sharp/Problems/math/MinimumSumOfFourDigitNumberAfterSplittingDigits.cs
@@ -0,0 +1,92 @@
+﻿/**
+ * 2160
+ * Minimum Sum of Four Digit Number After Splitting Digits
+ **
+ * You are given a positive integer num consisting of exactly four digits.
+ * Split num into two new integers new1 and new2
+ * by using the digits found in num.
+ * Leading zeros are allowed in new1 and new2,
+ * and all the digits found in num must be used.
+ *
+ * For example,
+ * given num = 2932,
+ * you have the following digits:
+ * two 2's, one 9 and one 3.
+ * Some of the possible pairs [new1, new2]
+ * are [22, 93], [23, 92], [223, 9] and [2, 329].
+ *
+ * Return the minimum possible sum of new1 and new2.
+ *
+ * Example 1:
+ *   Input: num = 2932
+ *   Output: 52
+ *   Explanation:
+ *     Some possible pairs [new1, new2] are [29, 23], [223, 9], etc.
+ *     The minimum sum can be obtained by the pair [29, 23]: 29 + 23 = 52.
+ *
+ * Example 2:
+ *   Input: num = 4009
+ *   Output: 13
+ *   Explanation:
+ *     Some possible pairs [new1, new2] are [0, 49], [490, 0], etc.
+ *     The minimum sum can be obtained by the pair [4, 9]: 4 + 9 = 13.
+ *
+ * Constraints:
+ *   • 1000 <= num <= 9999
+ *
+ * Hint 1:
+ *   Notice that the most optimal way to obtain the minimum possible sum
+ *   using 4 digits is by summing up two 2-digit numbers.
+ *
+ * Hint 2:
+ *   We can use the two smallest digits out of the four
+ *   as the digits found in the tens place respectively.
+ *
+ * Hint 3:
+ *   Similarly,
+ *   we use the final 2 larger digits
+ *   as the digits found in the ones place.
+ **
+ * https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/
+***/
+
+namespace Problems;
+
+public class MinimumSumOfFourDigitNumberAfterSplittingDigits
+{
+    public int MinimumSum( int num )
+    {
+        int[] map = new int[10];
+
+        while ( num > 0 )
+        {
+            map[num % 10]++;
+            num /= 10;
+        }
+
+        int new1 = 0;
+        int new2 = 0;
+
+        bool nFlip = true;
+
+        for ( int i = 0; i < map.Length; i++ )
+        {
+            while ( map[i] > 0 )
+            {
+                if ( nFlip )
+                {
+                    new1 = new1 * 10 + i;
+                }
+                else
+                {
+                    new2 = new2 * 10 + i;
+                }
+
+                map[i]--;
+                nFlip = !nFlip;
+            }
+        }
+
+        return new1 + new2;
+    }
+}
---
+++ c-sharp/Tests/math/MinimumSumOfFourDigitNumberAfterSplittingDigitsTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class MinimumSumOfFourDigitNumberAfterSplittingDigitsTests
+{
+    [TestCase( 2932, ExpectedResult = 52 )]
+    [TestCase( 4009, ExpectedResult = 13 )]
+    public int MinimumSumTest( int num ) =>
+        new MinimumSumOfFourDigitNumberAfterSplittingDigits().MinimumSum( num );
+}
