4c931b3c72d53d0f35387d43cc24db233821df51
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-11 22:37:37 +0200

    #3688: Bitwise OR of Even Numbers in an Array; solution & tests.

---
+++ c-sharp/Tests/bit-manipulation/BitwiseOrOfEvenNumbersInAnArrayTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace BitManipulation;
+
+public class BitwiseOrOfEvenNumbersInAnArrayTests
+{
+    [TestCase( new int[] { 1, 2, 3, 4, 5, 6 }, ExpectedResult = 6 )]
+    [TestCase( new int[] { 7, 9, 11 }, ExpectedResult = 0 )]
+    [TestCase( new int[] { 1, 8, 16 }, ExpectedResult = 24 )]
+    public int EvenNumberBitwiseORsTest( int[] nums ) =>
+        new BitwiseOrOfEvenNumbersInAnArray().EvenNumberBitwiseORs( nums );
+}
