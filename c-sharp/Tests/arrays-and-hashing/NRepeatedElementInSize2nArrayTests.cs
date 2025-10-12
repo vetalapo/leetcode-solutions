ef8e1ed485be100f805d258a933bf761cfe15688
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-12 23:21:58 +0200

    #961: N-Repeated Element in Size 2N Array; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/NRepeatedElementInSize2nArrayTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class NRepeatedElementInSize2nArrayTests
+{
+    [TestCase( new int[] { 1, 2, 3, 3 }, ExpectedResult = 3 )]
+    [TestCase( new int[] { 2, 1, 2, 5, 3, 2 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { 5, 1, 5, 2, 5, 3, 5, 4 }, ExpectedResult = 5 )]
+    public int RepeatedNTimesTest( int[] nums ) =>
+        new NRepeatedElementInSize2nArray().RepeatedNTimes( nums );
+}
