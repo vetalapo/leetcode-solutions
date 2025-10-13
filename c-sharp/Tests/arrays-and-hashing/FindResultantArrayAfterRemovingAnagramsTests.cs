d3cb72bfee40e9a493e05af84890c73f003b4ecc
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-13 18:55:26 +0200

    #2273: Find Resultant Array After Removing Anagrams; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/FindResultantArrayAfterRemovingAnagramsTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class FindResultantArrayAfterRemovingAnagramsTests
+{
+    [TestCase( [new string[] { "abba", "baba", "bbaa", "cd", "cd" }], ExpectedResult = new string[] { "abba", "cd" } )]
+    [TestCase( [new string[] { "a", "b", "c", "d", "e" }], ExpectedResult = new string[] { "a", "b", "c", "d", "e" } )]
+    [TestCase( [new string[] { "a", "b", "a" }], ExpectedResult = new string[] { "a", "b", "a" } )]
+    public IList<string> RemoveAnagramsTest( string[] words ) =>
+        new FindResultantArrayAfterRemovingAnagrams().RemoveAnagrams( words );
+}
