4fce9972d0d37866e2620fd91eba56d51e399bb4
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-14 18:44:43 +0200

    #3349: Adjacent Increasing Subarrays Detection I; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/AdjacentIncreasingSubarraysDetectionITests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class AdjacentIncreasingSubarraysDetectionITests
+{
+    [TestCase( new int[] { 2, 5, 7, 8, 9, 2, 3, 4, 3, 1 }, 3, ExpectedResult = true )]
+    [TestCase( new int[] { 1, 2, 3, 4, 4, 4, 4, 5, 6, 7 }, 5, ExpectedResult = false )]
+    [TestCase( new int[] { -15, 19 }, 1, ExpectedResult = true )]
+    [TestCase( new int[] { -15, 3, 16, 0 }, 2, ExpectedResult = false )]
+    public bool HasIncreasingSubarraysTest( IList<int> nums, int k ) =>
+        new AdjacentIncreasingSubarraysDetectionI().HasIncreasingSubarrays( nums, k );
+}
