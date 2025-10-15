5425c3ef02ea93e4b2bda3cfb84fea73fb1e1a70
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-15 23:57:44 +0200

    #3712: Sum of Elements With Frequency Divisible by K; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/SumOfElementsWithFrequencyDivisibleByKTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class SumOfElementsWithFrequencyDivisibleByKTests
+{
+    [TestCase( new int[] { 1, 2, 2, 3, 3, 3, 3, 4 }, 2, ExpectedResult = 16 )]
+    [TestCase( new int[] { 1, 2, 3, 4, 5 }, 2, ExpectedResult = 0 )]
+    [TestCase( new int[] { 4, 4, 4, 1, 2, 3 }, 3, ExpectedResult = 12 )]
+    public int SumDivisibleByKTest( int[] nums, int k ) =>
+        new SumOfElementsWithFrequencyDivisibleByK().SumDivisibleByK( nums, k );
+}
