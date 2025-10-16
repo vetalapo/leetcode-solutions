a82607fbcd180c67082d7430c5c932f8d4894c70
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-16 19:54:18 +0200

    #2357: Make Array Zero by Subtracting Equal Amounts; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/MakeArrayZeroBySubtractingEqualAmountsTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class MakeArrayZeroBySubtractingEqualAmountsTests
+{
+    [TestCase( new int[] { 1, 5, 0, 3, 5 }, ExpectedResult = 3 )]
+    [TestCase( new int[] { 0 }, ExpectedResult = 0 )]
+    public int MinimumOperationsTest( int[] nums ) =>
+        new MakeArrayZeroBySubtractingEqualAmounts().MinimumOperations( nums );
+}
