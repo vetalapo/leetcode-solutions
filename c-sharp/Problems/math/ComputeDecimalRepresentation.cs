d16eb50a9c575eb03b58cf7977d6f8ba567538ef
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-17 13:30:22 +0200

    #3697: Compute Decimal Representation; solution & tests.

---
+++ c-sharp/Problems/math/ComputeDecimalRepresentation.cs
@@ -0,0 +1,81 @@
+﻿/**
+ * 3697
+ * Compute Decimal Representation
+ **
+ * You are given a positive integer n.
+ *
+ * A positive integer is a base-10 component
+ * if it is the product of a single digit from 1 to 9
+ * and a non-negative power of 10.
+ * For example,
+ * 500, 30, and 7 are base-10 components, while 537, 102, and 11 are not.
+ *
+ * Express n as a sum of only base-10 components,
+ * using the fewest base-10 components possible.
+ *
+ * Return an array containing these base-10 components in descending order.
+ *
+ * Example 1:
+ *   Input: n = 537
+ *   Output: [500,30,7]
+ *   Explanation:
+ *     We can express 537 as 500 + 30 + 7.
+ *     It is impossible to express 537
+ *     as a sum using fewer than 3 base-10 components.
+ *
+ * Example 2:
+ *   Input: n = 102
+ *   Output: [100,2]
+ *   Explanation:
+ *     We can express 102 as 100 + 2.
+ *     102 is not a base-10 component,
+ *     which means 2 base-10 components are needed.
+ *
+ * Example 3:
+ *   Input: n = 6
+ *   Output: [6]
+ *   Explanation:
+ *     6 is a base-10 component.
+ *
+ * Constraints:
+ *   • 1 <= n <= 10^9
+ *
+ * Hint 1:
+ *   Break the number down digit by digit starting from the ones place.
+ *
+ * Hint 2:
+ *   Each nonzero digit contributes a base-10 component.
+ *
+ * Hint 3:
+ *   Collect all components and return them in descending order.
+ **
+ * https://leetcode.com/problems/compute-decimal-representation/
+***/
+
+using System.Collections.Generic;
+
+namespace Problems;
+
+public class ComputeDecimalRepresentation
+{
+    public int[] DecimalRepresentation( int n )
+    {
+        List<int> result = [];
+        int pow = 1;
+
+        while ( n > 0 )
+        {
+            int curr = ( n % 10 ) * pow;
+
+            if ( curr > 0 )
+            {
+                result.Insert( 0, curr );
+            }
+
+            n /= 10;
+            pow *= 10;
+        }
+
+        return result.ToArray();
+    }
+}
---
+++ c-sharp/Tests/math/ComputeDecimalRepresentationTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class ComputeDecimalRepresentationTests
+{
+    [TestCase( 537, ExpectedResult = new int[] { 500, 30, 7 } )]
+    [TestCase( 102, ExpectedResult = new int[] { 100, 2 } )]
+    [TestCase( 6, ExpectedResult = new int[] { 6 } )]
+    public int[] DecimalRepresentationTest( int n ) =>
+        new ComputeDecimalRepresentation().DecimalRepresentation( n );
+}
