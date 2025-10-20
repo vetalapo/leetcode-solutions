ccbc3773d35c28a686cc4e629119a4f4609e334d
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-20 20:29:23 +0200

    #2525: Categorize Box According to Criteria; solution & tests.

---
+++ c-sharp/Problems/math/CategorizeBoxAccordingToCriteria.cs
@@ -0,0 +1,78 @@
+﻿/**
+ * 2525
+ * Categorize Box According to Criteria
+ **
+ * Given four integers length, width, height, and mass,
+ * representing the dimensions and mass of a box, respectively,
+ * return a string representing the category of the box.
+ *
+ * • The box is "Bulky" if:
+ *     - Any of the dimensions of the box is greater or equal to 10^4.
+ *     - Or, the volume of the box is greater or equal to 10^9.
+ * • If the mass of the box is greater or equal to 100, it is "Heavy".
+ * • If the box is both "Bulky" and "Heavy", then its category is "Both".
+ * • If the box is neither "Bulky" nor "Heavy", then its category is "Neither".
+ * • If the box is "Bulky" but not "Heavy", then its category is "Bulky".
+ * • If the box is "Heavy" but not "Bulky", then its category is "Heavy".
+ *
+ * Note that the volume of the box is the product of its length, width and height.
+ *
+ * Example 1:
+ *   Input: length = 1000, width = 35, height = 700, mass = 300
+ *   Output: "Heavy"
+ *   Explanation:
+ *     None of the dimensions of the box is greater or equal to 10^4.
+ *     Its volume = 24500000 <= 10^9. So it cannot be categorized as "Bulky".
+ *     However mass >= 100, so the box is "Heavy".
+ *     Since the box is not "Bulky" but "Heavy", we return "Heavy".
+ *
+ * Example 2:
+ *   Input: length = 200, width = 50, height = 800, mass = 50
+ *   Output: "Neither"
+ *   Explanation:
+ *     None of the dimensions of the box is greater or equal to 10^4.
+ *     Its volume = 8 * 10^6 <= 10^9. So it cannot be categorized as "Bulky".
+ *     Its mass is also less than 100, so it cannot be categorized as "Heavy" either.
+ *     Since its neither of the two above categories, we return "Neither".
+ *
+ * Constraints:
+ *   • 1 <= length, width, height <= 10^5
+ *   • 1 <= mass <= 10^3
+ *
+ * Hint 1:
+ *   Use conditional statements to find the right category of the box.
+ **
+ * https://leetcode.com/problems/categorize-box-according-to-criteria/
+***/
+
+namespace Problems;
+
+public class CategorizeBoxAccordingToCriteria
+{
+    public string CategorizeBox( int length, int width, int height, int mass )
+    {
+        bool isBulky = length >= 10000 ||
+            width >= 10000 ||
+            height >= 10000 ||
+            GetVolume( length, width, height ) >= 1000000000;
+
+        bool isHeavy = mass >= 100;
+
+        if ( isBulky && isHeavy )
+        {
+            return "Both";
+        }
+
+        if ( !isBulky && !isHeavy )
+        {
+            return "Neither";
+        }
+
+        return isBulky ? "Bulky" : "Heavy";
+    }
+
+    private long GetVolume( int length, int width, int height )
+    {
+        return length * width * (long)height;
+    }
+}
---
+++ c-sharp/Tests/math/CategorizeBoxAccordingToCriteriaTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class CategorizeBoxAccordingToCriteriaTests
+{
+    [TestCase( 1000, 35, 700, 300, ExpectedResult = "Heavy" )]
+    [TestCase( 200, 50, 800, 50, ExpectedResult = "Neither" )]
+    [TestCase( 2909, 3968, 3272, 727, ExpectedResult = "Both" )]
+    public string CategorizeBoxTest( int length, int width, int height, int mass ) =>
+        new CategorizeBoxAccordingToCriteria().CategorizeBox( length, width, height, mass );
+}
