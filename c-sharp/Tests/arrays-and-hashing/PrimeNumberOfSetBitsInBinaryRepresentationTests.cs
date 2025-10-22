e3c7817bd9d88830fb25f3449c8149048ac824f6
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-22 21:57:33 +0200

    #762: Prime Number of Set Bits in Binary Representation; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/PrimeNumberOfSetBitsInBinaryRepresentationTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class PrimeNumberOfSetBitsInBinaryRepresentationTests
+{
+    [TestCase( 6, 10, ExpectedResult = 4 )]
+    [TestCase( 10, 15, ExpectedResult = 5 )]
+    public int CountPrimeSetBitsTest( int left, int right ) =>
+        new PrimeNumberOfSetBitsInBinaryRepresentation().CountPrimeSetBits( left, right );
+}
