f05221f1eaa2c4b4db8b66892341d86d10b3e5fd
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-23 15:31:10 +0200

    #3461: Check If Digits Are Equal in String After Operations I; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/CheckIfDigitsAreEqualInStringAfterOperationsITests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class CheckIfDigitsAreEqualInStringAfterOperationsITests
+{
+    [TestCase( "3902", ExpectedResult = true )]
+    [TestCase( "34789", ExpectedResult = false )]
+    public bool HasSameDigitsTest( string s ) =>
+        new CheckIfDigitsAreEqualInStringAfterOperationsI().HasSameDigits( s );
+}
