20b38456c8875c222f4aa6bb9ccc53e5e292e8f3
Vitaly Dushkin <vetalapo@gmail.com>
2025-10-26 20:02:41 +0100

    #2042: Improve namespacing

--- c-sharp/Tests/arrays-and-hashing/CheckIfNumbersAreAscendingInASentenceTests.cs
+++ c-sharp/Tests/arrays-and-hashing/CheckIfNumbersAreAscendingInASentenceTests.cs
@@ -2,6 +2,8 @@
 
 using Problems;
 
+namespace ArraysAndHashing;
+
 public class CheckIfNumbersAreAscendingInASentenceTests
 {
     [TestCase( "1 box has 3 blue 4 red 6 green and 12 yellow marbles", ExpectedResult = true )]
