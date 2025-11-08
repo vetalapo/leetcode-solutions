996f8570e01134d21daa2d695e3041cb42795a3d
Vitaly Dushkin <vetalapo@gmail.com>
2025-11-08 15:04:16 +0100

    #1437: Check If All 1's Are at Least Length K Places Away; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/CheckIfAll1sAreAtLeastLengthKPlacesAwayTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class CheckIfAll1sAreAtLeastLengthKPlacesAwayTests
+{
+    [TestCase( new int[] { 1, 0, 0, 0, 1, 0, 0, 1 }, 2, ExpectedResult = true )]
+    [TestCase( new int[] { 1, 0, 0, 1, 0, 1 }, 2, ExpectedResult = false )]
+    public bool KLengthApartTest( int[] nums, int k ) =>
+        new CheckIfAll1sAreAtLeastLengthKPlacesAway().KLengthApart( nums, k );
+}
