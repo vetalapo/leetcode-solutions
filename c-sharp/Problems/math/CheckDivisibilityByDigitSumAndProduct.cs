6e08b27bab8b6237787c08945b6ca4f3de9bc569
Vitaly Dushkin <vetalapo@gmail.com>
2025-11-10 19:14:01 +0100

    #3622: Check Divisibility by Digit Sum and Product; solution & tests.

---
+++ c-sharp/Problems/math/CheckDivisibilityByDigitSumAndProduct.cs
@@ -0,0 +1,59 @@
+﻿/**
+ * 3622
+ * Check Divisibility by Digit Sum and Product
+ **
+ * You are given a positive integer n.
+ * Determine whether n is divisible
+ * by the sum of the following two values:
+ *   • The digit sum of n (the sum of its digits).
+ *   • The digit product of n (the product of its digits).
+ *
+ * Return true if n is divisible by this sum; otherwise, return false.
+ *
+ * Example 1:
+ *   Input: n = 99
+ *   Output: true
+ *   Explanation:
+ *     Since 99 is divisible by the sum (9 + 9 = 18)
+ *     plus product (9 * 9 = 81) of its digits (total 99),
+ *     the output is true.
+ *
+ * Example 2:
+ *   Input: n = 23
+ *   Output: false
+ *   Explanation:
+ *     Since 23 is not divisible by the sum (2 + 3 = 5)
+ *     plus product (2 * 3 = 6) of its digits (total 11),
+ *     the output is false.
+ *
+ * Constraints:
+ *   • 1 <= n <= 10^6
+ **
+ * https://leetcode.com/problems/check-divisibility-by-digit-sum-and-product/
+***/
+
+namespace Problems;
+
+public class CheckDivisibilityByDigitSumAndProduct
+{
+    public bool CheckDivisibility( int n )
+    {
+        return n % DigitSumAndProduct( n ) == 0;
+    }
+
+    private int DigitSumAndProduct( int n )
+    {
+        int sum = 0;
+        int product = 1;
+
+        while ( n > 0 )
+        {
+            sum += n % 10;
+            product *= n % 10;
+
+            n /= 10;
+        }
+
+        return sum + product;
+    }
+}
---
+++ c-sharp/Tests/math/CheckDivisibilityByDigitSumAndProductTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class CheckDivisibilityByDigitSumAndProductTests
+{
+    [TestCase( 99, ExpectedResult = true )]
+    [TestCase( 23, ExpectedResult = false )]
+    public bool CheckDivisibilityTest( int n ) =>
+        new CheckDivisibilityByDigitSumAndProduct().CheckDivisibility( n );
+}
