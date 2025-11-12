76eb52d1485d3d6c4d0683d89eeeea286bd92e90
Vitaly Dushkin <vetalapo@gmail.com>
2025-11-12 12:55:58 +0100

    #3492: Maximum Containers on a Ship; solution & test.

---
+++ c-sharp/Tests/math/MaximumContainersOnAShipTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class MaximumContainersOnAShipTests
+{
+    [TestCase( 2, 3, 15, ExpectedResult = 4 )]
+    [TestCase( 3, 5, 20, ExpectedResult = 4 )]
+    public int MaxContainersTest( int n, int w, int maxWeight ) =>
+        new MaximumContainersOnAShip().MaxContainers( n, w, maxWeight );
+}
