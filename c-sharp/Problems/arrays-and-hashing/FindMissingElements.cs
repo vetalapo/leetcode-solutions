baad8c481e0e31b8b0f1b0eaa185a6cba6693db5
Vitaly Dushkin <vetalapo@gmail.com>
2025-11-16 16:14:00 +0100

    #3731: Find Missing Elements; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/FindMissingElements.cs
@@ -0,0 +1,91 @@
+﻿/**
+ * 3731
+ * Find Missing Elements
+ **
+ * You are given an integer array nums consisting of unique integers.
+ *
+ * Originally, nums contained every integer within a certain range.
+ * However, some integers might have gone missing from the array.
+ *
+ * The smallest and largest integers of the original range
+ * are still present in nums.
+ *
+ * Return a sorted list of all the missing integers in this range.
+ * If no integers are missing, return an empty list.
+ *
+ * Example 1:
+ *   Input: nums = [1,4,2,5]
+ *   Output: [3]
+ *   Explanation:
+ *     The smallest integer is 1 and the largest is 5,
+ *     so the full range should be [1,2,3,4,5].
+ *     Among these, only 3 is missing.
+ *
+ * Example 2:
+ *   Input: nums = [7,8,6,9]
+ *   Output: []
+ *   Explanation:
+ *     The smallest integer is 6 and the largest is 9,
+ *     so the full range is [6,7,8,9].
+ *     All integers are already present,
+ *     so no integer is missing.
+ *
+ * Example 3:
+ *   Input: nums = [5,1]
+ *   Output: [2,3,4]
+ *   Explanation:
+ *     The smallest integer is 1 and the largest is 5,
+ *     so the full range should be [1,2,3,4,5].
+ *     The missing integers are 2, 3, and 4.
+ *
+ * Constraints:
+ *   • 2 <= nums.length <= 100
+ *   • 1 <= nums[i] <= 100
+ *
+ * Hint 1:
+ *   First, find the maximum and minimum elements in the array.
+ *
+ * Hint 2:
+ *   Then, iterate over all the integers in the range [min, max]
+ *   and check if they are in the array.
+ *
+ * Hint 3:
+ *   If not, add them to the array,
+ *   and return the sorted array at the end.
+ **
+ * https://leetcode.com/problems/find-missing-elements/
+***/
+
+using System;
+using System.Collections.Generic;
+
+namespace Problems;
+
+public class FindMissingElementsSolution
+{
+    public IList<int> FindMissingElements( int[] nums )
+    {
+        int min = nums[0];
+        int max = nums[0];
+        bool[] map = new bool[101];
+
+        foreach ( int num in nums )
+        {
+            min = Math.Min( min, num );
+            max = Math.Max( max, num );
+            map[num] = true;
+        }
+
+        List<int> result = [];
+
+        for ( int i = min + 1; i < max; i++ )
+        {
+            if ( !map[i] )
+            {
+                result.Add( i );
+            }
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/FindMissingElementsSolutionTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class FindMissingElementsSolutionTests
+{
+    [TestCase( new int[] { 1, 4, 2, 5 }, ExpectedResult = new int[] { 3 } )]
+    [TestCase( new int[] { 7, 8, 6, 9 }, ExpectedResult = new int[] { } )]
+    [TestCase( new int[] { 5, 1 }, ExpectedResult = new int[] { 2, 3, 4 } )]
+    public IList<int> FindMissingElementsTest( int[] nums ) =>
+        new FindMissingElementsSolution().FindMissingElements( nums );
+}
