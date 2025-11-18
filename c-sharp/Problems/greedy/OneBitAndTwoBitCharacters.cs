2b08e5efbd632e32d8ae4a098c75cb3b923866e7
Vitaly Dushkin <vetalapo@gmail.com>
2025-11-18 23:38:55 +0100

    #717: 1-bit and 2-bit Characters; solution & tests.

---
+++ c-sharp/Problems/greedy/OneBitAndTwoBitCharacters.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * 717
+ * 1-bit and 2-bit Characters
+ **
+ * We have two special characters:
+ *   • The first character can be represented by one bit 0.
+ *   • The second character can be represented by two bits (10 or 11).
+ *
+ * Given a binary array bits that ends with 0,
+ * return true if the last character must be a one-bit character.
+ *
+ * Example 1:
+ *   Input: bits = [1,0,0]
+ *   Output: true
+ *   Explanation:
+ *     The only way to decode it is two-bit character and one-bit character.
+ *     So the last character is one-bit character.
+ *
+ * Example 2:
+ *   Input: bits = [1,1,1,0]
+ *   Output: false
+ *   Explanation:
+ *     The only way to decode it is two-bit character and two-bit character.
+ *     So the last character is not one-bit character.
+ *
+ * Constraints:
+ *   • 1 <= bits.length <= 1000
+ *   • bits[i] is either 0 or 1.
+ *
+ * Hint 1:
+ *   Keep track of where the next character starts.
+ *   At the end, you want to know if you started on the last bit.
+ **
+ * https://leetcode.com/problems/1-bit-and-2-bit-characters/
+***/
+
+namespace Problems;
+
+public class OneBitAndTwoBitCharacters
+{
+    public bool IsOneBitCharacter( int[] bits )
+    {
+        int oneCount = 0;
+
+        for ( int i = bits.Length - 2; i >= 0 && bits[i] == 1; i-- )
+        {
+            oneCount++;
+        }
+
+        return ( oneCount & 1 ) == 0;
+    }
+}
---
+++ c-sharp/Tests/greedy/OneBitAndTwoBitCharactersTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Greedy;
+
+public class OneBitAndTwoBitCharactersTests
+{
+    [TestCase( new int[] { 1, 0, 0 }, ExpectedResult = true )]
+    [TestCase( new int[] { 1, 1, 1, 0 }, ExpectedResult = false )]
+    public bool IsOneBitCharacterTest( int[] bits ) =>
+        new OneBitAndTwoBitCharacters().IsOneBitCharacter( bits );
+}
