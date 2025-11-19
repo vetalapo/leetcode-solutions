be565142e4cbf568535939b791a8fb5f6f6eade8
Vitaly Dushkin <vetalapo@gmail.com>
2025-11-19 16:51:42 +0100

    #67: Add Binary; solution & tests.

---
+++ c-sharp/Problems/bit-manipulation/AddBinary.cs
@@ -0,0 +1,60 @@
+﻿/**
+ * 67
+ * Add Binary
+ **
+ * Given two binary strings a and b,
+ * return their sum as a binary string.
+ *
+ * Example 1:
+ *   Input: a = "11", b = "1"
+ *   Output: "100"
+ *
+ * Example 2:
+ *   Input: a = "1010", b = "1011"
+ *   Output: "10101"
+ *
+ * Constraints:
+ *   • 1 <= a.length, b.length <= 10^4
+ *   • a and b consist only of '0' or '1' characters.
+ *   • Each string does not contain leading zeros except for the zero itself.
+ **
+ * https://leetcode.com/problems/add-binary/
+***/
+
+using System;
+using System.Collections.Generic;
+
+namespace Problems;
+
+public class AddBinarySolution
+{
+    public string AddBinary( string a, string b )
+    {
+        int ai = a.Length - 1;
+        int bi = b.Length - 1;
+
+        List<char> result = [];
+
+        int carry = 0;
+
+        while ( ai >= 0 || bi >= 0 || carry > 0 )
+        {
+            if ( ai >= 0 )
+            {
+                carry += a[ai] - '0';
+                ai--;
+            }
+
+            if ( bi >= 0 )
+            {
+                carry += b[bi] - '0';
+                bi--;
+            }
+
+            result.Insert( 0, (char)( ( carry & 1 ) + '0' ) );
+            carry = carry > 1 ? 1 : 0;
+        }
+
+        return new String( [.. result] );
+    }
+}
---
+++ c-sharp/Tests/bit-manipulation/AddBinarySolutionTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace BitManipulation;
+
+public class AddBinarySolutionTests
+{
+    [TestCase( "11", "1", ExpectedResult = "100" )]
+    [TestCase( "1010", "1011", ExpectedResult = "10101" )]
+    [TestCase( "1111", "1111", ExpectedResult = "11110" )]
+    public string AddBinaryTest( string a, string b ) =>
+        new AddBinarySolution().AddBinary( a, b );
+}
