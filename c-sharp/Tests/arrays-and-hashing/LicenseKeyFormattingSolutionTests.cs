d539387f631065c54ea59375ba873367fc63a318
Vitaly Dushkin <vetalapo@gmail.com>
2025-11-21 19:25:22 +0100

    #482: )icense Key Formatting; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/LicenseKeyFormattingSolutionTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class LicenseKeyFormattingSolutionTests
+{
+    [TestCase( "5F3Z-2e-9-w", 4, ExpectedResult = "5F3Z-2E9W" )]
+    [TestCase( "2-5g-3-J", 2, ExpectedResult = "2-5G-3J" )]
+    [TestCase( "aaaa", 2, ExpectedResult = "AA-AA" )]
+    public string LicenseKeyFormattingTest( string s, int k ) =>
+        new LicenseKeyFormattingSolution().LicenseKeyFormatting( s, k );
+}
