05349676b84be339ed099f3f2370a776bfe05459
Vitaly Dushkin <vetalapo@gmail.com>
2025-11-22 21:08:42 +0100

    #3750: Minimum Number of Flips to Reverse Binary String; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/MinimumNumberOfFlipsToReverseBinaryString.cs
@@ -0,0 +1,68 @@
+﻿/**
+ * 3750
+ * Minimum Number of Flips to Reverse Binary String
+ **
+ * You are given a positive integer n.
+ * Let s be the binary representation of n without leading zeros.
+ * The reverse of a binary string s
+ * is obtained by writing the characters of s in the opposite order.
+ * You may flip any bit in s (change 0 → 1 or 1 → 0).
+ * Each flip affects exactly one bit.
+ *
+ * Return the minimum number of flips required to make s equal
+ * to the reverse of its original form.
+ *
+ * Example 1:
+ *   Input: n = 7
+ *   Output: 0
+ *   Explanation:
+ *     The binary representation of 7 is "111".
+ *     Its reverse is also "111", which is the same.
+ *     Hence, no flips are needed.
+ *
+ * Example 2:
+ *   Input: n = 10
+ *   Output: 4
+ *   Explanation:
+ *     The binary representation of 10 is "1010".
+ *     Its reverse is "0101".
+ *     All four bits must be flipped to make them equal.
+ *     Thus, the minimum number of flips required is 4.
+ *
+ * Constraints:
+ *   • 1 <= n <= 10^9
+ *
+ * Hint 1:
+ *   Generate the reverse of the binary string
+ *   and use two pointers from the ends to determine
+ *   where flips are needed.
+ **
+ * https://leetcode.com/problems/minimum-number-of-flips-to-reverse-binary-string/
+***/
+
+namespace Problems;
+
+public class MinimumNumberOfFlipsToReverseBinaryString
+{
+    public int MinimumFlips( int n )
+    {
+        string num = n.ToString( "B" );
+        int count = 0;
+
+        int l = 0;
+        int r = num.Length - 1;
+
+        while ( l < r )
+        {
+            if ( num[l] != num[r] )
+            {
+                count += 2;
+            }
+
+            l++;
+            r--;
+        }
+
+        return count;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/MinimumNumberOfFlipsToReverseBinaryStringTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class MinimumNumberOfFlipsToReverseBinaryStringTests
+{
+    [TestCase( 7, ExpectedResult = 0 )]
+    [TestCase( 10, ExpectedResult = 4 )]
+    public int MinimumFlipsTest( int n ) =>
+        new MinimumNumberOfFlipsToReverseBinaryString().MinimumFlips( n );
+}
