018170b9cfb7c6207c25a42641c24032c9abbd54
Vitaly Dushkin <vetalapo@gmail.com>
2025-11-24 15:41:33 +0100

    #3736: Minimum moves to Equal Array Elements III; solution & tests.

---
+++ c-sharp/Tests/math/MinimumMovesToEqualArrayElementsIIITests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class MinimumMovesToEqualArrayElementsIIITests
+{
+    [TestCase( new int[] { 2, 1, 3 }, ExpectedResult = 3 )]
+    [TestCase( new int[] { 4, 4, 5 }, ExpectedResult = 2 )]
+    public int MinMovesTest( int[] nums ) =>
+        new MinimumMovesToEqualArrayElementsIII().MinMoves( nums );
+}
