e1f5e41bb2d58b75d94489921c36dd78e5e5cbfd
Vitaly Dushkin <vetalapo@gmail.com>
2025-11-26 23:50:22 +0100

    #3536: Maximum Product of Two Digits; solution & tests.

---
+++ c-sharp/Tests/math/MaximumProductOfTwoDigitsTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class MaximumProductOfTwoDigitsTests
+{
+    [TestCase( 31, ExpectedResult = 3 )]
+    [TestCase( 22, ExpectedResult = 4 )]
+    [TestCase( 124, ExpectedResult = 8 )]
+    public int MaxProductTest( int n ) =>
+        new MaximumProductOfTwoDigits().MaxProduct( n );
+}
