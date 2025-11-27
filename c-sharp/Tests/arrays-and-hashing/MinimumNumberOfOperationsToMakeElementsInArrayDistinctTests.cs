c08bb90d1b73e4f8732b41e5900610641b5fbc3f
Vitaly Dushkin <vetalapo@gmail.com>
2025-11-27 22:38:03 +0100

    #3396: Minimum Number of Operations to Make Elements in Array Distinct; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/MinimumNumberOfOperationsToMakeElementsInArrayDistinctTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class MinimumNumberOfOperationsToMakeElementsInArrayDistinctTests
+{
+    [TestCase( new int[] { 1, 2, 3, 4, 2, 3, 3, 5, 7 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { 4, 5, 6, 4, 4 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { 6, 7, 8, 9 }, ExpectedResult = 0 )]
+    public int MinimumOperationsTest( int[] nums ) =>
+        new MinimumNumberOfOperationsToMakeElementsInArrayDistinct().MinimumOperations( nums );
+}
