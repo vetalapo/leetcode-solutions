56b49e4dee7023e15ce625a05e2e9a778a91cfdc
Vitaly Dushkin <vetalapo@gmail.com>
2025-11-28 23:55:47 +0100

    #3452: Sum of Good Numbers; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/SumOfGoodNumbers.cs
@@ -0,0 +1,63 @@
+﻿/**
+ * 3452
+ * Sum of Good Numbers
+ **
+ * Given an array of integers nums and an integer k,
+ * an element nums[i] is considered good
+ * if it is strictly greater
+ * than the elements at indices i - k and i + k (if those indices exist).
+ * If neither of these indices exists,
+ * nums[i] is still considered good.
+ *
+ * Return the sum of all the good elements in the array.
+ *
+ * Example 1:
+ *   Input: nums = [1,3,2,1,5,4], k = 2
+ *   Output: 12
+ *   Explanation:
+ *     The good numbers are nums[1] = 3, nums[4] = 5, and nums[5] = 4
+ *     because they are strictly greater
+ *     than the numbers at indices i - k and i + k.
+ *
+ * Example 2:
+ *   Input: nums = [2,1], k = 1
+ *   Output: 2
+ *   Explanation:
+ *     The only good number is nums[0] = 2
+ *     because it is strictly greater than nums[1].
+ *
+ * Constraints:
+ *   • 2 <= nums.length <= 100
+ *   • 1 <= nums[i] <= 1000
+ *   • 1 <= k <= floor(nums.length / 2)
+ *
+ * Hint 1:
+ *   For each index,
+ *   check if nums[i] is strictly greater
+ *   than nums[i - k] and nums[i + k].
+ **
+ * https://leetcode.com/problems/sum-of-good-numbers/
+***/
+
+namespace Problems;
+
+public class SumOfGoodNumbersSolution
+{
+    public int SumOfGoodNumbers( int[] nums, int k )
+    {
+        int sum = 0;
+
+        for ( int i = 0; i < nums.Length; i++ )
+        {
+            int left = i - k < 0 ? int.MinValue : nums[i - k];
+            int right = i + k >= nums.Length ? int.MinValue : nums[i + k];
+
+            if ( nums[i] > left && nums[i] > right )
+            {
+                sum += nums[i];
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/SumOfGoodNumbersSolutionTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class SumOfGoodNumbersSolutionTests
+{
+    [TestCase( new int[] { 1, 3, 2, 1, 5, 4 }, 2, ExpectedResult = 12 )]
+    [TestCase( new int[] { 2, 1 }, 1, ExpectedResult = 2 )]
+    public int SumOfGoodNumbersTest( int[] nums, int k ) =>
+        new SumOfGoodNumbersSolution().SumOfGoodNumbers( nums, k );
+}
