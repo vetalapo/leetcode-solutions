7372c93e2b62599032da6be9070d86c7793972f6
Vitaly Dushkin <vetalapo@gmail.com>
2025-12-05 11:13:27 +0100

    #3432: Count Partitions with Even Sum Difference; solution & tests.

---
+++ c-sharp/Tests/math/CountPartitionsWithEvenSumDifferenceTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class CountPartitionsWithEvenSumDifferenceTests
+{
+    [TestCase( new int[] { 10, 10, 3, 7, 6 }, ExpectedResult = 4 )]
+    [TestCase( new int[] { 1, 2, 2 }, ExpectedResult = 0 )]
+    [TestCase( new int[] { 2, 4, 6, 8 }, ExpectedResult = 3 )]
+    public int CountPartitionsTest( int[] nums ) =>
+        new CountPartitionsWithEvenSumDifference().CountPartitions( nums );
+}
