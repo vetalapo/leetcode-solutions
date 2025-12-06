ae08aef18101bb7760aeb0e6faa7857643abf060
Vitaly Dushkin <vetalapo@gmail.com>
2025-12-06 18:51:01 +0100

    #2047: Number of Valid Words in a Sentence; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/NumberOfValidWordsInASentence.cs
@@ -0,0 +1,124 @@
+﻿/**
+ * 2047
+ * Number of Valid Words in a Sentence
+ **
+ * A sentence consists of lowercase letters ('a' to 'z'),
+ * digits ('0' to '9'), hyphens ('-'), punctuation marks ('!', '.', and ','),
+ * and spaces (' ') only.
+ * Each sentence can be broken down into one or more tokens
+ * separated by one or more spaces ' '.
+ *
+ * A token is a valid word if all three of the following are true:
+ *   • It only contains lowercase letters, hyphens,
+ *     and/or punctuation (no digits).
+ *   • There is at most one hyphen '-'.
+ *     If present, it must be surrounded by lowercase characters
+ *     ("a-b" is valid, but "-ab" and "ab-" are not valid).
+ *   • There is at most one punctuation mark.
+ *     If present, it must be at the end of the token
+ *     ("ab,", "cd!", and "." are valid, but "a!b" and "c.," are not valid).
+ *
+ * Examples of valid words include "a-b.", "afad", "ba-c", "a!", and "!".
+ *
+ * Given a string sentence,
+ * return the number of valid words in sentence.
+ *
+ * Example 1:
+ *   Input: sentence = "cat and  dog"
+ *   Output: 3
+ *   Explanation:
+ *     The valid words in the sentence are "cat", "and", and "dog".
+ *
+ * Example 2:
+ *   Input: sentence = "!this  1-s b8d!"
+ *   Output: 0
+ *   Explanation:
+ *     There are no valid words in the sentence.
+ *     "!this" is invalid because it starts with a punctuation mark.
+ *     "1-s" and "b8d" are invalid because they contain digits.
+ *
+ * Example 3:
+ *   Input: sentence = "alice and  bob are playing stone-game10"
+ *   Output: 5
+ *   Explanation:
+ *     The valid words in the sentence are
+ *     "alice", "and", "bob", "are", and "playing".
+ *     "stone-game10" is invalid because it contains digits.
+ *
+ * Constraints:
+ *   • 1 <= sentence.length <= 1000
+ *   • sentence only contains lowercase English letters,
+ *     digits, ' ', '-', '!', '.', and ','.
+ *   • There will be at least 1 token.
+ *
+ * Hint 1:
+ *   Iterate through the string to split it by spaces.
+ *
+ * Hint 2:
+ *   Count the number of characters of each type
+ *   (letters, numbers, hyphens, and punctuations).
+ **
+ * https://leetcode.com/problems/number-of-valid-words-in-a-sentence/
+***/
+
+using System;
+
+namespace Problems;
+
+public class NumberOfValidWordsInASentence
+{
+    public int CountValidWords( string sentence )
+    {
+        int count = 0;
+
+        string[] words = sentence.Split( ' ', StringSplitOptions.RemoveEmptyEntries );
+
+        foreach ( string word in words )
+        {
+            if ( IsValid( word ) )
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private bool IsValid( string s )
+    {
+        int hyphenCount = 0;
+        int punctuationCount = 0;
+
+        for ( int i = 0; i < s.Length; i++ )
+        {
+            if ( s[i] >= 'a' && s[i] <= 'z' )
+            {
+                continue;
+            }
+
+            if ( s[i] == '-' )
+            {
+                if ( i == 0 || i == s.Length - 1 || s[i + 1] < 'a' || ++hyphenCount > 1 )
+                {
+                    return false;
+                }
+
+                continue;
+            }
+
+            if ( s[i] == ',' || s[i] == '.' || s[i] == '!' )
+            {
+                if ( i != s.Length - 1 || ++punctuationCount > 1 )
+                {
+                    return false;
+                }
+
+                continue;
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/NumberOfValidWordsInASentenceTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Problems;
+
+public class NumberOfValidWordsInASentenceTests
+{
+    [TestCase( "cat and  dog", ExpectedResult = 3 )]
+    [TestCase( "!this  1-s b8d!", ExpectedResult = 0 )]
+    [TestCase( "alice and  bob are playing stone-game10", ExpectedResult = 5 )]
+    [TestCase( "he bought 2 pencils, 3 erasers, and 1  pencil-sharpener.", ExpectedResult = 6 )]
+    [TestCase( " o6 t", ExpectedResult = 1 )]
+    [TestCase( "a-b-c", ExpectedResult = 0 )]
+    public int CountValidWordsTest( string sentence ) =>
+        new NumberOfValidWordsInASentence().CountValidWords( sentence );
+}
