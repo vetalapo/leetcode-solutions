045cb564e3e15ca89506e50fd3b4b819feab30f2
Vitaly Dushkin <vetalapo@gmail.com>
2025-12-08 17:08:58 +0100

    #1925: Count Square Sum Triples; solution & tests.

---
+++ c-sharp/Problems/math/CountSquareSumTriples.cs
@@ -0,0 +1,74 @@
+﻿/**
+ * 1925
+ * Count Square Sum Triples
+ **
+ * A square triple (a,b,c) is a triple
+ * where a, b, and c are integers and a2 + b2 = c2.
+ *
+ * Given an integer n,
+ * return the number of square triples such
+ * that 1 <= a, b, c <= n.
+ *
+ * Example 1:
+ *   Input: n = 5
+ *   Output: 2
+ *   Explanation:
+ *     The square triples are
+ *     (3,4,5) and (4,3,5).
+ *
+ * Example 2:
+ *   Input: n = 10
+ *   Output: 4
+ *   Explanation:
+ *     The square triples are
+ *     (3,4,5), (4,3,5), (6,8,10), and (8,6,10).
+ *
+ * Constraints:
+ *   • 1 <= n <= 250
+ *
+ * Hint 1:
+ *   Iterate over all possible pairs (a,b)
+ *   and check that the square root of a * a + b * b
+ *   is an integers less than or equal n
+ *
+ * Hint 2:
+ *   You can check
+ *   that the square root of an integer
+ *   is an integer using binary seach
+ *   or a builtin function like sqrt
+ **
+ * https://leetcode.com/problems/count-square-sum-triples/
+***/
+
+using System;
+
+namespace Problems;
+
+public class CountSquareSumTriples
+{
+    public int CountTriples( int n )
+    {
+        int count = 0;
+
+        for ( int a = 3; a < n - 1; a++ )
+        {
+            for ( int b = a + 1; b < n; b++ )
+            {
+                double c = Math.Sqrt( a * a + b * b );
+
+                if ( c > n )
+                {
+                    break;
+                }
+
+                if ( c <= n && double.IsInteger( c ) )
+                {
+                    count++;
+                    b += 2;
+                }
+            }
+        }
+
+        return count * 2;
+    }
+}
---
+++ c-sharp/Tests/math/CountSquareSumTriplesTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class CountSquareSumTriplesTests
+{
+    [TestCase( 5, ExpectedResult = 2 )]
+    [TestCase( 10, ExpectedResult = 4 )]
+    [TestCase( 12, ExpectedResult = 4 )]
+    [TestCase( 18, ExpectedResult = 10 )]
+    [TestCase( 41, ExpectedResult = 34 )]
+    public int CountTriplesTest( int n ) =>
+        new CountSquareSumTriples().CountTriples( n );
+}
