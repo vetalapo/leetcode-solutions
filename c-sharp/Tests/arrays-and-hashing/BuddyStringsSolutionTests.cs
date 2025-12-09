47475919a121b2ff7a395682845db88f6079a90a
Vitaly Dushkin <vetalapo@gmail.com>
2025-12-09 22:50:28 +0100

    #859: Buddy Strings; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/BuddyStringsSolutionTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class BuddyStringsSolutionTests
+{
+    [TestCase( "ab", "ba", ExpectedResult = true )]
+    [TestCase( "ab", "ab", ExpectedResult = false )]
+    [TestCase( "aa", "aa", ExpectedResult = true )]
+    [TestCase( "abcaa", "abcbb", ExpectedResult = false )]
+    [TestCase( "ab", "babbb", ExpectedResult = false )]
+    public bool BuddyStringsTest( string s, string goal ) =>
+        new BuddyStringsSolution().BuddyStrings( s, goal );
+}
