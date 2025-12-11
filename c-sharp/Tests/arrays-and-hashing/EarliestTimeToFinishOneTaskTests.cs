3455d70c83f5407bc738206233e99c8d0e54676a
Vitaly Dushkin <vetalapo@gmail.com>
2025-12-11 19:33:17 +0100

    #3683: Earliest Time to Finish One Task; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/EarliestTimeToFinishOneTaskTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class EarliestTimeToFinishOneTaskTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int EarliestTimeTest( int[][] tasks ) =>
+        new EarliestTimeToFinishOneTask().EarliestTime( tasks );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( [new int[][] { [1, 6], [2, 3] }] ).Returns( 5 );
+
+            yield return new TestCaseData( [new int[][] { [100, 100], [100, 100], [100, 100] }] ).Returns( 200 );
+        }
+    }
+}
