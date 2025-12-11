1742f174925bd7b056e1d68650168a6f2cde7fa9
Vitaly Dushkin <vetalapo@gmail.com>
2025-12-11 02:48:53 +0100

    #3114: Latest Time You Can Obtain After Replacing Characters; solution & tests.

---
+++ c-sharp/Tests/math/LatestTimeYouCanObtainAfterReplacingCharactersTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class LatestTimeYouCanObtainAfterReplacingCharactersTests
+{
+    [TestCase( "1?:?4", ExpectedResult = "11:54" )]
+    [TestCase( "0?:5?", ExpectedResult = "09:59" )]
+    [TestCase( "??:1?", ExpectedResult = "11:19" )]
+    public string FindLatestTimeTest( string s ) =>
+        new LatestTimeYouCanObtainAfterReplacingCharacters().FindLatestTime( s );
+}
