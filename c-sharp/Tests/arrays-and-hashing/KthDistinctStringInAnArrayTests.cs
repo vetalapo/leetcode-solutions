bd0afedf144c6b1deed1bb6f5ec5bacda725bd65
Vitaly Dushkin <vetalapo@gmail.com>
2025-12-15 20:45:50 +0100

    #2053: Kth Distinct String in an Array; soluton & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/KthDistinctStringInAnArrayTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class KthDistinctStringInAnArrayTests
+{
+    [TestCase( new string[] { "d", "b", "c", "b", "c", "a" }, 2, ExpectedResult = "a" )]
+    [TestCase( new string[] { "aaa", "aa", "a" }, 1, ExpectedResult = "aaa" )]
+    [TestCase( new string[] { "a", "b", "a" }, 3, ExpectedResult = "" )]
+    public string KthDistinctTest( string[] arr, int k ) =>
+        new KthDistinctStringInAnArray().KthDistinct( arr, k );
+}
