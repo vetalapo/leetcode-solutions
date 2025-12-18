7cad13520d6229511e344ce5b0fd5a4a1756a64f
Vitaly Dushkin <vetalapo@gmail.com>
2025-12-18 17:03:36 +0100

    #2220: Minimum Bit Flips to Convert Number; solution & tests.

---
+++ c-sharp/Tests/bit-manipulation/MinimumBitFlipsToConvertNumberTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace BitManipulation;
+
+public class MinimumBitFlipsToConvertNumberTests
+{
+    [TestCase( 10, 7, ExpectedResult = 3 )]
+    [TestCase( 3, 4, ExpectedResult = 3 )]
+    public int MinBitFlipsTest( int start, int goal ) =>
+        new MinimumBitFlipsToConvertNumber().MinBitFlips( start, goal );
+}
