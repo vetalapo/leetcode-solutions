aedc5def1238968c1832a09ea754eebf50625b21
Vitaly Dushkin <vetalapo@gmail.com>
2025-12-19 23:43:17 +0100

    #461: Hamming Distance; solution & tests.

---
+++ c-sharp/Tests/bit-manipulation/HammingDistanceSolutionTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace BitManipulation;
+
+public class HammingDistanceSolutionTests
+{
+    [TestCase( 1, 4, ExpectedResult = 2 )]
+    [TestCase( 3, 1, ExpectedResult = 1 )]
+    public int HammingDistanceTest( int x, int y ) =>
+        new HammingDistanceSolution().HammingDistance( x, y );
+}
