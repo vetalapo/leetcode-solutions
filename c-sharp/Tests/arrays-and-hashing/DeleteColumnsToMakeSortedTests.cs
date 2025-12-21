167ccfa27ee52d07ddc6abb2110fa807ee579fe6
Vitaly Dushkin <vetalapo@gmail.com>
2025-12-21 14:43:02 +0100

    #944: Delete Columns to Make Sorted; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/DeleteColumnsToMakeSortedTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class DeleteColumnsToMakeSortedTests
+{
+    [TestCase( [new string[] { "cba", "daf", "ghi" }], ExpectedResult = 1 )]
+    [TestCase( [new string[] { "a", "b" }], ExpectedResult = 0 )]
+    [TestCase( [new string[] { "zyx", "wvu", "tsr" }], ExpectedResult = 3 )]
+    public int MinDeletionSizeTest( string[] strs ) =>
+        new DeleteColumnsToMakeSorted().MinDeletionSize( strs );
+}
