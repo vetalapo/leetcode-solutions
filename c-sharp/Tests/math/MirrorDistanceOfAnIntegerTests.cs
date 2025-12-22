21d455ee85e2ea52b4e77fa510c9847af505a598
Vitaly Dushkin <vetalapo@gmail.com>
2025-12-22 15:47:25 +0100

    #3783: Mirror Distance of an Integer; solution & tests.

---
+++ c-sharp/Tests/math/MirrorDistanceOfAnIntegerTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class MirrorDistanceOfAnIntegerTests
+{
+    [TestCase( 25, ExpectedResult = 27 )]
+    [TestCase( 10, ExpectedResult = 9 )]
+    [TestCase( 7, ExpectedResult = 0 )]
+    [TestCase( 111, ExpectedResult = 0 )]
+    public int MirrorDistanceTest( int n ) =>
+        new MirrorDistanceOfAnInteger().MirrorDistance( n );
+}
