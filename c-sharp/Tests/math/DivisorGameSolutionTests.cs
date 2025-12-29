f8cfa0f7b1dbf82f92b09197483fe29dbbcb0338
Vitaly Dushkin <vetalapo@gmail.com>
2025-12-29 23:30:04 +0100

    #1025: Divisor Game; solution & tests.

---
+++ c-sharp/Tests/math/DivisorGameSolutionTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class DivisorGameSolutionTests
+{
+    [TestCase( 2, ExpectedResult = true )]
+    [TestCase( 3, ExpectedResult = false )]
+    public bool DivisorGameTest( int n ) =>
+        new DivisorGameSolution().DivisorGame( n );
+}
