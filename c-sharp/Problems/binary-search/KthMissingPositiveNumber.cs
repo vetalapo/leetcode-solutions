66583e69197232c74443fe5d1d52e700c1ef63bc
Vitaly Dushkin <vetalapo@gmail.com>
2025-12-30 23:07:10 +0100

    #1539: Kth Missing Positive Number; solution & tests.

---
+++ c-sharp/Problems/binary-search/KthMissingPositiveNumber.cs
@@ -0,0 +1,88 @@
+﻿/**
+ * 1539
+ * Kth Missing Positive Number
+ **
+ * Given an array arr of positive integers
+ * sorted in a strictly increasing order,
+ * and an integer k.
+ *
+ * Return the kth positive integer
+ * that is missing from this array.
+ *
+ * Example 1:
+ *   Input: arr = [2,3,4,7,11], k = 5
+ *   Output: 9
+ *   Explanation:
+ *     The missing positive integers are [1,5,6,8,9,10,12,13,...].
+ *     The 5th missing positive integer is 9.
+ *
+ * Example 2:
+ *   Input: arr = [1,2,3,4], k = 2
+ *   Output: 6
+ *   Explanation:
+ *     The missing positive integers are [5,6,7,...].
+ *     The 2nd missing positive integer is 6.
+ *
+ * Constraints:
+ *   • 1 <= arr.length <= 1000
+ *   • 1 <= arr[i] <= 1000
+ *   • 1 <= k <= 1000
+ *   • arr[i] < arr[j] for 1 <= i < j <= arr.length
+ *
+ * Follow up:
+ *   Could you solve this problem in less than O(n) complexity?
+ *
+ * Hint 1;
+ *   Keep track of how many positive numbers are missing
+ *   as you scan the array.
+ **
+ * https://leetcode.com/problems/kth-missing-positive-number/
+***/
+
+namespace Problems;
+
+public class KthMissingPositiveNumber
+{
+    public int FindKthPositive( int[] arr, int k )
+    {
+        int left = 0;
+        int right = arr.Length;
+
+        while ( left < right )
+        {
+            int middle = left + ( right - left ) / 2;
+
+            if ( arr[middle] - middle - 1 < k )
+            {
+                left = middle + 1;
+            }
+            else
+            {
+                right = middle;
+            }
+        }
+
+        return left + k;
+    }
+
+    public int FindKthPositiveOn( int[] arr, int k )
+    {
+        for ( int i = 0, curr = 1; k > 0; curr++ )
+        {
+            if ( i < arr.Length && arr[i] == curr )
+            {
+                i++;
+                continue;
+            }
+
+            k--;
+
+            if ( k == 0 )
+            {
+                return curr;
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ c-sharp/Tests/binary-search/KthMissingPositiveNumberTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+namespace BinarySearch;
+
+[TestFixture()]
+public class KthMissingPositiveNumberTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int FindKthPositiveTest( int[] arr, int k )
+        => new KthMissingPositiveNumber().FindKthPositive( arr, k );
+
+    [TestCaseSource( nameof( TestCases ) )]
+    public int FindKthPositiveOnTest( int[] arr, int k )
+        => new KthMissingPositiveNumber().FindKthPositiveOn( arr, k );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( new int[] { 2, 3, 4, 7, 11 }, 5 ).Returns( 9 );
+
+            yield return new TestCaseData( new int[] { 1, 2, 3, 4 }, 2 ).Returns( 6 );
+
+            yield return new TestCaseData( new int[] { 1, 2 }, 1 ).Returns( 3 );
+        }
+    }
+}
