48ca338ba7596ff97e5df05eb8a6e023db92d22f
Vitaly Dushkin <vetalapo@gmail.com>
2025-12-31 19:46:15 +0100

    #3226: Number of Bit Changes to Make Two Integers Equal; solution & tests.

---
+++ c-sharp/Tests/bit-manipulation/NumberOfBitChangesToMakeTwoIntegersEqualTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace BitManipulation;
+
+public class NumberOfBitChangesToMakeTwoIntegersEqualTests
+{
+    [TestCase( 13, 4, ExpectedResult = 2 )]
+    [TestCase( 21, 21, ExpectedResult = 0 )]
+    [TestCase( 14, 13, ExpectedResult = -1 )]
+    public int MinChangesTest( int n, int k ) =>
+        new NumberOfBitChangesToMakeTwoIntegersEqual().MinChanges( n, k );
+}
