45ab8d3ae2205871526ff39bf0420a9184f85215
Vitaly Dushkin <vetalapo@gmail.com>
2026-01-12 21:40:29 +0100

    #1266: Minimum Time Visiting All Points; solution & tests.

---
+++ c-sharp/Tests/math/MinimumTimeVisitingAllPointsTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class MinimumTimeVisitingAllPointsTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int MinTimeToVisitAllPointsTest( int[][] points ) =>
+        new MinimumTimeVisitingAllPoints().MinTimeToVisitAllPoints( points );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( [new int[][] { [1, 1], [3, 4], [-1, 0] }] ).Returns( 7 );
+            yield return new TestCaseData( [new int[][] { [3, 2], [-2, 2] }] ).Returns( 5 );
+        }
+    }
+}
