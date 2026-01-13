9ae6559ef99ca784297356947a48a100b8c4684e
Vitaly Dushkin <vetalapo@gmail.com>
2026-01-13 18:10:07 +0100

    #3794: Reverse String Prefix; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/ReverseStringPrefix.cs
@@ -0,0 +1,63 @@
+﻿/**
+ * 3794
+ * Reverse String Prefix
+ **
+ * You are given a string s and an integer k.
+ * Reverse the first k characters of s and return the resulting string.
+ *
+ * Example 1:
+ *   Input: s = "abcd", k = 2
+ *   Output: "bacd"
+ *   Explanation:​​​​​​​
+ *     The first k = 2 characters "ab" are reversed to "ba".
+ *     The final resulting string is "bacd".
+ *
+ * Example 2:
+ *   Input: s = "xyz", k = 3
+ *   Output: "zyx"
+ *   Explanation:
+ *     The first k = 3 characters "xyz" are reversed to "zyx".
+ *     The final resulting string is "zyx".
+ *
+ * Example 3:
+ *   Input: s = "hey", k = 1
+ *   Output: "hey"
+ *   Explanation:
+ *     The first k = 1 character "h" remains unchanged on reversal.
+ *     The final resulting string is "hey".
+ *
+ * Constraints:
+ *   • 1 <= s.length <= 100
+ *   • s consists of lowercase English letters.
+ *   • 1 <= k <= s.length
+ *
+ * Hint 1:
+ *   Simulate as described
+ **
+ * https://leetcode.com/problems/reverse-string-prefix/
+***/
+
+using System;
+
+namespace Problems;
+
+public class ReverseStringPrefix
+{
+    public string ReversePrefix( string s, int k )
+    {
+        char[] chars = s.ToCharArray();
+
+        int left = 0;
+        int right = k - 1;
+
+        while ( left < right )
+        {
+            (chars[left], chars[right]) = (chars[right], chars[left]);
+
+            left++;
+            right--;
+        }
+
+        return new String( chars );
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/ReverseStringPrefixTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class ReverseStringPrefixTests
+{
+    [TestCase( "abcd", 2, ExpectedResult = "bacd" )]
+    [TestCase( "xyz", 3, ExpectedResult = "zyx" )]
+    [TestCase( "hey", 1, ExpectedResult = "hey" )]
+    public string ReversePrefixTest( string s, int k ) =>
+        new ReverseStringPrefix().ReversePrefix( s, k );
+}
