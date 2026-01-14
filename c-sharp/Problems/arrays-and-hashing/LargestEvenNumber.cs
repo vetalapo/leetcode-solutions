013b18b3cc0d53c3a15c49ec9b86d959ab575a06
Vitaly Dushkin <vetalapo@gmail.com>
2026-01-14 16:15:09 +0100

    #3798: Largest Even Number; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/LargestEvenNumber.cs
@@ -0,0 +1,62 @@
+﻿/**
+ * 3798
+ * Largest Even Number
+ **
+ * You are given a string s consisting only of the characters '1' and '2'.
+ * You may delete any number of characters from s
+ * without changing the order of the remaining characters.
+ * Return the largest possible resultant string
+ * that represents an even integer.
+ * If there is no such string, return the empty string "".
+ *
+ * Example 1:
+ *   Input: s = "1112"
+ *   Output: "1112"
+ *   Explanation:
+ *     The string already represents the largest possible even number,
+ *     so no deletions are needed.
+ *
+ * Example 2:
+ *   Input: s = "221"
+ *   Output: "22"
+ *   Explanation:
+ *     Deleting '1' results in the largest possible even number
+ *     which is equal to 22.
+ *
+ * Example 3:
+ *   Input: s = "1"
+ *   Output: ""
+ *   Explanation:
+ *     There is no way to get an even number.
+ *
+ * Constraints:
+ *   • 1 <= s.length <= 100
+ *   • s consists only of the characters '1' and '2'.
+ *
+ * Hint 1:
+ *   A number ending with '1' is odd.
+ *
+ * Hint 2:
+ *   Find the last '2' in s. If none, return "".
+ *   Otherwise return the prefix up to and including that '2'.
+ **
+ * https://leetcode.com/problems/largest-even-number/
+***/
+
+namespace Problems;
+
+public class LargestEvenNumber
+{
+    public string LargestEven( string s )
+    {
+        for ( int i = s.Length - 1; i >= 0; i-- )
+        {
+            if ( ( s[i] & 1 ) == 0 )
+            {
+                return s[0..( i + 1 )];
+            }
+        }
+
+        return string.Empty;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/LargestEvenNumberTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class LargestEvenNumberTests
+{
+    [TestCase( "1112", ExpectedResult = "1112" )]
+    [TestCase( "221", ExpectedResult = "22" )]
+    [TestCase( "1", ExpectedResult = "" )]
+    public string LargestEvenTest( string s ) =>
+        new LargestEvenNumber().LargestEven( s );
+}
