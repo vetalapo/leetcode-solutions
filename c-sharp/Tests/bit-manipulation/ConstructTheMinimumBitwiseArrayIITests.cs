6ba85c531b47d6e88740aba0aa4fb12279e280ab
Vitaly Dushkin <vetalapo@gmail.com>
2026-01-21 09:43:34 +0100

    #3315: Construct the Minimum Bitwise Array II; solution & tests.

---
+++ c-sharp/Tests/bit-manipulation/ConstructTheMinimumBitwiseArrayIITests.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+namespace BitManipulation;
+
+public class ConstructTheMinimumBitwiseArrayIITests
+{
+    [TestCase( new int[] { 2, 3, 5, 7 }, ExpectedResult = new int[] { -1, 1, 4, 3 } )]
+    [TestCase( new int[] { 11, 13, 31 }, ExpectedResult = new int[] { 9, 12, 15 } )]
+    public int[] MinBitwiseArrayTest( IList<int> nums ) =>
+        new ConstructTheMinimumBitwiseArrayII().MinBitwiseArray( nums );
+}
