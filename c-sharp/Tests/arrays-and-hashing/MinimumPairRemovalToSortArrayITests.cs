0293f9a2469dd00ae5c6a7cae0f9d8efd0d609a2
Vitaly Dushkin <vetalapo@gmail.com>
2026-01-22 16:16:15 +0100

    #3507: Minimum Pair Removal to Sort Array I; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/MinimumPairRemovalToSortArrayITests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class MinimumPairRemovalToSortArrayITests
+{
+    [TestCase( new int[] { 5, 2, 3, 1 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { 1, 2, 2 }, ExpectedResult = 0 )]
+    public int MinimumPairRemovalTest( int[] nums ) =>
+        new MinimumPairRemovalToSortArrayI().MinimumPairRemoval( nums );
+}
