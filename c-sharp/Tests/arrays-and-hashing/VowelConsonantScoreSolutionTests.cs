9bb8afffbd8d3cd87e5b59628c211a9e94e7d268
Vitaly Dushkin <vetalapo@gmail.com>
2026-01-23 17:49:00 +0100

    #3813: Vowel-Consonant Score; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/VowelConsonantScoreSolutionTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class VowelConsonantScoreSolutionTests
+{
+    [TestCase( "cooear", ExpectedResult = 2 )]
+    [TestCase( "axeyizou", ExpectedResult = 1 )]
+    [TestCase( "au 123", ExpectedResult = 0 )]
+    [TestCase( "a", ExpectedResult = 0 )]
+    [TestCase( "i3", ExpectedResult = 0 )]
+    public int VowelConsonantScoreTest( string s ) =>
+        new VowelConsonantScoreSolution().VowelConsonantScore( s );
+}
