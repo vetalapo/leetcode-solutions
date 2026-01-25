a7bc0ea23d1ea886696725259f9a2f1472e6282c
Vitaly Dushkin <vetalapo@gmail.com>
2026-01-25 23:53:12 +0100

    #1984: Minimum Difference Between Highest and Lowest of K Scores; solution & tests.

---
+++ c-sharp/Tests/sliding-window/MinimumDifferenceBetweenHighestAndLowestOfKScoresTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace SlidingWindow;
+
+public class MinimumDifferenceBetweenHighestAndLowestOfKScoresTests
+{
+    [TestCase( new int[] { 90 }, 1, ExpectedResult = 0 )]
+    [TestCase( new int[] { 9, 4, 1, 7 }, 2, ExpectedResult = 2 )]
+    public int MinimumDifferenceTest( int[] nums, int k ) =>
+        new MinimumDifferenceBetweenHighestAndLowestOfKScores().MinimumDifference( nums, k );
+}
