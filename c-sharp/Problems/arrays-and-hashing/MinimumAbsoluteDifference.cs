935aec3ee3b1e931da249b15109f315c7c24aade
Vitaly Dushkin <vetalapo@gmail.com>
2026-01-26 23:25:39 +0100

    #1200: Minimum Absolute Difference; solution & tests

---
+++ c-sharp/Problems/arrays-and-hashing/MinimumAbsoluteDifference.cs
@@ -0,0 +1,76 @@
+﻿/**
+ * 1200
+ * Minimum Absolute Difference
+ **
+ * Given an array of distinct integers arr,
+ * find all pairs of elements
+ * with the minimum absolute difference of any two elements.
+ *
+ * Return a list of pairs in ascending order (with respect to pairs),
+ * each pair [a, b] follows
+ *   • a, b are from arr
+ *   • a < b
+ *   • b - a equals to the minimum absolute difference of any two elements in arr
+ *
+ * Example 1:
+ *   Input: arr = [4,2,1,3]
+ *   Output: [[1,2],[2,3],[3,4]]
+ *   Explanation:
+ *     The minimum absolute difference is 1.
+ *     List all pairs with difference equal to 1 in ascending order.
+ *
+ * Example 2:
+ *   Input: arr = [1,3,6,10,15]
+ *   Output: [[1,3]]
+ *
+ * Example 3:
+ *   Input: arr = [3,8,-10,23,19,-4,-14,27]
+ *   Output: [[-14,-10],[19,23],[23,27]]
+ *
+ * Constraints:
+ *   • 2 <= arr.length <= 10^5
+ *   • -10^6 <= arr[i] <= 10^6
+ *
+ * Hint 1:
+ *   Find the minimum absolute difference between two elements in the array.
+ *
+ * Hint 2:
+ *   The minimum absolute difference must be a difference
+ *   between two consecutive elements in the sorted array.
+ **
+ * https://leetcode.com/problems/minimum-absolute-difference/
+***/
+
+using System;
+using System.Collections.Generic;
+
+namespace Problems;
+
+public class MinimumAbsoluteDifference
+{
+    public IList<IList<int>> MinimumAbsDifference( int[] arr )
+    {
+        Array.Sort( arr );
+
+        List<IList<int>> result = [];
+        int min = int.MaxValue;
+
+        for ( int i = 1; i < arr.Length; i++ )
+        {
+            int diff = arr[i] - arr[i - 1];
+
+            if ( diff == min )
+            {
+                result.Add( [arr[i - 1], arr[i]] );
+            }
+            else if ( diff < min )
+            {
+                min = diff;
+                result.Clear();
+                result.Add( [arr[i - 1], arr[i]] );
+            }
+        }
+
+        return result;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/MinimumAbsoluteDifferenceTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class MinimumAbsoluteDifferenceTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public IList<IList<int>> MinimumAbsDifferenceTest( int[] arr ) =>
+        new MinimumAbsoluteDifference().MinimumAbsDifference( arr );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( new int[] { 4, 2, 1, 3 } )
+                .Returns( new int[][] { [1, 2], [2, 3], [3, 4] } );
+
+            yield return new TestCaseData( new int[] { 1, 3, 6, 10, 15 } )
+                .Returns( new int[][] { [1, 3] } );
+
+            yield return new TestCaseData( new int[] { 3, 8, -10, 23, 19, -4, -14, 27 } )
+                .Returns( new int[][] { [-14, -10], [19, 23], [23, 27] } );
+        }
+    }
+}
