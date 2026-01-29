d145c7a2f2cd4a3a7256902ddb208a0eb080928c
Vitaly Dushkin <vetalapo@gmail.com>
2026-01-29 20:34:48 +0100

    #190: Reverse Bits; solution & tests.

---
+++ c-sharp/Problems/bit-manipulation/ReverseBits.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * 190
+ * Reverse Bits
+ **
+ * Reverse bits of a given 32 bits signed integer.
+ *
+ * Example 1:
+ *   Input: n = 43261596
+ *   Output: 964176192
+ *   Explanation:
+ *     Integer    Binary
+ *     43261596   00000010100101000001111010011100
+ *     964176192  00111001011110000010100101000000
+ *
+ * Example 2:
+ *   Input: n = 2147483644
+ *   Output: 1073741822
+ *   Explanation:
+ *     Integer     Binary
+ *     2147483644  01111111111111111111111111111100
+ *     1073741822  00111111111111111111111111111110
+ *
+ * Constraints:
+ *   • 0 <= n <= 2^31 - 2
+ *   • n is even.
+ *
+ * Follow up:
+ *   If this function is called many times,
+ *   how would you optimize it?
+ **
+ * https://leetcode.com/problems/reverse-bits/
+***/
+
+namespace Problems;
+
+public class ReverseBitsSolution
+{
+    public int ReverseBits( int n )
+    {
+        int result = 0;
+        int offset = 32;
+
+        while ( n > 0 )
+        {
+            result = ( result << 1 ) + ( n & 1 );
+            n >>= 1;
+            offset--;
+        }
+
+        return result << offset;
+    }
+}
---
+++ c-sharp/Tests/bit-manipulation/ReverseBitsSolutionTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace BitManipulation;
+
+public class ReverseBitsSolutionTests
+{
+    [TestCase( 43261596, ExpectedResult = 964176192 )]
+    [TestCase( 2147483644, ExpectedResult = 1073741822 )]
+    public int ReverseBitsTest( int n ) =>
+        new ReverseBitsSolution().ReverseBits( n );
+}
