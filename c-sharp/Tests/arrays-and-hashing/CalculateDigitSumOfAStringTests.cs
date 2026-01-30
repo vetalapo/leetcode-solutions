2ddfb83b7252df80a070e84fa74cb23c054e2a94
Vitaly Dushkin <vetalapo@gmail.com>
2026-01-30 12:59:02 +0100

    #2243: Calculate Digit Sum of a String; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/CalculateDigitSumOfAStringTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class CalculateDigitSumOfAStringTests
+{
+    [TestCase( "11111222223", 3, ExpectedResult = "135" )]
+    [TestCase( "00000000", 3, ExpectedResult = "000" )]
+    [TestCase( "1234", 2, ExpectedResult = "37" )]
+    public string DigitSumTest( string s, int k ) =>
+        new CalculateDigitSumOfAString().DigitSum( s, k );
+}
