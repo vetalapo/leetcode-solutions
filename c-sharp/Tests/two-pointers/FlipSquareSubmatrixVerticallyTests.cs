83c68635159a8ab509a5e0406b7d310531f33a73
Vitaly Dushkin <vetalapo@gmail.com>
2026-02-02 12:54:30 +0100

    #3643: Flip Square Submatrix Vertically; solution & tests.

---
+++ c-sharp/Tests/two-pointers/FlipSquareSubmatrixVerticallyTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+namespace TwoPointers;
+
+public class FlipSquareSubmatrixVerticallyTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int[][] ReverseSubmatrixTest( int[][] grid, int x, int y, int k ) =>
+        new FlipSquareSubmatrixVertically().ReverseSubmatrix( grid, x, y, k );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( new int[][] { [1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16] }, 1, 0, 3 )
+                .Returns( new int[][] { [1, 2, 3, 4], [13, 14, 15, 8], [9, 10, 11, 12], [5, 6, 7, 16] } );
+
+            yield return new TestCaseData( new int[][] { [3, 4, 2, 3], [2, 3, 4, 2] }, 0, 2, 2 )
+                .Returns( new int[][] { [3, 4, 4, 2], [2, 3, 2, 3] } );
+        }
+    }
+}
