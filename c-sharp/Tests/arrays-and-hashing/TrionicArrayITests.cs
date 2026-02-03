462b7c701ecbe155d1802f6a6d6e3af6b6d17c54
Vitaly Dushkin <vetalapo@gmail.com>
2026-02-03 23:29:23 +0100

    #3637: Trionic Array I; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/TrionicArrayITests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class TrionicArrayITests
+{
+    [TestCase( new int[] { 1, 3, 5, 4, 2, 6 }, ExpectedResult = true )]
+    [TestCase( new int[] { 2, 1, 3 }, ExpectedResult = false )]
+    public bool IsTrionicTest( int[] nums ) =>
+        new TrionicArrayI().IsTrionic( nums );
+}
