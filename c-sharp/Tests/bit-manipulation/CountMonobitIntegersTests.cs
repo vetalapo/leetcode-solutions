20a6581798831f67f87f121d0568e0a2f3d3c0ab
Vitaly Dushkin <vetalapo@gmail.com>
2026-02-04 10:47:20 +0100

    #3827: Count Monobit Integers; solution & tests

---
+++ c-sharp/Tests/bit-manipulation/CountMonobitIntegersTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace BitManipulation;
+
+public class CountMonobitIntegersTests
+{
+    [TestCase( 1, ExpectedResult = 2 )]
+    [TestCase( 4, ExpectedResult = 3 )]
+    [TestCase( 5, ExpectedResult = 3 )]
+    public int CountMonobitTest( int n ) =>
+        new CountMonobitIntegers().CountMonobit( n );
+}
