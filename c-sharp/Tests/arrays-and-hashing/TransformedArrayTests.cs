3358d0acb9a6cc3ce72087306558e23d9e1cc2ee
Vitaly Dushkin <vetalapo@gmail.com>
2026-02-05 12:05:47 +0100

    #3379: Transformed Array; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/TransformedArrayTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class TransformedArrayTests
+{
+    [TestCase( new int[] { 3, -2, 1, 1 }, ExpectedResult = new int[] { 1, 1, 1, 3 } )]
+    [TestCase( new int[] { -1, 4, -1 }, ExpectedResult = new int[] { -1, -1, 4 } )]
+    [TestCase( new int[] { -10, -10, -4 }, ExpectedResult = new int[] { -4, -10, -10 } )]
+    [TestCase( new int[] { -10, -10 }, ExpectedResult = new int[] { -10, -10 } )]
+    public int[] ConstructTransformedArrayTest( int[] nums ) =>
+        new TransformedArray().ConstructTransformedArray( nums );
+}
