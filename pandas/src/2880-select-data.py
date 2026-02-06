05df68256de6dd65adbfc3c60f7d9acfb9e51a6b
Vitaly Dushkin <vetalapo@gmail.com>
2026-02-06 15:02:35 +0100

    #2880: Select Data; solution & tests

---
+++ pandas/src/2880-select-data.py
@@ -0,0 +1,54 @@
+﻿##
+# 2880
+# Select Data
+##
+# DataFrame students:
+#   +-------------+--------+
+#   | Column Name | Type   |
+#   +-------------+--------+
+#   | student_id  | int    |
+#   | name        | object |
+#   | age         | int    |
+#   +-------------+--------+
+#
+# Write a solution to select the name and age of the student with student_id = 101.
+#
+# The result format is in the following example.
+#
+# Example 1:
+#   Input:
+#     +------------+---------+-----+
+#     | student_id | name    | age |
+#     +------------+---------+-----+
+#     | 101        | Ulysses | 13  |
+#     | 53         | William | 10  |
+#     | 128        | Henry   | 6   |
+#     | 3          | Henry   | 11  |
+#     +------------+---------+-----+
+#   Output:
+#     +---------+-----+
+#     | name    | age |
+#     +---------+-----+
+#     | Ulysses | 13  |
+#     +---------+-----+
+#   Explanation:
+#     Student Ulysses has student_id = 101, we select the name and age.
+#
+# Hint 1:
+#   Consider applying both row and column filtering to select the desired data.
+##
+# https://leetcode.com/problems/select-data/
+##
+
+import sys
+
+# Preventing creation of __pycache__
+sys.dont_write_bytecode = True
+
+import pandas as pd
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    return students[students.student_id == 101][["name", "age"]]
+
+# To make display 0 runtime
+# __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
---
+++ pandas/src/tests/test_2880_select_data.py
@@ -0,0 +1,34 @@
+﻿import sys
+
+# Preventing creation of __pycache__
+sys.dont_write_bytecode = True
+
+import importlib
+import unittest
+import pandas as pd
+
+# Python really doesn't like paths with "-" and "/"
+# I don't like Python not liking it so
+# have to use importlib to import modules
+unit = importlib.import_module("src.2880-select-data")
+
+df = pd.DataFrame({
+    "student_id": [101, 53, 128, 3],
+    "name": ["Ulysses", "William", "Henry", "Henry"],
+    "age": [13, 10, 6, 11]
+})
+
+expected_df = pd.DataFrame({
+    "name": ["Ulysses"],
+    "age": [13]
+})
+
+class TestSelectData(unittest.TestCase):
+    def test_selectData(self):
+        actual_df = unit.selectData(df)
+        actual_df.reset_index(drop=True, inplace=True)
+        
+        pd.testing.assert_frame_equal(actual_df, expected_df)
+
+if __name__ == "__main__":
+    unittest.main()
