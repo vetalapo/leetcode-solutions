d8ca6bb4ee2ab9064e5754d6dacd54cb0e644b4d
Vitaly Dushkin <vetalapo@gmail.com>
2026-02-07 22:49:44 +0100

    #2138: Divide a String Into Groups of Size k; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/DivideAStringIntoGroupsOfSizeKTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class DivideAStringIntoGroupsOfSizeKTests
+{
+    [TestCase( "abcdefghi", 3, 'x', ExpectedResult = new string[] { "abc", "def", "ghi" } )]
+    [TestCase( "abcdefghij", 3, 'x', ExpectedResult = new string[] { "abc", "def", "ghi", "jxx" } )]
+    public string[] DivideStringTest( string s, int k, char fill ) =>
+        new DivideAStringIntoGroupsOfSizeK().DivideString( s, k, fill );
+}
