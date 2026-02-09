2a9ee634bb73e9e5450a50bc4693462e6460ede8
Vitaly Dushkin <vetalapo@gmail.com>
2026-02-09 18:58:54 +0100

    #Count Dominant Indices; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/CountDominantIndicesTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class CountDominantIndicesTests
+{
+    [TestCase( new int[] { 5, 4, 3 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { 4, 1, 2 }, ExpectedResult = 1 )]
+    public int DominantIndicesTest( int[] nums ) =>
+        new CountDominantIndices().DominantIndices( nums );
+}
