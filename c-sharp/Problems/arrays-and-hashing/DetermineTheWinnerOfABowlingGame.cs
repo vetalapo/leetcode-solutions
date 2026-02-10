7446074b659314238b231d737f8d0f6c4a0207b4
Vitaly Dushkin <vetalapo@gmail.com>
2026-02-10 11:52:54 +0100

    #2660: Determine the Winner of a Bowling Game; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/DetermineTheWinnerOfABowlingGame.cs
@@ -0,0 +1,109 @@
+﻿/**
+ * 2660
+ * Determine the Winner of a Bowling Game
+ **
+ * You are given two 0-indexed integer arrays player1 and player2,
+ * representing the number of pins
+ * that player 1 and player 2 hit in a bowling game, respectively.
+ *
+ * The bowling game consists of n turns,
+ * and the number of pins in each turn is exactly 10.
+ *
+ * Assume a player hits xi pins in the ith turn.
+ * The value of the ith turn for the player is:
+ *   • 2xi if the player hits 10 pins in either (i - 1)th or (i - 2)th turn.
+ *   • Otherwise, it is xi.
+ *
+ * The score of the player is the sum of the values of their n turns.
+ *
+ * Return
+ *   • 1 if the score of player 1 is more than the score of player 2,
+ *   • 2 if the score of player 2 is more than the score of player 1, and
+ *   • 0 in case of a draw.
+ *
+ * Example 1:
+ *   Input: player1 = [5,10,3,2], player2 = [6,5,7,3]
+ *   Output: 1
+ *   Explanation:
+ *     The score of player 1 is 5 + 10 + 2*3 + 2*2 = 25.
+ *     The score of player 2 is 6 + 5 + 7 + 3 = 21.
+ *
+ * Example 2:
+ *   Input: player1 = [3,5,7,6], player2 = [8,10,10,2]
+ *   Output: 2
+ *   Explanation:
+ *     The score of player 1 is 3 + 5 + 7 + 6 = 21.
+ *     The score of player 2 is 8 + 10 + 2*10 + 2*2 = 42.
+ *
+ * Example 3:
+ *   Input: player1 = [2,3], player2 = [4,1]
+ *   Output: 0
+ *   Explanation:
+ *     The score of player1 is 2 + 3 = 5.
+ *     The score of player2 is 4 + 1 = 5.
+ *
+ * Example 4:
+ *   Input: player1 = [1,1,1,10,10,10,10], player2 = [10,10,10,10,1,1,1]
+ *   Output: 2
+ *   Explanation:
+ *     The score of player1 is 1 + 1 + 1 + 10 + 2*10 + 2*10 + 2*10 = 73.
+ *     The score of player2 is 10 + 2*10 + 2*10 + 2*10 + 2*1 + 2*1 + 1 = 75.
+ *
+ * Constraints:
+ *   • n == player1.length == player2.length
+ *   • 1 <= n <= 1000
+ *   • 0 <= player1[i], player2[i] <= 10
+ *
+ * Hint 1:
+ *   Think about simulating the process to calculate the answer.
+ *
+ * Hint 2:
+ *   Iterate over each element and check the previous two elements.
+ *   See if one of them is 10 and can affect the score.
+ **
+ * https://leetcode.com/problems/determine-the-winner-of-a-bowling-game/
+***/
+
+namespace Problems;
+
+public class DetermineTheWinnerOfABowlingGame
+{
+    public int IsWinner( int[] player1, int[] player2 )
+    {
+        int player1Score = player1[0];
+        int player2Score = player2[0];
+
+        // This till the loop slightly improves run times.
+        if ( player1.Length == 1 )
+        {
+            return player1Score == player2Score ? 0 : player1Score > player2Score ? 1 : 2;
+        }
+
+        player1Score += player1[1] + ( player1Score == 10 ? player1[1] : 0 );
+        player2Score += player2[1] + ( player2Score == 10 ? player2[1] : 0 );
+
+        for ( int i = 2; i < player1.Length; i++ )
+        {
+            player1Score += player1[i];
+
+            if ( player1[i - 1] == 10 || player1[i - 2] == 10 )
+            {
+                player1Score += player1[i];
+            }
+
+            player2Score += player2[i];
+
+            if ( player2[i - 1] == 10 || player2[i - 2] == 10 )
+            {
+                player2Score += player2[i];
+            }
+        }
+
+        if ( player1Score == player2Score )
+        {
+            return 0;
+        }
+
+        return player1Score > player2Score ? 1 : 2;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/DetermineTheWinnerOfABowlingGameTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class DetermineTheWinnerOfABowlingGameTests
+{
+    [TestCase( new int[] { 5, 10, 3, 2 }, new int[] { 6, 5, 7, 3 }, ExpectedResult = 1 )]
+    [TestCase( new int[] { 3, 5, 7, 6 }, new int[] { 8, 10, 10, 2 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { 2, 3 }, new int[] { 4, 1 }, ExpectedResult = 0 )]
+    [TestCase( new int[] { 1, 1, 1, 10, 10, 10, 10 }, new int[] { 10, 10, 10, 10, 1, 1, 1 }, ExpectedResult = 2 )]
+    public int IsWinnerTest( int[] player1, int[] player2 ) =>
+        new DetermineTheWinnerOfABowlingGame().IsWinner( player1, player2 );
+}
