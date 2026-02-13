a487751c6f94cd4f4c9266bc884e13d679ffffed
Vitaly Dushkin <vetalapo@gmail.com>
2026-02-13 23:59:30 +0100

    #3285: improve tests namespacing

--- c-sharp/Tests/arrays-and-hashing/FindIndicesOfStableMountainsTests.cs
+++ c-sharp/Tests/arrays-and-hashing/FindIndicesOfStableMountainsTests.cs
@@ -4,6 +4,8 @@ using NUnit.Framework;
 
 using Problems;
 
+namespace ArraysAndHashing;
+
 public class FindIndicesOfStableMountainsTests
 {
     [TestCase( new int[] { 1, 2, 3, 4, 5 }, 2, ExpectedResult = new int[] { 3, 4 } )]
