6c11f3ac5bf4f25f6e9fd2eecdd742a97a74aa42
Vitaly Dushkin <vetalapo@gmail.com>
2026-02-15 12:55:01 +0100

    #3842: Toggle Light Bulbs; solution & tests

---
+++ c-sharp/Tests/arrays-and-hashing/ToggleLightBulbsSolutionTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class ToggleLightBulbsSolutionTests
+{
+    [TestCase( new int[] { 10, 30, 20, 10 }, ExpectedResult = new int[] { 20, 30 } )]
+    [TestCase( new int[] { 100, 100 }, ExpectedResult = new int[0] )]
+    public IList<int> ToggleLightBulbsTest( IList<int> bulbs ) =>
+        new ToggleLightBulbsSolution().ToggleLightBulbs( bulbs );
+}
