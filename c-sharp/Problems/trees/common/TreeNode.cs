5c51e486ed6e9e8adac2405c2c5af4ba913bca7b
Vitaly Dushkin <vetalapo@gmail.com>
2026-02-20 18:11:40 +0100

    #938: Rang Sum of BST; solution & tests.

--- c-sharp/Problems/trees/common/TreeNode.cs
+++ c-sharp/Problems/trees/common/TreeNode.cs
@@ -34,7 +34,7 @@ public class TreeNode
             node.left = values[i] is null
                 ? null
                 : new TreeNode( (int)values[i] );
-            
+
             node.right = i + 1 >= values.Length || values[i + 1] is null
                 ? null
                 : new TreeNode( (int)values[i + 1] );
---
+++ c-sharp/Tests/trees/RangeSumOfBSTTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+using Problems.Common;
+
+namespace Tree;
+
+public class RangeSumOfBSTTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int RangeSumBSTTest( TreeNode root, int low, int high ) =>
+        new RangeSumOfBST().RangeSumBST( root, low, high );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( TreeNode.Create( [10, 5, 15, 3, 7, null, 18] ), 7, 15 )
+                .Returns( 32 );
+
+            yield return new TestCaseData( TreeNode.Create( [10, 5, 15, 3, 7, 13, 18, 1, null, 6] ), 6, 10 )
+                .Returns( 23 );
+        }
+    }
+}
