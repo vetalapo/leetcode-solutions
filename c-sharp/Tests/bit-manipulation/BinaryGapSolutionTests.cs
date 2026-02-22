ee12b8efa88f72be0e7c4a322496aeb8a2bb8a26
Vitaly Dushkin <vetalapo@gmail.com>
2026-02-22 22:55:16 +0100

    #868: Binary Gap; solution & tests.

---
+++ c-sharp/Tests/bit-manipulation/BinaryGapSolutionTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace BitManipulation;
+
+public class BinaryGapSolutionTests
+{
+    [TestCase( 22, ExpectedResult = 2 )]
+    [TestCase( 8, ExpectedResult = 0 )]
+    [TestCase( 5, ExpectedResult = 2 )]
+    [TestCase( 6, ExpectedResult = 1 )]
+    public int BinaryGapTest( int n ) =>
+        new BinaryGapSolution().BinaryGap( n );
+}
