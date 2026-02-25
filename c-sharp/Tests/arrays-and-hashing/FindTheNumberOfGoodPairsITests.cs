84e521aea884ea3888dc7f255c6de5b5dc2543d1
Vitaly Dushkin <vetalapo@gmail.com>
2026-02-25 22:53:53 +0100

    #3162: Find the Number of Good Pairs I; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/FindTheNumberOfGoodPairsITests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class FindTheNumberOfGoodPairsITests
+{
+    [TestCase( new int[] { 1, 3, 4 }, new int[] { 1, 3, 4 }, 1, ExpectedResult = 5 )]
+    [TestCase( new int[] { 1, 2, 4, 12 }, new int[] { 2, 4 }, 3, ExpectedResult = 2 )]
+    public int NumberOfPairsTest( int[] nums1, int[] nums2, int k ) =>
+        new FindTheNumberOfGoodPairsI().NumberOfPairs( nums1, nums2, k );
+}
