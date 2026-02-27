fc978e20206cc092fe01d0b1935e7af1be895dd2
Vitaly Dushkin <vetalapo@gmail.com>
2026-02-27 16:02:18 +0100

    #2176: Count Equal and Divisible Pairs in an Array; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/CountEqualAndDivisiblePairsInAnArrayTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class CountEqualAndDivisiblePairsInAnArrayTests
+{
+    [TestCase( new int[] { 3, 1, 2, 2, 2, 1, 3 }, 2, ExpectedResult = 4 )]
+    [TestCase( new int[] { 1, 2, 3, 4 }, 1, ExpectedResult = 0 )]
+    public int CountPairsTest( int[] nums, int k ) =>
+        new CountEqualAndDivisiblePairsInAnArray().CountPairs( nums, k );
+}
