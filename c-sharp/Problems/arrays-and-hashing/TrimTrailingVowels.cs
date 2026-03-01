18decc4a8daa7a6e49ec34e60f3d84f3ec38180a
Vitaly Dushkin <vetalapo@gmail.com>
2026-03-01 12:44:24 +0100

    #3856: Trim Trailing Vowels; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/TrimTrailingVowels.cs
@@ -0,0 +1,67 @@
+﻿/**
+ * 3856
+ * Trim Trailing Vowels
+ **
+ * You are given a string s
+ * that consists of lowercase English letters.
+ *
+ * Return the string
+ * obtained by removing all trailing vowels from s.
+ *
+ * The vowels consist of the characters
+ * 'a', 'e', 'i', 'o', and 'u'.
+ *
+ * Example 1:
+ *   Input: s = "idea"
+ *   Output: "id"
+ *   Explanation:
+ *     Removing "idea",
+ *     we obtain the string "id".
+ *
+ * Example 2:
+ *   Input: s = "day"
+ *   Output: "day"
+ *   Explanation:
+ *     There are no trailing vowels in the string "day".
+ *
+ * Example 3:
+ *   Input: s = "aeiou"
+ *   Output: ""
+ *   Explanation:
+ *     Removing "aeiou",
+ *     we obtain the string "".
+ *
+ * Constraints:
+ *    • 1 <= s.length <= 100
+ *    • s consists of only lowercase English letters.
+ *
+ * Hint 1:
+ *   Simulate;
+ *   pop the last character as long as it is a vowel.
+ **
+ * https://leetcode.com/problems/trim-trailing-vowels/
+***/
+
+namespace Problems;
+
+public class TrimTrailingVowelsSolution
+{
+    public string TrimTrailingVowels( string s )
+    {
+        int i = s.Length - 1;
+
+        while ( i >= 0 && IsVowel( s[i] ) )
+        {
+            i--;
+        }
+
+        return i < 0
+            ? string.Empty
+            : s[0..( i + 1 )];
+    }
+
+    private bool IsVowel( char c )
+    {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/TrimTrailingVowelsSolutionTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class TrimTrailingVowelsSolutionTests
+{
+    [TestCase( "idea", ExpectedResult = "id" )]
+    [TestCase( "day", ExpectedResult = "day" )]
+    [TestCase( "aeiou", ExpectedResult = "" )]
+    public string TrimTrailingVowelsTest( string s ) =>
+        new TrimTrailingVowelsSolution().TrimTrailingVowels( s );
+}
