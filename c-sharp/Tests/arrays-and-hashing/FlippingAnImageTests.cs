c8185f15d4ab1137ebea2e09e130abe08a7b3b0d
Vitaly Dushkin <vetalapo@gmail.com>
2026-03-02 22:58:04 +0100

    #832: Flipping an Image; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/FlippingAnImageTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class FlippingAnImageTests
+{
+    [TestCaseSource( nameof( TestCases ) )]
+    public int[][] FlipAndInvertImageTest( int[][] image ) =>
+        new FlippingAnImage().FlipAndInvertImage( image );
+
+    private static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData( [new int[][] { [1, 1, 0], [1, 0, 1], [0, 0, 0] }] )
+                .Returns( new int[][] { [1, 0, 0], [0, 1, 0], [1, 1, 1] } );
+
+            yield return new TestCaseData( [new int[][] { [1, 1, 0, 0], [1, 0, 0, 1], [0, 1, 1, 1], [1, 0, 1, 0] }] )
+                .Returns( new int[][] { [1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 1], [1, 0, 1, 0] } );
+        }
+    }
+}
