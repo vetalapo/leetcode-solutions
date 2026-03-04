c422bcd0fb52c859699073f21e5a54f8ad3c4a4d
Vitaly Dushkin <vetalapo@gmail.com>
2026-03-04 23:58:30 +0100

    #3582: imrove tests namespacing

--- c-sharp/Tests/arrays-and-hashing/GenerateTagForVideoCaptionTests.cs
+++ c-sharp/Tests/arrays-and-hashing/GenerateTagForVideoCaptionTests.cs
@@ -2,6 +2,8 @@
 
 using Problems;
 
+namespace ArraysAndHashing;
+
 public class GenerateTagForVideoCaptionTests
 {
     [TestCase( "Leetcode daily streak achieved", ExpectedResult = "#leetcodeDailyStreakAchieved" )]
