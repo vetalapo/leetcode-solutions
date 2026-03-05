1e6dc1c7c9fffbb31ee43aded51f1bff6fbcf020
Vitaly Dushkin <vetalapo@gmail.com>
2026-03-05 01:11:11 +0100

    #905: Sort Array By Parity; solution & tests.

---
+++ c-sharp/Tests/two-pointers/SortArrayByParitySolutionTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace TwoPointers;
+
+public class SortArrayByParitySolutionTests
+{
+    [TestCase( new int[] { 3, 1, 2, 4 }, ExpectedResult = new int[] { 2, 4, 1, 3 } )]
+    [TestCase( new int[] { 0 }, ExpectedResult = new int[] { 0 } )]
+    public int[] SortArrayByParityTest( int[] nums ) =>
+        new SortArrayByParitySolution().SortArrayByParity( nums );
+}
