78f95991d658eb755dd342a17feb5783ecc406cc
Vitaly Dushkin <vetalapo@gmail.com>
2026-03-06 18:05:34 +0100

    #3427: Sum of Variable Length Subarrays; solution & tests.

---
+++ c-sharp/Tests/prefix-sum/SumOfVariableLengthSubarraysTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace PrefixSum;
+
+public class SumOfVariableLengthSubarraysTests
+{
+    [TestCase( new int[] { 2, 3, 1 }, ExpectedResult = 11 )]
+    [TestCase( new int[] { 3, 1, 1, 2 }, ExpectedResult = 13 )]
+    public int SubarraySumTest( int[] nums ) =>
+        new SumOfVariableLengthSubarrays().SubarraySum( nums );
+}
