877b4a1136332b3b73a829e5299a8bc986524edb
Vitaly Dushkin <vetalapo@gmail.com>
2026-03-09 23:46:13 +0100

    #3861: Minimum Capacity Box; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/MinimumCapacityBoxTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class MinimumCapacityBoxTests
+{
+    [TestCase( new int[] { 1, 5, 3, 7 }, 3, ExpectedResult = 2 )]
+    [TestCase( new int[] { 3, 5, 4, 3 }, 2, ExpectedResult = 0 )]
+    [TestCase( new int[] { 4 }, 5, ExpectedResult = -1 )]
+    public int MinimumIndexTest( int[] capacity, int itemSize ) =>
+        new MinimumCapacityBox().MinimumIndex( capacity, itemSize );
+}
