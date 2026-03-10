4d4878aa62b05edad79dd2db379e668f6f90aaba
Vitaly Dushkin <vetalapo@gmail.com>
2026-03-10 10:40:47 +0100

    #3803: Count Residue Prefixes; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/CountResiduePrefixesTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class CountResiduePrefixesTests
+{
+    [TestCase( "abc", ExpectedResult = 2 )]
+    [TestCase( "dd", ExpectedResult = 1 )]
+    [TestCase( "bob", ExpectedResult = 2 )]
+    [TestCase( "bbbb", ExpectedResult = 2 )]
+    public int ResiduePrefixesTest( string s ) =>
+        new CountResiduePrefixes().ResiduePrefixes( s );
+}
