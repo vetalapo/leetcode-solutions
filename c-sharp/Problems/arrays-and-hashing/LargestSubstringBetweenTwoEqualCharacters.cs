c7a07b9d95c3d6a39bdeede9b4d32e0860091186
Vitaly Dushkin <vetalapo@gmail.com>
2026-03-11 23:11:51 +0100

    #1624: Largest Substring Between Two Equal Characters; solution & tests.

---
+++ c-sharp/Problems/arrays-and-hashing/LargestSubstringBetweenTwoEqualCharacters.cs
@@ -0,0 +1,68 @@
+﻿/**
+ * 1624
+ * Largest Substring Between Two Equal Characters
+ **
+ * Given a string s,
+ * return the length of the longest substring between two equal characters,
+ * excluding the two characters.
+ * If there is no such substring return -1.
+ *
+ * A substring is a contiguous sequence of characters within a string.
+ *
+ * Example 1:
+ *   Input: s = "aa"
+ *   Output: 0
+ *   Explanation:
+ *     The optimal substring here is an empty substring between the two 'a's.
+ *
+ * Example 2:
+ *   Input: s = "abca"
+ *   Output: 2
+ *   Explanation:
+ *     The optimal substring here is "bc".
+ *
+ * Example 3:
+ *   Input: s = "cbzxy"
+ *   Output: -1
+ *   Explanation:
+ *     There are no characters that appear twice in s.
+ *
+ * Constraints:
+ *   • 1 <= s.length <= 300
+ *   • s contains only lowercase English letters.
+ *
+ * Hint 1:
+ *   Try saving the first and last position of each character
+ *
+ * Hint 2:
+ *   Try finding every pair of indexes with equal characters
+ **
+ * https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+***/
+
+using System;
+
+namespace Problems;
+
+public class LargestSubstringBetweenTwoEqualCharacters
+{
+    public int MaxLengthBetweenEqualCharacters( string s )
+    {
+        int max = -1;
+        int[] map = new int[27];
+
+        for ( int i = 0; i < s.Length; i++ )
+        {
+            if ( map[s[i] - 'a' + 1] > 0 )
+            {
+                max = Math.Max( max, i - map[s[i] - 'a' + 1] );
+            }
+            else
+            {
+                map[s[i] - 'a' + 1] = i + 1;
+            }
+        }
+
+        return max;
+    }
+}
---
+++ c-sharp/Tests/arrays-and-hashing/LargestSubstringBetweenTwoEqualCharactersTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class LargestSubstringBetweenTwoEqualCharactersTests
+{
+    [TestCase( "aa", ExpectedResult = 0 )]
+    [TestCase( "abca", ExpectedResult = 2 )]
+    [TestCase( "cbzxy", ExpectedResult = -1 )]
+    [TestCase( "mgntdygtxrvxjnwksqhxuxtrv", ExpectedResult = 18 )]
+    public int MaxLengthBetweenEqualCharactersTest( string s ) =>
+        new LargestSubstringBetweenTwoEqualCharacters().MaxLengthBetweenEqualCharacters( s );
+}
