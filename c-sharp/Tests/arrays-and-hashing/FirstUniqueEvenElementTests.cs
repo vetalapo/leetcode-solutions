3fd49dd86a13c3ac6eadca3578205d20d5aa22ef
Vitaly Dushkin <vetalapo@gmail.com>
2026-03-15 20:03:17 +0100

    #3866: First Unique Even Element; solution & tests.

---
+++ c-sharp/Tests/arrays-and-hashing/FirstUniqueEvenElementTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace ArraysAndHashing;
+
+public class FirstUniqueEvenElementTests
+{
+    [TestCase( new int[] { 3, 4, 2, 5, 4, 6 }, ExpectedResult = 2 )]
+    [TestCase( new int[] { 4, 4 }, ExpectedResult = -1 )]
+    public int FirstUniqueEvenTest( int[] nums ) =>
+        new FirstUniqueEvenElement().FirstUniqueEven( nums );
+}
