849c6924f84c98c02591d772cf4c5ff0fdc6be26
Vitaly Dushkin <vetalapo@gmail.com>
2026-03-16 21:05:54 +0100

    #3870: Count Commas in Range; solution & tests.

---
+++ c-sharp/Tests/math/CountCommasInRangeTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class CountCommasInRangeTests
+{
+    [TestCase( 1002, ExpectedResult = 3 )]
+    [TestCase( 998, ExpectedResult = 0 )]
+    public int CountCommasTest( int n ) =>
+        new CountCommasInRange().CountCommas( n );
+}
