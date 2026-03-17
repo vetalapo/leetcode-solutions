df94e5ed33dcf541ff41fe2bdfd2263b1a57c3fc
Vitaly Dushkin <vetalapo@gmail.com>
2026-03-17 13:38:58 +0100

    #70: Climbing Stairs; solution & tests.

---
+++ c-sharp/Problems/math/ClimbingStairs.cs
@@ -0,0 +1,56 @@
+﻿/**
+ * 70
+ * Climbing Stairs
+ **
+ * You are climbing a staircase.
+ * It takes n steps to reach the top.
+ *
+ * Each time you can either climb 1 or 2 steps.
+ * In how many distinct ways can you climb to the top?
+ *
+ * Example 1:
+ *   Input: n = 2
+ *   Output: 2
+ *   Explanation:
+ *     There are two ways to climb to the top.
+ *       1. 1 step + 1 step
+ *       2. 2 steps
+ *
+ * Example 2:
+ *   Input: n = 3
+ *   Output: 3
+ *   Explanation:
+ *     There are three ways to climb to the top.
+ *       1. 1 step + 1 step + 1 step
+ *       2. 1 step + 2 steps
+ *       3. 2 steps + 1 step
+ *
+ * Constraints:
+ *   • 1 <= n <= 45
+ *
+ * Hint 1:
+ *   To reach nth step,
+ *   what could have been your previous steps?
+ *   (Think about the step sizes)
+ **
+ * https://leetcode.com/problems/climbing-stairs/
+***/
+
+namespace Problems;
+
+public class ClimbingStairs
+{
+    public int ClimbStairs( int n )
+    {
+        int prev = 0;
+        int fib = 1;
+
+        while ( n > 0 )
+        {
+            (prev, fib) = (fib, prev + fib);
+            n--;
+        }
+
+        return fib;
+    }
+}
---
+++ c-sharp/Tests/math/ClimbingStairsTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Problems;
+
+namespace Mathematics;
+
+public class ClimbingStairsTests
+{
+    [TestCase( 2, ExpectedResult = 2 )]
+    [TestCase( 3, ExpectedResult = 3 )]
+    [TestCase( 4, ExpectedResult = 5 )]
+    [TestCase( 5, ExpectedResult = 8 )]
+    public int ClimbStairsTest( int n ) =>
+        new ClimbingStairs().ClimbStairs( n );
+}
