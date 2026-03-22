54a9a89308a40b430144019d6d2aaa1497bcf402
Vitaly Dushkin <vetalapo@gmail.com>
2026-03-22 21:59:43 +0100

    #2881: improve formatting

--- pandas/src/2881-create-a-new-column.py
+++ pandas/src/2881-create-a-new-column.py
@@ -11,7 +11,8 @@
 #   +-------------+--------+
 #
 # A company plans to provide its employees with a bonus.
-# Write a solution to create a new column name bonus that contains the doubled values of the salary column.
+# Write a solution to create a new column name bonus
+# that contains the doubled values of the salary column.
 #
 # The result format is in the following example.
 #
