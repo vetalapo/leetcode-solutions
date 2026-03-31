0ba49461c2a25019b44a223a89d8b60d112c3fbc
Vitaly Dushkin <vetalapo@gmail.com>
2026-03-31 23:56:33 +0200

    #2881: improve formatting

--- pandas/src/2881-create-a-new-column.py
+++ pandas/src/2881-create-a-new-column.py
@@ -19,28 +19,28 @@
 # Example 1:
 #   Input:
 #   DataFrame employees
-#   +---------+--------+
-#   | name    | salary |
-#   +---------+--------+
-#   | Piper   | 4548   |
-#   | Grace   | 28150  |
-#   | Georgia | 1103   |
-#   | Willow  | 6593   |
-#   | Finn    | 74576  |
-#   | Thomas  | 24433  |
-#   +---------+--------+
+#     +---------+--------+
+#     | name    | salary |
+#     +---------+--------+
+#     | Piper   | 4548   |
+#     | Grace   | 28150  |
+#     | Georgia | 1103   |
+#     | Willow  | 6593   |
+#     | Finn    | 74576  |
+#     | Thomas  | 24433  |
+#     +---------+--------+
 #
 #   Output:
-#   +---------+--------+--------+
-#   | name    | salary | bonus  |
-#   +---------+--------+--------+
-#   | Piper   | 4548   | 9096   |
-#   | Grace   | 28150  | 56300  |
-#   | Georgia | 1103   | 2206   |
-#   | Willow  | 6593   | 13186  |
-#   | Finn    | 74576  | 149152 |
-#   | Thomas  | 24433  | 48866  |
-#   +---------+--------+--------+
+#     +---------+--------+--------+
+#     | name    | salary | bonus  |
+#     +---------+--------+--------+
+#     | Piper   | 4548   | 9096   |
+#     | Grace   | 28150  | 56300  |
+#     | Georgia | 1103   | 2206   |
+#     | Willow  | 6593   | 13186  |
+#     | Finn    | 74576  | 149152 |
+#     | Thomas  | 24433  | 48866  |
+#     +---------+--------+--------+
 #
 #   Explanation:
 #     A new column bonus is created by doubling the value in the column salary.
