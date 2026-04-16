e018f462f9ad72e64b3888fc31fad122afe3d27c
Vitaly Dushkin <vetalapo@gmail.com>
2026-04-17 00:05:26 +0200

    #2515: improve formatting

--- c-sharp/Tests/arrays-and-hashing/ShortestDistanceToTargetStringInACircularArrayTests.cs
+++ c-sharp/Tests/arrays-and-hashing/ShortestDistanceToTargetStringInACircularArrayTests.cs
@@ -6,9 +6,9 @@ namespace ArraysAndHashing;
 
 public class ShortestDistanceToTargetStringInACircularArrayTests
 {
-    [TestCase( new string[] { "hello","i","am","leetcode","hello" }, "hello", 1, ExpectedResult = 1 )]
-    [TestCase( new string[] { "a","b","leetcode" }, "leetcode", 0, ExpectedResult = 1 )]
-    [TestCase( new string[] { "i","eat","leetcode" }, "ate", 0, ExpectedResult = -1 )]
+    [TestCase( new string[] { "hello", "i", "am", "leetcode", "hello" }, "hello", 1, ExpectedResult = 1 )]
+    [TestCase( new string[] { "a", "b", "leetcode" }, "leetcode", 0, ExpectedResult = 1 )]
+    [TestCase( new string[] { "i", "eat", "leetcode" }, "ate", 0, ExpectedResult = -1 )]
     public int ClosestTargetTest( string[] words, string target, int startIndex ) =>
         new ShortestDistanceToTargetStringInACircularArray().ClosestTarget( words, target, startIndex );
 }
