fd723c040d797dcf708f50b2d9a7698d8a83341b
Vitaly Dushkin <vetalapo@gmail.com>
2026-04-18 01:21:09 +0200

    #88: add tests

--- c-sharp/Tests/arrays-and-hashing/MergeSortedArrayTests.cs
+++ c-sharp/Tests/arrays-and-hashing/MergeSortedArrayTests.cs
@@ -6,7 +6,13 @@ namespace ArraysAndHashing;
 
 public class MergeSortedArrayTests
 {
-    public void MergeTest( int[] nums1, int m, int[] nums2, int n )
+    [TestCase( new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3, new int[] { 1, 2, 2, 3, 5, 6 } )]
+    [TestCase( new int[] { 1 }, 1, new int[0], 0, new int[] { 1 } )]
+    [TestCase( new int[] { 0 }, 0, new int[] { 1 }, 1, new int[] { 1 } )]
+    public void MergeTest( int[] nums1, int m, int[] nums2, int n, int[] expectedResult )
     {
+        new MergeSortedArray().Merge( nums1, m, nums2, n );
+
+	Assert.AreEqual( nums1, expectedResult );
     }
 }
