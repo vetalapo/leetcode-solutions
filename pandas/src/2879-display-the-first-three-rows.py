b19f4d6c2ece6f3f9af353e5bef0d020dfe2822d
Vitaly Dushkin <vetalapo@gmail.com>
2026-05-04 20:38:27 +0200

    #2879: improve formatting

--- pandas/src/2879-display-the-first-three-rows.py
+++ pandas/src/2879-display-the-first-three-rows.py
@@ -44,11 +44,10 @@
 ##
 
 import sys
+import pandas as pd
 
 # Preventing creation of __pycache__
 sys.dont_write_bytecode = True
-
-import pandas as pd
 
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
     return employees.head(3)
