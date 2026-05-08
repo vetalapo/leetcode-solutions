945b22e311fe526c4351dc95e0d6dc166b085fbe
Vitaly Dushkin <vetalapo@gmail.com>
2026-05-08 21:45:09 +0200

    #2885: improve formatting

--- pandas/src/2885-rename-columns.py
+++ pandas/src/2885-rename-columns.py
@@ -54,11 +54,10 @@
 ##
 
 import sys
+import pandas as pd
 
 # Preventing creation of __pycache__
 sys.dont_write_bytecode = True
-
-import pandas as pd
 
 def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
     return students.rename(columns={
