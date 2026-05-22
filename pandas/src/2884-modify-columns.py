7af7a717e15a686cc3733ed3d980981c8589bad1
Vitaly Dushkin <vetalapo@gmail.com>
2026-05-22 23:52:17 +0200

    #2884: improve formatting

--- pandas/src/2884-modify-columns.py
+++ pandas/src/2884-modify-columns.py
@@ -11,10 +11,11 @@
 #   +-------------+--------+
 #
 # A company intends to give its employees a pay rise.
-# Write a solution to modify the salary column by multiplying each salary by 2.
-# 
+# Write a solution to modify the salary column
+# by multiplying each salary by 2.
+#
 # The result format is in the following example.
-# 
+#
 # Example 1:
 #   Input:
 #     DataFrame employees
