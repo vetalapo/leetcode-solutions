e3f22585bb1ab4e7017af16cd8624bc8f6e1dbc5
Vitaly Dushkin <vetalapo@gmail.com>
2026-05-23 21:37:49 +0200

    #2879: improve formatting

--- pandas/src/2879-display-the-first-three-rows.py
+++ pandas/src/2879-display-the-first-three-rows.py
@@ -2,7 +2,8 @@
 # 2879
 # Display the First Three Rows
 ##
-# DataFrame: employees
+# DataFrame:
+#   employees
 #   +-------------+--------+
 #   | Column Name | Type   |
 #   +-------------+--------+
@@ -16,17 +17,18 @@
 # 
 # Example 1:
 #   Input:
-#     DataFrame employees
-#     +-------------+-----------+-----------------------+--------+
-#     | employee_id | name      | department            | salary |
-#     +-------------+-----------+-----------------------+--------+
-#     | 3           | Bob       | Operations            | 48675  |
-#     | 90          | Alice     | Sales                 | 11096  |
-#     | 9           | Tatiana   | Engineering           | 33805  |
-#     | 60          | Annabelle | InformationTechnology | 37678  |
-#     | 49          | Jonathan  | HumanResources        | 23793  |
-#     | 43          | Khaled    | Administration        | 40454  |
-#     +-------------+-----------+-----------------------+--------+
+#     DataFrame:
+#       employees
+#       +-------------+-----------+-----------------------+--------+
+#       | employee_id | name      | department            | salary |
+#       +-------------+-----------+-----------------------+--------+
+#       | 3           | Bob       | Operations            | 48675  |
+#       | 90          | Alice     | Sales                 | 11096  |
+#       | 9           | Tatiana   | Engineering           | 33805  |
+#       | 60          | Annabelle | InformationTechnology | 37678  |
+#       | 49          | Jonathan  | HumanResources        | 23793  |
+#       | 43          | Khaled    | Administration        | 40454  |
+#       +-------------+-----------+-----------------------+--------+
 #
 #   Output:
 #     +-------------+---------+-------------+--------+
