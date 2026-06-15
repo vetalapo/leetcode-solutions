c72e3c613793dfdd907994f682ab833c87a31295
Vitaly Dushkin <vetalapo@gmail.com>
2026-06-15 15:00:00 +0200

    128: improve tests namespacing

--- c-sharp/Tests/arrays-and-hashing/LongestConsecutiveSequenceTests.cs
+++ c-sharp/Tests/arrays-and-hashing/LongestConsecutiveSequenceTests.cs
@@ -2,10 +2,12 @@
 
 using Problems;
 
+namespace ArraysAndHashing;
+
 public class LongestConsecutiveSequenceTests
 {
-    [TestCase(new object[] { new int[] { 100, 4, 200, 1, 3, 2 } }, ExpectedResult = 4 )]
-    [TestCase(new object[] { new int[] { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 } }, ExpectedResult = 9 )]
+    [TestCase( [new int[] { 100, 4, 200, 1, 3, 2 }], ExpectedResult = 4 )]
+    [TestCase( [new int[] { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 }], ExpectedResult = 9 )]
     public int LongestConsecutiveTest( int[] nums ) =>
         new LongestConsecutiveSequence().LongestConsecutive( nums );
 }
